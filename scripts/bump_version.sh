94395be95579c39d3a106419fe43efb15a69b4e6
Michael Dwan <m@dwan.io>
2019-08-01 11:01:24 -0500

    confirmation & clean state checks in bump version script

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -6,14 +6,13 @@ ORIGIN=${ORIGIN:-origin}
 
 bump=${1:-patch}
 
-# if [[ $(git status --porcelain) != "" ]]; then
-#   echo "Error: repo is dirty. Run git status, clean repo and try again."
-#   exit 1
-# elif [[ $(git status --porcelain -b | grep -e "ahead" -e "behind") != "" ]]; then
-#   echo "Error: repo has unpushed commits. Push commits to remote and try again."
-#   exit 1
-# fi  
-
+if [[ $(git status --porcelain) != "" ]]; then
+  echo "Error: repo is dirty. Run git status, clean repo and try again."
+  exit 1
+elif [[ $(git status --porcelain -b | grep -e "ahead" -e "behind") != "" ]]; then
+  echo "Error: repo has unpushed commits. Push commits to remote and try again."
+  exit 1
+fi  
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -22,10 +21,14 @@ previous_version="$("$dir"/../scripts/version.sh -s)"
 new_version=$("$dir"/../scripts/semver bump "$bump" "$previous_version")
 new_version="v$new_version"
 
-echo $new_version
-
 echo "Bumping version from v${previous_version} to ${new_version}"
 
-git tag -m "release ${new_version}" -a "$new_version" && git push "${ORIGIN}" tag "$new_version"
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  git tag -m "release ${new_version}" -a "$new_version" && git push "${ORIGIN}" tag "$new_version"
 
-echo "done"
+  echo "done"
+fi
+
