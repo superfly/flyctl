0c520d6e65fca852694252057b962d4d718cc0f5
Michael Dwan <m@dwan.io>
2019-08-02 16:26:38 -0500

    pull+push remote images, better output and error handling

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
-	"github.com/gosuri/uiprogress"
+	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
 	"github.com/superfly/flyctl/api"
 	"github.com/superfly/flyctl/docker"
@@ -72,57 +73,49 @@ func (cmd *pushCommand) Init(args []string) error {
 func (cmd *pushCommand) Run(args []string) error {
 	cmd.imageTag = docker.NewDeploymentTag(cmd.appName)
 
+	cmdOutput := os.Stderr
+
 	buildPath := ""
 
-	if docker.IsDockerfilePath(cmd.imageRef) {
-		fmt.Println("we have a file, use the base dir and build!")
-		buildPath = path.Dir(cmd.imageRef)
-	} else if docker.IsDirContainingDockerfile(cmd.imageRef) {
-		fmt.Println("we have a directory containing a dockerfile")
-		buildPath = cmd.imageRef
-	} else if strings.HasPrefix(cmd.imageRef, ".") {
-		cwd, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-		buildPath = cwd
+	printHeader("Resolving image")
+
+	buildPath, err := cmd.resolveBuildPath()
+	if err != nil {
+		return err
 	}
 
 	if buildPath != "" {
-		op := cmd.dockerClient.BuildImage(cmd.imageRef, cmd.imageTag)
-
-		for msg := range op.Status() {
-			fmt.Print(msg.Stream)
+		printHeader("Building image")
+		if err := cmd.dockerClient.BuildImage(buildPath, cmd.imageTag, cmdOutput); err != nil {
+			return err
 		}
-
-		if op.Error() != nil {
-			return op.Error()
-		}
-		// } else if docker.IsRemoteImageReference(cmd.imageRef) {
-		// 	fmt.Println("it's a remote image, pull then deploy?")
-		// 	return nil
 	} else {
-		fmt.Println("Locating image...")
 		if err := cmd.locateImageID(); err != nil {
 			return err
 		}
 		fmt.Println("-->", cmd.imageID)
 
-		fmt.Println("Creating deployment tag...")
-		if err := cmd.createDeploymentTag(); err != nil {
+		printHeader("Creating deployment tag")
+		if err := cmd.dockerClient.TagImage(cmd.imageID, cmd.imageTag); err != nil {
 			return err
 		}
 		fmt.Println("-->", cmd.imageTag)
 	}
 
-	fmt.Println("Pushing image...")
-	if err := cmd.pushImage(); err != nil {
+	printHeader("Pushing image")
+	if err := cmd.dockerClient.PushImage(cmd.imageTag, cmdOutput); err != nil {
 		return err
 	}
 	fmt.Println("-->", "done")
 
-	fmt.Println("Releasing...")
+	printHeader("Releasing")
 	if err := cmd.deployImage(); err != nil {
+		return err
+	}
+	fmt.Println("-->", "done")
+
+	printHeader("Cleaning")
+	if err := cmd.dockerClient.DeleteDeploymentImages(cmd.appName); err != nil {
 		return err
 	}
 	fmt.Println("-->", "done")
@@ -136,49 +129,34 @@ func (cmd *pushCommand) Run(args []string) error {
 	return nil
 }
 
+func (cmd *pushCommand) resolveBuildPath() (string, error) {
+	if docker.IsDockerfilePath(cmd.imageRef) {
+		fmt.Printf("found file at '%s'\n", cmd.imageRef)
+		return path.Dir(cmd.imageRef), nil
+	} else if docker.IsDirContainingDockerfile(cmd.imageRef) {
+		fmt.Printf("found Dockerfile in '%s'\n", cmd.imageRef)
+		return cmd.imageRef, nil
+	} else if strings.HasPrefix(cmd.imageRef, ".") {
+		fmt.Printf("'%s' is a local path\n", cmd.imageRef)
+		return filepath.Abs(cmd.imageRef)
+	}
+
+	return "", nil
+}
+
 func (cmd *pushCommand) locateImageID() error {
-	img, err := cmd.dockerClient.FindImage(cmd.imageRef)
+	img, err := cmd.dockerClient.ResolveImage(cmd.imageRef)
 	if err != nil {
 		return err
 	}
 
 	if img == nil {
-		return fmt.Errorf("Could not find a local image tagged %s", cmd.imageRef)
+		return fmt.Errorf("Could not resolve image %s", cmd.imageRef)
 	}
 
 	cmd.imageID = img.ID
 
 	return nil
-}
-
-func (cmd *pushCommand) createDeploymentTag() error {
-	return cmd.dockerClient.TagImage(cmd.imageID, cmd.imageTag)
-}
-
-func (cmd *pushCommand) pushImage() error {
-	op := cmd.dockerClient.PushImage(cmd.imageTag)
-
-	var layers = 0
-	var doneLayers = 0
-
-	uiprogress.Start()
-
-	bar := uiprogress.AddBar(100)
-	bar.AppendCompleted()
-	bar.Head = '>'
-	bar.PrependFunc(func(b *uiprogress.Bar) string {
-		return fmt.Sprintf("--> layer %d of %d", doneLayers, layers)
-	})
-
-	for msg := range op.Stream() {
-		layers = msg.LayersTotal
-		doneLayers = msg.LayersComplete
-		bar.Set(msg.Progress)
-	}
-
-	uiprogress.Stop()
-
-	return op.Error()
 }
 
 func (cmd *pushCommand) deployImage() error {
@@ -211,4 +189,8 @@ func (cmd *pushCommand) deployImage() error {
 	cmd.deployment = data.DeployImage.Deployment
 
 	return nil
+}
+
+func printHeader(message string) {
+	fmt.Println(aurora.Blue("==>"), message)
 }
--- docker/docker.go
+++ docker/docker.go
@@ -4,19 +4,34 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/pkg/term"
+	"github.com/mholt/archiver"
+	dockerparser "github.com/novln/docker-parser"
 	"github.com/spf13/viper"
 	"github.com/superfly/flyctl/flyctl"
+	"github.com/superfly/flyctl/terminal"
 	"golang.org/x/net/context"
 )
 
 func NewDeploymentTag(appName string) string {
 	t := time.Now()
 
-	return fmt.Sprintf("registry.fly.io/%s:deployment-%d", appName, t.Unix())
+	return fmt.Sprintf("%s%d", DeploymentTagPrefix(appName), t.Unix())
+}
+
+func DeploymentTagPrefix(appName string) string {
+	return fmt.Sprintf("registry.fly.io/%s:deployment-", appName)
 }
 
 type DockerClient struct {
@@ -34,8 +49,9 @@ func NewDockerClient() (*DockerClient, error) {
 	accessToken := viper.GetString(flyctl.ConfigAPIAccessToken)
 
 	authConfig := types.AuthConfig{
-		Username: accessToken,
-		Password: "x",
+		Username:      accessToken,
+		Password:      "x",
+		ServerAddress: "registry.fly.io",
 	}
 	encodedJSON, err := json.Marshal(authConfig)
 	if err != nil {
@@ -52,6 +68,196 @@ func NewDockerClient() (*DockerClient, error) {
 	return c, nil
 }
 
+func (c *DockerClient) ResolveImage(imageName string) (*types.ImageSummary, error) {
+	img, err := c.FindImage(imageName)
+	if img != nil {
+		return img, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Unable to find image '%s' locally\n", imageName)
+
+	ref, err := dockerparser.Parse(imageName)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := c.PullImage(ref.Remote(), os.Stdout); err != nil {
+		return nil, err
+	}
+
+	return c.FindImage(imageName)
+}
+
+func (c *DockerClient) PullImage(imageName string, out io.Writer) error {
+	resp, err := c.docker.ImagePull(c.ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+	defer resp.Close()
+
+	termFd, isTerm := term.GetFdInfo(os.Stderr)
+	return jsonmessage.DisplayJSONMessagesStream(resp, out, termFd, isTerm, nil)
+}
+
 func (c *DockerClient) TagImage(sourceRef, tag string) error {
 	return c.docker.ImageTag(c.ctx, sourceRef, tag)
+}
+
+func (c *DockerClient) DeleteDeploymentImages(appName string) error {
+	tagPrefix := DeploymentTagPrefix(appName)
+
+	filters := filters.NewArgs()
+	filters.Add("reference", tagPrefix+"*")
+
+	images, err := c.docker.ImageList(c.ctx, types.ImageListOptions{Filters: filters})
+	if err != nil {
+		return err
+	}
+
+	for _, image := range images {
+		for _, tag := range image.RepoTags {
+			_, err := c.docker.ImageRemove(c.ctx, tag, types.ImageRemoveOptions{PruneChildren: true})
+			if err != nil {
+				terminal.Error("Error deleting image", err)
+			}
+			fmt.Println("Removed deployment image:", tag)
+		}
+	}
+
+	return nil
+}
+
+func (c *DockerClient) BuildImage(path, tag string, out io.Writer) error {
+	tarReader, tarWriter := io.Pipe()
+
+	go func() {
+		defer tarWriter.Close()
+
+		if err := tarBuildContext(tarWriter, path); err != nil {
+			// if error occures here log and bail, the build will fail when the stream is closed
+			terminal.Error(err)
+			return
+		}
+	}()
+
+	resp, err := c.docker.ImageBuild(c.ctx, tarReader, types.ImageBuildOptions{
+		Tags: []string{tag},
+	})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	termFd, isTerm := term.GetFdInfo(os.Stderr)
+
+	return jsonmessage.DisplayJSONMessagesStream(resp.Body, out, termFd, isTerm, nil)
+}
+
+func tarBuildContext(writer io.Writer, path string) error {
+	tar := archiver.Tar{
+		MkdirAll: true,
+	}
+
+	if err := tar.Create(writer); err != nil {
+		return err
+	}
+
+	err := filepath.Walk(path, func(fpath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(path, fpath)
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Open(fpath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		err = tar.Write(archiver.File{
+			FileInfo: archiver.FileInfo{
+				FileInfo:   info,
+				CustomName: relPath,
+			},
+			ReadCloser: file,
+		})
+
+		return err
+	})
+
+	return err
+}
+
+var imageIDPattern = regexp.MustCompile("[a-f0-9]")
+
+func (c *DockerClient) FindImage(imageName string) (*types.ImageSummary, error) {
+	ref, err := dockerparser.Parse(imageName)
+	if err != nil {
+		return nil, err
+	}
+
+	isID := imageIDPattern.MatchString(imageName)
+
+	images, err := c.docker.ImageList(c.ctx, types.ImageListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	if isID {
+		for _, img := range images {
+			if img.ID[7:7+len(imageName)] == imageName {
+				terminal.Debug("Found image by id", imageName)
+				return &img, nil
+			}
+		}
+	}
+
+	searchTerms := []string{
+		imageName,
+		imageName + ":" + ref.Tag(),
+		ref.Name(),
+		ref.ShortName(),
+		ref.Remote(),
+		ref.Repository(),
+	}
+
+	terminal.Debug("Search terms:", searchTerms)
+
+	for _, img := range images {
+		for _, tag := range img.RepoTags {
+			// skip <none>:<none>
+			if strings.HasPrefix(tag, "<none>") {
+				continue
+			}
+
+			for _, term := range searchTerms {
+				if tag == term {
+					return &img, nil
+				}
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+func (c *DockerClient) PushImage(imageName string, out io.Writer) error {
+	resp, err := c.docker.ImagePush(c.ctx, imageName, types.ImagePushOptions{RegistryAuth: c.registryAuth})
+	if err != nil {
+		return err
+	}
+	defer resp.Close()
+
+	termFd, isTerm := term.GetFdInfo(os.Stderr)
+	return jsonmessage.DisplayJSONMessagesStream(resp, out, termFd, isTerm, nil)
 }
