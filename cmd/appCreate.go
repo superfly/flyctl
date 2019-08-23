82b85920e52434bf1d2bb17aa2ce1e8831de4f15
Michael Dwan <m@dwan.io>
2019-08-23 14:26:57 -0500

    Handle sigint when creating an app (fixes #7)

--- cmd/appCreate.go
+++ cmd/appCreate.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/manifoldco/promptui"
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/superfly/flyctl/cmd/presenters"
 	"github.com/superfly/flyctl/flyctl"
 )
@@ -27,21 +27,34 @@ func newAppCreateCommand() *Command {
 	return cmd
 }
 
+func isInterrupt(err error) bool {
+	return err != nil && err.Error() == "interrupt"
+}
+
 func runAppCreate(ctx *CmdContext) error {
 	name, _ := ctx.Config.GetString("name")
 	if name == "" {
-		prompt := promptui.Prompt{
-			Label: "App Name (leave blank to use an auto-generated name)",
+		prompt := &survey.Input{
+			Message: "App Name (leave blank to use an auto-generated name)",
 		}
-		name, _ = prompt.Run()
+		if err := survey.AskOne(prompt, &name); err != nil {
+			if isInterrupt(err) {
+				return nil
+			}
+		}
 	}
 
-	org, err := setTargetOrg(ctx)
-	if err != nil {
+	targetOrgSlug, _ := ctx.Config.GetString("org")
+	org, err := selectOrganization(ctx.FlyClient, targetOrgSlug)
+
+	switch {
+	case isInterrupt(err):
+		return nil
+	case err != nil || org == nil:
 		return fmt.Errorf("Error setting organization: %s", err)
 	}
 
-	app, err := ctx.FlyClient.CreateApp(name, org)
+	app, err := ctx.FlyClient.CreateApp(name, org.ID)
 	if err != nil {
 		return err
 	}
@@ -69,43 +82,4 @@ func runAppCreate(ctx *CmdContext) error {
 	}
 
 	return nil
-}
-
-func setTargetOrg(ctx *CmdContext) (string, error) {
-	orgs, err := ctx.FlyClient.GetOrganizations()
-	if err != nil {
-		return "", err
-	}
-
-	var targetOrgID string
-
-	if targetOrgSlug, _ := ctx.Config.GetString("org"); targetOrgSlug != "" {
-		for _, org := range orgs {
-			if org.Slug == targetOrgSlug {
-				targetOrgID = org.ID
-				break
-			}
-		}
-
-		if targetOrgSlug == "" {
-			return "", fmt.Errorf(`orgnaization "%s" not found`, targetOrgSlug)
-		}
-	}
-
-	if targetOrgID == "" {
-		prompt := promptui.Select{
-			Label: "Select Organization",
-			Items: orgs,
-			Size:  16,
-			Templates: &promptui.SelectTemplates{
-				Active:   "❯ {{ .Name }} ({{ .Slug }})",
-				Inactive: "  {{ .Name }} ({{ .Slug }})",
-			},
-		}
-
-		i, _, _ := prompt.Run()
-		targetOrgID = orgs[i].ID
-	}
-
-	return targetOrgID, nil
 }
---
+++ cmd/input.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/superfly/flyctl/api"
+)
+
+func confirm(message string) bool {
+	confirm := false
+	prompt := &survey.Confirm{
+		Message: message,
+	}
+	survey.AskOne(prompt, &confirm)
+
+	return confirm
+}
+
+func selectOrganization(client *api.Client, slug string) (*api.Organization, error) {
+	orgs, err := client.GetOrganizations()
+	if err != nil {
+		return nil, err
+	}
+
+	if slug != "" {
+		for _, org := range orgs {
+			if org.Slug == slug {
+				return &org, nil
+			}
+		}
+
+		return nil, fmt.Errorf(`orgnaization "%s" not found`, slug)
+	}
+
+	options := []string{}
+
+	for _, org := range orgs {
+		options = append(options, fmt.Sprintf("%s (%s)", org.Name, org.Slug))
+	}
+
+	selectedOrg := 0
+	prompt := &survey.Select{
+		Message:  "Select organization:",
+		Options:  options,
+		PageSize: 15,
+	}
+	if err := survey.AskOne(prompt, &selectedOrg); err != nil {
+		return nil, err
+	}
+
+	return &orgs[selectedOrg], nil
+}
