693a589f10bfbd3b2f2b56dfdab71ef215bfae47
Michael Dwan <m@dwan.io>
2019-12-16 20:51:11 -0600

    Prevent Dockerfile and .dockerignore from being excluded

--- docker/build.go
+++ docker/build.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/briandowns/spinner"
 	"github.com/docker/docker/builder/dockerignore"
+	"github.com/docker/docker/pkg/fileutils"
 	"github.com/dustin/go-humanize"
 	"github.com/logrusorgru/aurora"
 	"github.com/superfly/flyctl/api"
@@ -207,7 +208,12 @@ func readDockerignore(workingDir string) ([]string, error) {
 		return []string{}, nil
 	}
 
-	return dockerignore.ReadAll(file)
+	excludes, err := dockerignore.ReadAll(file)
+	if err == nil {
+		excludes = trimExcludes(excludes)
+	}
+
+	return excludes, err
 }
 
 func readGitignore(workingDir string) ([]string, error) {
@@ -219,7 +225,24 @@ func readGitignore(workingDir string) ([]string, error) {
 		return []string{}, nil
 	}
 
-	return dockerignore.ReadAll(file)
+	excludes, err := dockerignore.ReadAll(file)
+	if err == nil {
+		excludes = trimExcludes(excludes)
+	}
+
+	return excludes, err
+}
+
+func trimExcludes(excludes []string) []string {
+	if match, _ := fileutils.Matches(".dockerignore", excludes); match {
+		excludes = append(excludes, "!.dockerignore")
+	}
+
+	if match, _ := fileutils.Matches("Dockerfile", excludes); match {
+		excludes = append(excludes, "!Dockerfile")
+	}
+
+	return excludes
 }
 
 func printImageSize(size uint64) {
