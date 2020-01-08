23983741e6f915f303c3a0a3c6f6f6c73816623e
Dj Walker-Morgan <codepope@gmail.com>
2020-01-08 17:45:58 +0000

    Switched to a code gen approach. details in helpgen/README.md

--- helpgen/helpgen.go
+++ helpgen/helpgen.go
@@ -16,14 +16,36 @@ func main() {
 		log.Fatal("Can't parse docStrings")
 	}
 
+	mapped := tree.ToMap()
+
 	fmt.Println("package docstrings\n\nvar docstrings=map[string]KeyStrings{")
-	for _, k := range tree.Keys() {
-		v := tree.Get(k).(*toml.Tree)
-		usage := v.Get("usage").(string)
-		short := v.Get("shortHelp").(string)
-		long := v.Get("longHelp").(string)
-		fmt.Printf("    \"%s\":KeyStrings{\"%s\",\"%s\",\n    `%s`,\n},\n", k, usage, short, long)
-	}
+
+	dumpMap("", mapped)
 
 	fmt.Println("}")
+}
+
+func dumpMap(prefix string, m map[string]interface{}) {
+	_, prs := m["usage"]
+	if prs {
+		usage := m["usage"].(string)
+		short := m["shortHelp"].(string)
+		long := m["longHelp"].(string)
+		fmt.Printf("    \"%s\":KeyStrings{\"%s\",\"%s\",\n    `%s`,\n},\n", prefix, usage, short, long)
+	}
+
+	for k, v := range m {
+		switch node := v.(type) {
+		case map[string]interface{}:
+			if prefix != "" {
+				dumpMap(prefix+"."+k, v.(map[string]interface{}))
+			} else {
+				dumpMap(k, v.(map[string]interface{}))
+			}
+		case string:
+			// Nothing to do
+		default:
+			fmt.Println("Node ", node, " not handled")
+		}
+	}
 }
