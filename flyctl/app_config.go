c5bc1274e0f8b99797f71d034d55004565b62ad3
Michael Dwan <m@dwan.io>
2020-02-13 09:45:55 -0800

    SetPorts => SetInternalPort

--- flyctl/app_config.go
+++ flyctl/app_config.go
@@ -191,23 +191,15 @@ func (ac *AppConfig) WriteToFile(filename string) error {
 	return ac.WriteTo(file, ConfigFormatFromPath(filename))
 }
 
-func (ac *AppConfig) SetPorts(internal int, external int) bool {
+func (ac *AppConfig) SetInternalPort(port int) bool {
 	if services, ok := ac.Definition["services"].([]interface{}); ok {
 		if len(services) == 0 {
 			return false
 		}
 
 		if service, ok := services[0].(map[string]interface{}); ok {
-			if ports, ok := service["ports"].([]interface{}); ok {
-				if len(ports) == 0 {
-					return false
-				}
-				if port, ok := ports[0].(map[string]interface{}); ok {
-					service["internal_port"] = internal
-					port["port"] = external
-					return true
-				}
-			}
+			service["internal_port"] = port
+			return true
 		}
 	}
 
