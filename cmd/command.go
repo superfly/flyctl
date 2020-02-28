b9ef662a6799a835376955520432a0c5a80c69fe
Flaque <evanjamesconrad@gmail.com>
2020-02-27 16:21:52 -0800

    prompt user to create app if no app

--- cmd/command.go
+++ cmd/command.go
@@ -331,7 +331,7 @@ func requireAppName(cmd *Command) Initializer {
 		},
 		PreRun: func(ctx *CmdContext) error {
 			if ctx.AppName == "" {
-				return fmt.Errorf("No app specified")
+				return fmt.Errorf("No app specified. Maybe create one with 'flyctl apps create [APPNAME]'")
 			}
 
 			if ctx.AppConfig == nil {
