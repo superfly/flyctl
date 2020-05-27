c8b94f34c445cd7f4e1f94fbe99459a4d3d5aaa2
Dj Walker-Morgan <dj@codepope.com>
2020-05-27 11:30:44 +0100

    Tighten table presentation

--- cmd/presenters/presenter.go
+++ cmd/presenters/presenter.go
@@ -7,22 +7,26 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
+// Presentable - Records (and field names) which may be presented by a Presenter
 type Presentable interface {
 	FieldNames() []string
 	Records() []map[string]string
 }
 
+// Presenter - A self managing presenter which can be rendered in multiple ways
 type Presenter struct {
 	Item Presentable
 	Out  io.Writer
 	Opts Options
 }
 
+// Options - Presenter options
 type Options struct {
 	Vertical   bool
 	HideHeader bool
 }
 
+// Render - Renders a presenter as a field list or table
 func (p *Presenter) Render() error {
 	if p.Opts.Vertical {
 		return p.renderFieldList()
@@ -42,9 +46,12 @@ func (p *Presenter) renderTable() error {
 	table.SetBorder(false)
 	table.SetHeaderLine(false)
 	table.SetAutoWrapText(false)
+	table.SetAutoFormatHeaders(true)
 	table.SetAlignment(tablewriter.ALIGN_LEFT)
 	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
 	table.SetColumnSeparator(" ")
+	table.SetNoWhiteSpace(true)
+	table.SetTablePadding(" ") // pad with tabs
 
 	for _, kv := range p.Item.Records() {
 		fields := []string{}
