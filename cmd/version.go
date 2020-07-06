be1f7ae6d1f1f7d1b8a58431724ff1cec4170233
Dj Walker-Morgan <codepope@gmail.com>
2020-07-06 18:00:58 +0100

    Tighter output for version command, add --full

--- cmd/version.go
+++ cmd/version.go
@@ -15,11 +15,18 @@ import (
 func newVersionCommand() *Command {
 	versionStrings := docstrings.Get("version")
 	version := BuildCommand(nil, runVersion, versionStrings.Usage, versionStrings.Short, versionStrings.Long, os.Stdout)
+	version.AddBoolFlag(BoolFlagOpts{
+		Name:        "full",
+		Shorthand:   "f",
+		Description: "Show full version details",
+	})
+
 	version.AddStringFlag(StringFlagOpts{
 		Name:        "completions",
 		Shorthand:   "c",
 		Description: "Generate completions for supported shells bash/zsh)",
 	})
+
 	version.AddStringFlag(StringFlagOpts{
 		Name:        "saveinstall",
 		Shorthand:   "s",
@@ -55,15 +62,26 @@ func runVersion(ctx *cmdctx.CmdContext) error {
 		}
 	}
 
-	if ctx.OutputJSON() {
-		type flyctlBuild struct {
-			Version string
-			Commit  string
-			Build   string
+	if ctx.Config.GetBool("full") {
+		if ctx.OutputJSON() {
+			type flyctlBuild struct {
+				Version string
+				Commit  string
+				Build   string
+			}
+			ctx.WriteJSON(flyctlBuild{Version: flyctl.Version, Commit: flyctl.Commit, Build: flyctl.BuildDate})
+		} else {
+			fmt.Printf("flyctl %s %s %s\n", flyctl.Version, flyctl.Commit, flyctl.BuildDate)
 		}
-		ctx.WriteJSON(flyctlBuild{Version: flyctl.Version, Commit: flyctl.Commit, Build: flyctl.BuildDate})
 	} else {
-		fmt.Printf("flyctl %s %s %s\n", flyctl.Version, flyctl.Commit, flyctl.BuildDate)
+		if ctx.OutputJSON() {
+			type flyctlBuild struct {
+				Version string
+			}
+			ctx.WriteJSON(flyctlBuild{Version: flyctl.Version})
+		} else {
+			fmt.Printf("%s\n", flyctl.Version)
+		}
 	}
 	return nil
 }
