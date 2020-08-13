3cff3fb708dd0b6a3e75a1a1c78682ad609d6b46
Dj Walker-Morgan <codepope@gmail.com>
2020-08-13 13:47:28 +0100

    Added resource support for go

--- builtinsupport/builtinsupport.go
+++ builtinsupport/builtinsupport.go
@@ -93,19 +93,22 @@ CMD ["run", "--allow-net", "main.ts"]
 `},
 	{Name: "go",
 		Description: "Go Builtin",
-		Details:     `Builds app.go from the directory, the app should use go modules.`,
+		Details:     `Builds main.go from the directory, the app should use go modules.`,
 		FileText: `
-FROM golang:1.13 as builder
+FROM golang:1.14 as builder
 WORKDIR /go/src/app
 COPY . .
 RUN go mod download
 RUN CGO_ENABLED=0 GOOS=linux go build -v -o app
 FROM alpine:latest  
-COPY --from=builder /go/src/app/app /app
-COPY ./resources/ /resources/
+COPY --from=builder /go/src/app/app /goapp/app
+WORKDIR /goapp
+COPY . /throwaway
+RUN cp -r /throwaway/resources ./resources || echo "No resources to copy"
+RUN rm -rf /throwaway
 RUN apk --no-cache add ca-certificates
 EXPOSE 8080
-CMD ["/app"]
+CMD ["/goapp/app"]
 `},
 	{Name: "static",
 		Description: "Web server builtin",
