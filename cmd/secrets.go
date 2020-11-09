f12210583d906a6f40b41ef0ec065d9d8179dae2
Dj Walker-Morgan <codepope@gmail.com>
2020-11-09 09:48:51 +0000

    Added multiline (python style) support to import

--- cmd/secrets.go
+++ cmd/secrets.go
@@ -150,21 +150,53 @@ func runImportSecrets(cc *cmdctx.CmdContext) error {
 
 	secretsArray := strings.Split(string(secretsString), "\n")
 
-	for _, pair := range secretsArray {
-		if pair != "" {
-			parts := strings.SplitN(pair, "=", 2)
-			if len(parts) != 2 {
-				return fmt.Errorf("Secrets must be provided as NAME=VALUE pairs (%s is invalid)", pair)
+	parsestate := 0
+	parsedkey := ""
+	var parsebuffer strings.Builder
+
+	for _, line := range secretsArray {
+		switch parsestate {
+		case 0:
+			if line != "" {
+				parts := strings.SplitN(line, "=", 2)
+				if strings.HasPrefix(parts[1], "\"\"\"") {
+					// Switch to multiline
+					parsestate = 1
+					parsedkey = parts[0]
+					parsebuffer.WriteString(strings.TrimPrefix(parts[1], "\"\"\""))
+					parsebuffer.WriteString("\n")
+				} else {
+					if len(parts) != 2 {
+						return fmt.Errorf("Secrets must be provided as NAME=VALUE pairs (%s is invalid)", line)
+					}
+					key := parts[0]
+					value := parts[1]
+					secrets[key] = value
+				}
 			}
-			key := parts[0]
-			value := parts[1]
-			secrets[key] = value
+		case 1:
+			if strings.HasSuffix(line, "\"\"\"") {
+				// End of multiline
+				parsebuffer.WriteString(strings.TrimSuffix(line, "\"\"\""))
+				secrets[parsedkey] = parsebuffer.String()
+				parsebuffer.Reset()
+				parsestate = 0
+				parsedkey = ""
+			} else {
+				if line != "" {
+					parsebuffer.WriteString(line)
+				}
+				parsebuffer.WriteString("\n")
+			}
+
 		}
+
 	}
 
 	if len(secrets) < 1 {
 		return errors.New("requires at least one SECRET=VALUE pair")
 	}
+	fmt.Println(secrets)
 
 	release, err := cc.Client.API().SetSecrets(cc.AppName, secrets)
 	if err != nil {
