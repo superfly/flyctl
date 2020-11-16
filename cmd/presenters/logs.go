d0890104568f5d9e0599ff30e9e68c6b8639bf68
Dj Walker-Morgan <codepope@gmail.com>
2020-11-16 17:28:28 +0000

    Remove logs deduplicator

--- cmd/presenters/logs.go
+++ cmd/presenters/logs.go
@@ -14,7 +14,6 @@ type LogPresenter struct {
 	RemoveNewlines bool
 	HideRegion     bool
 	HideAllocID    bool
-	Lastline       string
 }
 
 func (lp *LogPresenter) FPrint(w io.Writer, asJSON bool, entries []api.LogEntry) {
@@ -29,21 +28,10 @@ var newline = []byte("\n")
 func (lp *LogPresenter) printEntry(w io.Writer, asJSON bool, entry api.LogEntry) {
 	if asJSON {
 		outBuf, _ := json.MarshalIndent(entry, "", "    ")
-		thisline := string(outBuf)
-		if thisline == lp.Lastline {
-			return
-		}
-		lp.Lastline = thisline
 		fmt.Fprintln(w, string(outBuf))
 		return
 	}
 
-	thisline := entry.Timestamp + entry.Instance + entry.Region + entry.Level + entry.Message
-	if thisline == lp.Lastline {
-		return
-	}
-
-	lp.Lastline = thisline
 	fmt.Fprintf(w, "%s ", aurora.Faint(entry.Timestamp))
 
 	if !lp.HideAllocID {
