18d75dedda8284cdb654d2a3e955883db8b44f54
Thomas H. Ptacek <thomas@sockpuppet.org>
2020-12-17 14:54:17 -0600

    restrict peer names

--- cmd/wireguard.go
+++ cmd/wireguard.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"regexp"
 	"text/template"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -167,9 +168,18 @@ func runWireGuardCreate(ctx *cmdctx.CmdContext) error {
 		return err
 	}
 
-	name, err := argOrPrompt(ctx, 2, "Name of WireGuard peer to add: ")
-	if err != nil {
-		return err
+	var name string
+	rx := regexp.MustCompile("^[a-zA-Z0-9\\-]+$")
+	
+	for !rx.MatchString(name) {
+   		if name != "" { 
+			fmt.Println("Name must consist solely of letters, numbers, and the dash character.")
+		}
+
+		name, err = argOrPrompt(ctx, 2, "New DNS name for WireGuard peer: ")
+		if err != nil {
+			return err
+		}
 	}
 
 	fmt.Printf("Creating WireGuard peer \"%s\" in region \"%s\" for organization %s\n", name, region, org.Slug)
