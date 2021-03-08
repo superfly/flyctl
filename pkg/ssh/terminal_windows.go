4f524640a6b82c78ee3da31b0b45be54454e3a7f
Jerome Gravel-Niquet <jeromegn@gmail.com>
2021-03-08 09:40:55 -0500

    fix: no ide support because this only builds on windows, manually adding imports

--- pkg/ssh/terminal_windows.go
+++ pkg/ssh/terminal_windows.go
@@ -3,6 +3,12 @@
 
 package ssh
 
+import (
+	"context"
+
+	"golang.org/x/crypto/ssh"
+)
+
 func watchWindowSize(ctx context.Context, fd int, sess *ssh.Session) error {
 	// TODO: SIGWINCH for windows?
 	return nil
