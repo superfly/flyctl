766c8388a4dbf1722ae02288c0c3143e08589f7f
Panagiotis Siatras <azazeal@users.noreply.github.com>
2021-10-21 11:05:12 +0300

    internal/env: added more helpers

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,13 +1,72 @@
 // Package env implements environment related functionality.
 package env
 
-import "os"
+import (
+	"os"
+	"strings"
+)
+
+// GetDef retrieves the value of the environment variable named by the key. In
+// case the variable is not present, GetOr returns def.
+func GetDef(key, def string) string {
+	if v, ok := os.LookupEnv(key); ok {
+		return v
+	}
+
+	return def
+}
+
+// Get is shorthand for GetDef(key, "").
+func Get(key string) string {
+	return GetDef(key, "")
+}
+
+// IsTruthy reports that the value of environment variable named by the key is
+// evaluates to true. IsTruthy always reports false for unset variables.
+func IsTruthy(key string) bool {
+	v, ok := os.LookupEnv(key)
+	if !ok {
+		return false
+	}
+
+	switch strings.ToLower(v) {
+	default:
+		return false
+	case "1", "ok", "t", "true":
+		return true
+	}
+}
+
+// IsSet reports whether the environment variable named by the key is set.
+func IsSet(key string) (is bool) {
+	_, is = os.LookupEnv(key)
+
+	return
+}
+
+// https://github.com/watson/ci-info/blob/c4f1553f254c78babef5c200c48569ede313b718/index.js
+var ciKeys = []string{
+	// Travis CI, CircleCI, Cirrus CI,
+	// Gitlab CI, Appveyor, CodeShip, dsari
+	"CI",
+
+	// Travis CI, Cirrus CI
+	"CONTINUOUS_INTEGRATION",
+
+	// Jenkins, TeamCity
+	"BUILD_NUMBER",
+
+	// TaskCluster, dsari
+	"RUN_ID",
+}
 
 // IsCI reports whether the environment is a CI one.
-//
-// Based on https://github.com/watson/ci-info/blob/HEAD/index.js
 func IsCI() bool {
-	return os.Getenv("CI") != "" || // GitHub Actions, Travis CI, CircleCI, Cirrus CI, GitLab CI, AppVeyor, CodeShip, dsari
-		os.Getenv("BUILD_NUMBER") != "" || // Jenkins, TeamCity
-		os.Getenv("RUN_ID") != "" // TaskCluster, dsari
+	for _, key := range ciKeys {
+		if IsSet(key) {
+			return true
+		}
+	}
+
+	return false
 }
