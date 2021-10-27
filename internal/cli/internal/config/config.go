58050502997c01acd1253c370e025147a195ecdc
Panagiotis Siatras <azazeal@users.noreply.github.com>
2021-10-27 14:42:14 +0300

    internal/cli: more generic flag binding from env

--- internal/cli/internal/config/config.go
+++ internal/cli/internal/config/config.go
@@ -19,6 +19,7 @@ const (
 	verboseOutputEnvKey = envKeyPrefix + "VERBOSE"
 	jsonOutputEnvKey    = envKeyPrefix + "JSON"
 	logGQLEnvKey        = envKeyPrefix + "LOG_GQL_ERRORS"
+	localOnlyEnvKey     = envKeyPrefix + "LOCAL_ONLY"
 
 	defaultAPIBaseURL   = "https://api.fly.io"
 	defaultRegistryHost = "registry.fly.io"
@@ -33,6 +34,7 @@ type Config struct {
 	JSONOutput    bool   `yaml:"-"`
 	LogGQLErrors  bool   `yaml:"-"`
 	Organization  string `yaml:"-"`
+	LocalOnly     bool   `yaml:"-"`
 }
 
 // New returns a new instance of Config populated with default values.
@@ -52,6 +54,7 @@ func (cfg *Config) ApplyEnv() {
 	cfg.VerboseOutput = env.IsTruthy(verboseOutputEnvKey) || cfg.VerboseOutput
 	cfg.JSONOutput = env.IsTruthy(jsonOutputEnvKey) || cfg.JSONOutput
 	cfg.LogGQLErrors = env.IsTruthy(logGQLEnvKey) || cfg.LogGQLErrors
+	cfg.LocalOnly = env.IsTruthy(localOnlyEnvKey) || cfg.LocalOnly
 
 	cfg.Organization = env.FirstOrDefault(cfg.Organization,
 		orgEnvKey, organizationEnvKey)
@@ -75,32 +78,42 @@ func (cfg *Config) ApplyFile(path string) (err error) {
 // ApplyFlags sets the properties of cfg which may be set via command line flags
 // to the values the flags of the given FlagSet may contain.
 func (cfg *Config) ApplyFlags(fs *pflag.FlagSet) {
-	applyStringFlag(&cfg.AccessToken, fs, flag.AccessTokenName)
-	applyBoolFlag(&cfg.VerboseOutput, fs, flag.VerboseName)
-	applyBoolFlag(&cfg.JSONOutput, fs, flag.JSONName)
-	applyStringFlag(&cfg.Organization, fs, flag.OrgName)
+	applyStringFlags(fs, map[string]*string{
+		flag.AccessTokenName: &cfg.AccessToken,
+		flag.OrgName:         &cfg.Organization,
+	})
+
+	applyBoolFlags(fs, map[string]*bool{
+		flag.VerboseName: &cfg.VerboseOutput,
+		flag.JSONName:    &cfg.JSONOutput,
+		localOnlyEnvKey:  &cfg.LocalOnly,
+	})
 }
 
-func applyStringFlag(dst *string, fs *pflag.FlagSet, name string) {
-	if !fs.Changed(name) {
-		return
-	}
+func applyStringFlags(fs *pflag.FlagSet, flags map[string]*string) {
+	for name, dst := range flags {
+		if !fs.Changed(name) {
+			return
+		}
 
-	if v, err := fs.GetString(name); err != nil {
-		panic(err)
-	} else {
-		*dst = v
+		if v, err := fs.GetString(name); err != nil {
+			panic(err)
+		} else {
+			*dst = v
+		}
 	}
 }
 
-func applyBoolFlag(dst *bool, fs *pflag.FlagSet, name string) {
-	if !fs.Changed(name) {
-		return
-	}
+func applyBoolFlags(fs *pflag.FlagSet, flags map[string]*bool) {
+	for name, dst := range flags {
+		if !fs.Changed(name) {
+			return
+		}
 
-	if v, err := fs.GetBool(name); err != nil {
-		panic(err)
-	} else {
-		*dst = v
+		if v, err := fs.GetBool(name); err != nil {
+			panic(err)
+		} else {
+			*dst = v
+		}
 	}
 }
