a3bcc2fcde93c830c70c6d2daaf84b662d45c322
Panagiotis Siatras <azazeal@users.noreply.github.com>
2021-12-09 11:57:40 +0200

    internal/cli_test: initial implementation of the package

---
+++ internal/cli_test/cli_test.go
@@ -0,0 +1,44 @@
+package cli_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/superfly/flyctl/pkg/iostreams"
+
+	"github.com/superfly/flyctl/internal/cli"
+)
+
+func TestVersion(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	stdout, stderr, code := capture(ctx, t, "version")
+	assert.Equal(t, 0, code)
+
+	assert.Equal(t, 0, code)
+	assert.NotEmpty(t, stdout)
+	assert.Empty(t, stderr)
+}
+
+func capture(ctx context.Context, t *testing.T, args ...string) (stdout, stderr string, code int) {
+	t.Helper()
+
+	var o1, o2 strings.Builder
+
+	io := iostreams.IOStreams{
+		In:     nil,
+		Out:    &o1,
+		ErrOut: &o2,
+	}
+
+	code = cli.Run(ctx, &io, args...)
+	stdout = o1.String()
+	stderr = o2.String()
+
+	return
+}
