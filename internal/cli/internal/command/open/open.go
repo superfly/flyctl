3245ac39fcf726cdc471d7e18fa26540d42467df
Panagiotis Siatras <azazeal@users.noreply.github.com>
2021-12-28 14:55:34 +0200

    do not duplicate code for apps open

--- internal/cli/internal/command/open/open.go
+++ internal/cli/internal/command/open/open.go
@@ -3,29 +3,10 @@ package open
 import (
 	"github.com/spf13/cobra"
 
-	"github.com/superfly/flyctl/internal/cli/internal/command"
 	"github.com/superfly/flyctl/internal/cli/internal/command/apps"
-	"github.com/superfly/flyctl/internal/cli/internal/flag"
 )
 
+// TODO: deprecate
 func New() *cobra.Command {
-	const (
-		long = `Open browser to current deployed application. If an optional path is specified, this is appended to the
-URL for deployed application
-`
-		short = "Open browser to current deployed application"
-
-		usage = "open [RELATIVE_URI]"
-	)
-
-	cmd := command.New(usage, short, long, apps.RunOpen, command.RequireSession, command.RequireAppName)
-
-	cmd.Args = cobra.MaximumNArgs(1)
-
-	flag.Add(cmd,
-		flag.App(),
-		flag.AppConfig(),
-	)
-
-	return cmd
+	return apps.NewOpen()
 }
