0621b5dc39fbb9d56532cbadddd00a9da64bb071
Shaun Davis <davissp14@Shauns-MacBook-Pro.local>
2022-01-14 15:26:43 -0600

    Id -> ID

--- internal/cli/internal/command/services/postgres/util.go
+++ internal/cli/internal/command/services/postgres/util.go
@@ -6,40 +6,41 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/azazeal/pause"
 	"github.com/superfly/flyctl/internal/client"
+	"github.com/superfly/flyctl/internal/logger"
 	"github.com/superfly/flyctl/pkg/iostreams"
 )
 
-func waitForMachineState(ctx context.Context, client *client.Client, appId, machineID, state string) error {
-	io := iostreams.FromContext(ctx)
+func waitForMachineState(parent context.Context, client *client.Client, appID, machineID, state string) error {
+	io := iostreams.FromContext(parent)
 	fmt.Fprintf(io.Out, "Waiting for machine %q to reach a healthy state...\n", machineID)
 
-	timeout := time.After(2 * time.Minute)
-	tick := time.Tick(1 * time.Second)
-	for {
-		select {
-		case <-timeout:
-			return fmt.Errorf("Timed out waiting for machine %s to reach state %s.\n", machineID, state)
-		case <-tick:
-			// TODO - Target specific machine rather than listing all.
-			machines, err := client.API().ListMachines(ctx, appId, "")
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-			if err == nil {
-				for _, machine := range machines {
-					if machine.ID == machineID {
-						if machine.State == state {
-							return nil
-						}
-						fmt.Fprintf(io.Out, "Machine state: %q, wanted: %q\n", machine.State, state)
+	logger := logger.FromContext(parent)
 
-					}
+	ctx, cancel := context.WithTimeout(parent, time.Duration(time.Second*2))
+	defer cancel()
+
+	for ctx.Err() == nil {
+		pause.For(ctx, time.Second)
+
+		machines, err := client.API().ListMachines(ctx, appID, "")
+		if err != nil {
+			logger.Debugf("failed retrieving machines: %v", err)
+
+			continue
+		}
+
+		for _, machine := range machines {
+			if machine.ID == machineID {
+				if machine.State == state {
+					return nil
 				}
+				fmt.Fprintf(io.Out, "Machine state: %q, wanted: %q\n", machine.State, state)
 			}
-			time.Sleep(5 * time.Second)
 		}
 	}
+	return ctx.Err()
 }
 
 // encodeCommand will base64 encode a command string so it can be passed
