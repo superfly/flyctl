c9ce972b155e96ab5fb9d0676101295db3c2744c
Shaun Davis <davissp14@Shauns-MacBook-Pro-2.local>
2022-02-01 16:42:51 -0600

    Remove local hack

--- internal/cli/internal/command/services/postgres/launch.go
+++ internal/cli/internal/command/services/postgres/launch.go
@@ -122,12 +122,10 @@ func runLaunch(ctx context.Context) error {
 
 	client := client.FromContext(ctx).API()
 
-	// imageRef, err := client.GetLatestImageTag(ctx, "flyio/postgres")
-	// if err != nil {
-	// 	return err
-	// }
-
-	imageRef := "registry.fly.io/shaun-pg-flyctl:deployment-1643752852"
+	imageRef, err := client.GetLatestImageTag(ctx, "flyio/postgres")
+	if err != nil {
+		return err
+	}
 
 	config := LaunchConfig{
 		AppName:            name,
