1f57b71180aac6d4d045437bd854043f0f38cd59
Panagiotis Siatras <azazeal@users.noreply.github.com>
2022-02-03 14:45:55 +0200

    use windows.DETACHED_PROCESS when starting the agent on windows

--- pkg/agent/cmd_windows.go
+++ pkg/agent/cmd_windows.go
@@ -6,11 +6,13 @@ package agent
 import (
 	"os/exec"
 	"syscall"
+
+	"golang.org/x/sys/windows"
 )
 
 func setCommandFlags(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		HideWindow:    true,
-		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+		CreationFlags: windows.DETACHED_PROCESS | windows.CREATE_NEW_PROCESS_GROUP,
 	}
 }
