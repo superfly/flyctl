e606ca3929cdabaad35a8a63f5789baa16ed164b
Joshua Sierles <joshua@hey.com>
2022-02-04 20:10:20 +0100

    move scanner helpers to a separate file

--- internal/sourcecode/scan.go
+++ internal/sourcecode/scan.go
@@ -1,11 +1,9 @@
 package sourcecode
 
 import (
-	"bufio"
 	"embed"
 	"io/fs"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"regexp"
 
@@ -98,72 +96,7 @@ func Scan(sourceDir string) (*SourceInfo, error) {
 	return nil, nil
 }
 
-func SuggestAppName(sourceDir string) string {
-	return filepath.Base(sourceDir)
-}
-
 type sourceScanner func(sourceDir string) (*SourceInfo, error)
-
-func fileExists(filenames ...string) checkFn {
-	return func(dir string) bool {
-		for _, filename := range filenames {
-			info, err := os.Stat(filepath.Join(dir, filename))
-			if err != nil {
-				continue
-			}
-			if !info.IsDir() {
-				return true
-			}
-		}
-		return false
-	}
-}
-
-func fileContains(path string, pattern string) bool {
-	file, err := os.Open(path)
-
-	if err != nil {
-		return false
-	}
-
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		re := regexp.MustCompile(pattern)
-		if re.MatchString(scanner.Text()) {
-			return true
-		}
-	}
-
-	return false
-}
-
-func dirContains(glob string, patterns ...string) checkFn {
-	return func(dir string) bool {
-		for _, pattern := range patterns {
-			filenames, _ := filepath.Glob(filepath.Join(dir, glob))
-			for _, filename := range filenames {
-				if fileContains(filename, pattern) {
-					return true
-				}
-			}
-		}
-		return false
-	}
-}
-
-type checkFn func(dir string) bool
-
-func checksPass(sourceDir string, checks ...checkFn) bool {
-	for _, check := range checks {
-		if check(sourceDir) {
-			return true
-		}
-	}
-	return false
-}
 
 func configureDockerfile(sourceDir string) (*SourceInfo, error) {
 	if !checksPass(sourceDir, fileExists("Dockerfile")) {
