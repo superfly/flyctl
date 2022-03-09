2091b0de73e64020c3ceb34b26dd7c3efcad6914
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2022-03-09 16:32:28 +0200

    Port machine start

--- internal/cli/internal/command/machine/start.go
+++ internal/cli/internal/command/machine/start.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/superfly/flyctl/api"
+	"github.com/superfly/flyctl/internal/cli/internal/app"
 	"github.com/superfly/flyctl/internal/cli/internal/command"
+	"github.com/superfly/flyctl/internal/cli/internal/flag"
+	"github.com/superfly/flyctl/internal/client"
+	"github.com/superfly/flyctl/pkg/iostreams"
 )
 
 func newStart() *cobra.Command {
@@ -21,11 +26,30 @@ func newStart() *cobra.Command {
 		command.RequireAppName,
 	)
 
-	cmd.Args = cobra.MaximumNArgs(1)
+	cmd.Args = cobra.ExactArgs(1)
 
 	return cmd
 }
 
-func runMachineStart(ctx context.Context) error {
-	return fmt.Errorf("not implemented")
+func runMachineStart(ctx context.Context) (err error) {
+	var (
+		out     = iostreams.FromContext(ctx).Out
+		appName = app.NameFromContext(ctx)
+		id      = flag.FirstArg(ctx)
+		client  = client.FromContext(ctx).API()
+	)
+
+	input := api.StartMachineInput{
+		AppID: appName,
+		ID:    id,
+	}
+
+	machine, err := client.StartMachine(ctx, input)
+	if err != nil {
+		return fmt.Errorf("could not start machine %s: %w", id, err)
+	}
+
+	fmt.Fprintf(out, "%s\n", machine.ID)
+
+	return
 }
