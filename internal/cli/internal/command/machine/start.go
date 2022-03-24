9e57d66c090d93f187d50291cd3deeab3e93b999
divya <hello@shortdiv.com>
2022-03-23 18:25:21 -0600

    fix: Add flaps to start

--- internal/cli/internal/command/machine/start.go
+++ internal/cli/internal/command/machine/start.go
@@ -2,14 +2,16 @@ package machine
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
-	"github.com/superfly/flyctl/api"
 	"github.com/superfly/flyctl/internal/cli/internal/app"
 	"github.com/superfly/flyctl/internal/cli/internal/command"
 	"github.com/superfly/flyctl/internal/client"
 	"github.com/superfly/flyctl/internal/flag"
+	"github.com/superfly/flyctl/pkg/flaps"
 	"github.com/superfly/flyctl/pkg/iostreams"
 )
 
@@ -45,17 +47,38 @@ func runMachineStart(ctx context.Context) (err error) {
 		client  = client.FromContext(ctx).API()
 	)
 
-	input := api.StartMachineInput{
-		AppID: appName,
-		ID:    id,
+	if appName == "" {
+		return errors.New("app is not found")
+	}
+	app, err := client.GetApp(ctx, appName)
+	if err != nil {
+		return err
 	}
 
-	machine, err := client.StartMachine(ctx, input)
+	//machine, err := client.StartMachine(ctx, input)
+	flapsClient, err := flaps.New(ctx, app)
+	if err != nil {
+		return fmt.Errorf("could not make flaps client: %w", err)
+	}
+
+	machineID := flag.FirstArg(ctx)
+	if machineID == "" {
+		return errors.New("no machine ID defined")
+	}
+
+	mach, err := flapsClient.Start(ctx, machineID)
 	if err != nil {
 		return fmt.Errorf("could not start machine %s: %w", id, err)
 	}
 
-	fmt.Fprintf(out, "%s\n", machine.ID)
+	type body map[string]interface{}
+	var t body
+
+	if err := json.Unmarshal(mach, &t); err != nil {
+		return fmt.Errorf("machine could not be started %s", err)
+	}
+
+	fmt.Fprintf(out, "%s\n", t)
 
 	return
 }
