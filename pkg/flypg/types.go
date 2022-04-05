14d6f87cbf9ff6329aeb4ae34f37540a00df2ef8
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2022-04-05 14:37:38 +0200

    Use the http fly pg client for postgres commands

--- pkg/flypg/types.go
+++ pkg/flypg/types.go
@@ -1,5 +1,13 @@
 package flypg
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 type DatabaseListResponse struct {
 	Result []PostgresDatabase
 }
@@ -58,4 +66,44 @@ type FindDatabaseResponse struct {
 
 type FindUserResponse struct {
 	Result PostgresUser
+}
+
+type Error struct {
+	StatusCode int
+	Err        string `json:"error"`
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("%d: %s", e.StatusCode, e.Err)
+}
+
+func ErrorStatus(err error) int {
+	var e *Error
+
+	if errors.As(err, &e) {
+		return e.StatusCode
+	}
+	return http.StatusInternalServerError
+}
+
+func newError(status int, res *http.Response) error {
+	var e = new(Error)
+
+	e.StatusCode = status
+
+	switch res.Header.Get("Content-Type") {
+	case "application/json":
+
+		if err := json.NewDecoder(res.Body).Decode(e); err != nil {
+			return err
+		}
+	default:
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		e.Err = string(b)
+	}
+
+	return e
 }
