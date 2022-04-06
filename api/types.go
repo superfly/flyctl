cf1d2915be59cda86c4eedbb26ff5e1d857f46e6
Shaun Davis <davissp14@Shauns-MacBook-Pro-2.local>
2022-04-06 10:38:19 -0500

    PG Launch is now using the flaps API

--- api/types.go
+++ api/types.go
@@ -1260,7 +1260,7 @@ type RemoveMachineInput struct {
 type MachineRestartPolicy string
 
 var MachineRestartPolicyNo MachineRestartPolicy = "no"
-var MachineRestartPolicyOnFailure MachineRestartPolicy = "on-restart"
+var MachineRestartPolicyOnFailure MachineRestartPolicy = "on-failure"
 var MachineRestartPolicyAlways MachineRestartPolicy = "always"
 
 type MachineRestart struct {
