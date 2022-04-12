99a83a4ef518a471b644df14823093bf9ebf7026
Joshua Sierles <joshua@fly.io>
2022-04-12 22:56:39 +0200

    Add a nix shell for development

---
+++ shell.nix
@@ -0,0 +1,19 @@
+with (import (fetchTarball https://github.com/nixos/nixpkgs/archive/592dc9ed7f049c565e9d7c04a4907e57ae17e2d9.tar.gz) {});
+
+let
+
+ basePackages = [
+  go
+  gnumake
+  gnused
+  ];
+
+#  inputs = basePackages
+#    ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+#        Security
+#      ]);
+
+in mkShell {
+  buildInputs = basePackages;
+
+}
