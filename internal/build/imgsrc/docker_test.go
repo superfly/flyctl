4e28b7fc364286304c47ac655c161d787a7861ea
Daniel Graña <dangra@gmail.com>
2022-07-20 11:09:14 -0300

    Prefer remote builder for deployments
    This reverts commit 691004cffa5394f21dd6823c7537e35e3d4db8ee.

--- internal/build/imgsrc/docker_test.go
+++ internal/build/imgsrc/docker_test.go
@@ -8,18 +8,23 @@ import (
 
 func TestAllowedDockerDaemonMode(t *testing.T) {
 	tests := []struct {
-		allowLocal  bool
-		allowRemote bool
-		expected    DockerDaemonType
+		allowLocal   bool
+		allowRemote  bool
+		preferslocal bool
+		expected     DockerDaemonType
 	}{
-		{true, true, DockerDaemonTypeNone | DockerDaemonTypeLocal | DockerDaemonTypeRemote},
-		{false, true, DockerDaemonTypeNone | DockerDaemonTypeRemote},
-		{true, false, DockerDaemonTypeNone | DockerDaemonTypeLocal},
-		{false, false, DockerDaemonTypeNone},
+		{false, false, false, DockerDaemonTypeNone},
+		{false, false, true, DockerDaemonTypeNone | DockerDaemonTypePrefersLocal},
+		{false, true, false, DockerDaemonTypeNone | DockerDaemonTypeRemote},
+		{false, true, true, DockerDaemonTypeNone | DockerDaemonTypeRemote | DockerDaemonTypePrefersLocal},
+		{true, false, false, DockerDaemonTypeNone | DockerDaemonTypeLocal},
+		{true, false, true, DockerDaemonTypeNone | DockerDaemonTypeLocal | DockerDaemonTypePrefersLocal},
+		{true, true, false, DockerDaemonTypeNone | DockerDaemonTypeLocal | DockerDaemonTypeRemote},
+		{true, true, true, DockerDaemonTypeNone | DockerDaemonTypeLocal | DockerDaemonTypeRemote | DockerDaemonTypePrefersLocal},
 	}
 
 	for _, test := range tests {
-		m := NewDockerDaemonType(test.allowLocal, test.allowRemote)
+		m := NewDockerDaemonType(test.allowLocal, test.allowRemote, test.preferslocal)
 		assert.Equal(t, test.expected, m)
 	}
 }
