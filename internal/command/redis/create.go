3195aca9dc3d76f11243b488e9e32b56ca05f0ef
Joshua Sierles <joshua@fly.io>
2022-07-22 17:22:41 +0200

    launch with the latest tracked Redis image

--- internal/command/redis/create.go
+++ internal/command/redis/create.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/superfly/flyctl/api"
+	"github.com/superfly/flyctl/flaps"
 	"github.com/superfly/flyctl/iostreams"
 
 	"github.com/superfly/flyctl/client"
@@ -33,7 +34,8 @@ func newCreate() (cmd *cobra.Command) {
 
 func runCreate(ctx context.Context) (err error) {
 	var (
-		io = iostreams.FromContext(ctx)
+		io     = iostreams.FromContext(ctx)
+		client = client.FromContext(ctx).API()
 	)
 
 	org, err := prompt.Org(ctx)
@@ -42,14 +44,37 @@ func runCreate(ctx context.Context) (err error) {
 		return
 	}
 
-	input := api.CreateAppInput{
+	appInput := api.CreateAppInput{
 		OrganizationID: org.ID,
 		Machines:       true,
+		AppRoleID:      "redis",
 	}
 
-	app, err := client.FromContext(ctx).
-		API().
-		CreateApp(ctx, input)
+	app, err := client.CreateApp(ctx, appInput)
 
+	flapsClient, err := flaps.New(ctx, &api.AppCompact{
+		ID:   app.ID,
+		Name: app.Name,
+		Organization: &api.OrganizationBasic{
+			ID:   app.Organization.ID,
+			Slug: app.Organization.Slug,
+		},
+	})
+
+	imageRef, err := client.GetLatestImageTag(ctx, "flyio/redis")
+
+	if err != nil {
+		return err
+	}
+
+	launchInput := api.LaunchMachineInput{
+		AppID:   app.Name,
+		OrgSlug: app.Organization.ID,
+		Config: &api.MachineConfig{
+			Image: imageRef,
+		},
+	}
+	client.SetSecrets(ctx, app.Name, secrets)
+	flapsClient.Launch(ctx, launchInput)
 	return err
 }
