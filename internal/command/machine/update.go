1ae8c68e0f75a761099b07f99973c921b702e3a4
Joshua Sierles <joshua@fly.io>
2022-07-26 13:45:01 +0200

    allow machine run/update to share sets of CLI flags

--- internal/command/machine/update.go
+++ internal/command/machine/update.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/google/shlex"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	"github.com/superfly/flyctl/api"
@@ -32,99 +30,7 @@ func newUpdate() *cobra.Command {
 
 	flag.Add(
 		cmd,
-		flag.App(),
-		flag.Region(),
-		flag.AppConfig(),
-		flag.String{
-			Name:        "name",
-			Shorthand:   "n",
-			Description: "Machine name, will be generated if missing",
-		},
-		flag.StringSlice{
-			Name:        "port",
-			Shorthand:   "p",
-			Description: "Exposed port mappings (format: edgePort[:machinePort]/[protocol[:handler]])",
-		},
-		flag.Int{
-			Name:        "cpus",
-			Description: "Number of CPUs",
-			Hidden:      true,
-		},
-		flag.Int{
-			Name:        "memory",
-			Description: "Memory (in megabytes) to attribute to the machine",
-			Hidden:      true,
-		},
-		flag.StringSlice{
-			Name:        "env",
-			Shorthand:   "e",
-			Description: "Set of environment variables in the form of NAME=VALUE pairs. Can be specified multiple times.",
-		},
-		flag.StringSlice{
-			Name:        "volume",
-			Shorthand:   "v",
-			Description: "Volumes to mount in the form of <volume_id_or_name>:/path/inside/machine[:<options>]",
-			Hidden:      true,
-		},
-		flag.String{
-			Name:        "entrypoint",
-			Description: "ENTRYPOINT replacement",
-			Hidden:      true,
-		},
-		flag.String{
-			Name:        "image",
-			Shorthand:   "i",
-			Description: "Docker Image to update the machine with",
-		},
-		flag.Bool{
-			Name:        "build-only",
-			Description: "Only build the image without running the machine",
-			Hidden:      true,
-		},
-		flag.Bool{
-			Name:        "build-remote-only",
-			Description: "Perform builds remotely without using the local docker daemon",
-			Hidden:      true,
-		},
-		flag.Bool{
-			Name:        "build-local-only",
-			Description: "Only perform builds locally using the local docker daemon",
-			Hidden:      true,
-		},
-		flag.String{
-			Name:        "dockerfile",
-			Description: "Path to a Dockerfile. Defaults to the Dockerfile in the working directory.",
-		},
-		flag.StringSlice{
-			Name:        "build-arg",
-			Description: "Set of build time variables in the form of NAME=VALUE pairs. Can be specified multiple times.",
-			Hidden:      true,
-		},
-		flag.String{
-			Name:        "image-label",
-			Description: "Image label to use when tagging and pushing to the fly registry. Defaults to \"deployment-{timestamp}\".",
-			Hidden:      true,
-		},
-		flag.String{
-			Name:        "build-target",
-			Description: "Set the target build stage to build if the Dockerfile has more than one stage",
-			Hidden:      true,
-		},
-		flag.Bool{
-			Name:        "no-build-cache",
-			Description: "Do not use the cache when building the image",
-			Hidden:      true,
-		},
-		flag.StringSlice{
-			Name:        "kernel-arg",
-			Description: "List of kernel arguments to be provided to the init. Can be specified multiple times.",
-			Hidden:      true,
-		},
-		flag.StringSlice{
-			Name:        "metadata",
-			Shorthand:   "m",
-			Description: "Metadata in the form of NAME=VALUE pairs. Can be specified multiple times.",
-		},
+		sharedFlags,
 	)
 
 	cmd.Args = cobra.ExactArgs(1)
@@ -167,58 +73,10 @@ func runUpdate(ctx context.Context) (err error) {
 
 	machineConf := *machine.Config
 
-	if cpus := flag.GetInt(ctx, "cpus"); cpus != 0 {
-		machineConf.Guest.CPUs = cpus
-	}
+	machineConf, err = determineMachineConfig(ctx, machineConf, app, machine.Config.Image)
 
-	if memory := flag.GetInt(ctx, "memory"); memory != 0 {
-		machineConf.Guest.MemoryMB = memory
-	}
-
-	machineConf.Env, err = parseEnvVars(ctx)
 	if err != nil {
-		return err
-	}
-
-	services, err := determineServices(ctx)
-	if err != nil {
-		return err
-	}
-	if len(services) > 0 {
-		machineConf.Services = services
-	}
-
-	if entrypoint := flag.GetString(ctx, "entrypoint"); entrypoint != "" {
-		splitted, err := shlex.Split(entrypoint)
-		if err != nil {
-			return errors.Wrap(err, "invalid entrypoint")
-		}
-		machineConf.Init.Entrypoint = splitted
-	}
-
-	if cmd := flag.Args(ctx)[1:]; len(cmd) > 0 {
-		machineConf.Init.Cmd = cmd
-	}
-
-	machineConf.Mounts, err = determineMounts(ctx)
-	if err != nil {
-		return err
-	}
-
-	image := machine.FullImageRef()
-
-	if flag.GetString(ctx, "image") != "" {
-		image = flag.GetString(ctx, "image")
-	}
-
-	img, err := determineImage(ctx, app.Name, image)
-	if err != nil {
-		return err
-	}
-	machineConf.Image = img.Tag
-
-	if flag.GetBool(ctx, "build-only") {
-		return nil
+		return
 	}
 
 	input.Config = &machineConf
