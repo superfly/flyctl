07bd3fd6380737086afef9d4bd23aa11b176528d
Chris Fidao <fideloper@gmail.com>
2022-08-11 08:10:30 -0500

    removed commented out code

--- scanner/templates/laravel/common/docker/entrypoint.sh
+++ scanner/templates/laravel/common/docker/entrypoint.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env sh
 
-# Start Octane with the roadrunner binary if we find
-# that dependency in the composer.json file
-# This is a noop if octane is not used
-
-# TODO: Adjust for our new containers
-
-#if [ -f /var/www/html/composer.json ]; then
-#  if grep -Fq "spiral/roadrunner" /var/www/html/composer.json
-#  then
-#      sed -i 's/;rr command/command/g' /etc/supervisord.conf
-#  else
-#      sed -i 's/;swoole command/command/g' /etc/supervisord.conf
-#  fi
-#fi
-
 if [ $# -gt 0 ];then
     # If we passed a command, run it as root
     exec "$@"
