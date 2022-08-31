634fc127c9ec1614b8144a5e3c3920055a9e11e9
Sam Ruby <rubys@intertwingly.net>
2022-08-30 20:01:11 -0400

    address review comment by jsierles

--- scanner/rails.go
+++ scanner/rails.go
@@ -1,7 +1,6 @@
 package scanner
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -115,37 +114,6 @@ func configureRails(sourceDir string) (*SourceInfo, error) {
 				Value: string(masterKey),
 			},
 		}
-	}
-
-	rake := strings.TrimSpace(`
-# commands used to deploy a Rails application
-namespace :fly do
-  # BUILD step:
-  #  - changes to the fs make here DO get deployed
-  #  - NO access to secrets, volumes, databases
-  #  - Failures here prevent deployment
-  task :build => 'assets:precompile'
-
-  # RELEASE step:
-  #  - changes to the fs make here are DISCARDED
-  #  - full access to secrets, databases
-  #  - failures here prevent deployment
-  task :release => 'db:migrate'
-
-  # SERVER step:
-  #  - changes to the fs make here are deployed
-  #  - full access to secrets, databases
-  #  - failures here result in VM being stated, shutdown, and rolled back
-  #    to last successful deploy (if any).
-  task :server do
-    sh 'bin/rails server'
-  end
-end
-`)
-
-	_, err = os.Stat("lib/tasks/fly.rake")
-	if errors.Is(err, os.ErrNotExist) {
-		os.WriteFile("lib/tasks/fly.rake", []byte(rake), 0o600)
 	}
 
 	s.SkipDeploy = true
