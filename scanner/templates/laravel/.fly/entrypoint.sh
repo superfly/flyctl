1041dbfd3e33639483df47d049b87e5753448008
Chris Fidao <fideloper@gmail.com>
2022-09-20 13:14:57 -0500

    entrypoint always runs user scripts

--- scanner/templates/laravel/.fly/entrypoint.sh
+++ scanner/templates/laravel/.fly/entrypoint.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env sh
 
-if [ $# -gt 0 ];then
+# Run user scripts, if they exist
+for f in /var/www/html/.fly/scripts/*.sh; do
+    # Bail out this loop if any script exits with non-zero status code
+    bash "$f" || break
+done
+chown -R webuser:webgroup /var/www/html
+
+if [ $# -gt 0 ]; then
     # If we passed a command, run it as root
     exec "$@"
 else
-    # Otherwise start the web server
-
-    ## Prepare Laravel caches
-    /usr/bin/php /var/www/html/artisan config:cache --no-ansi -q
-    /usr/bin/php /var/www/html/artisan route:cache --no-ansi -q
-    /usr/bin/php /var/www/html/artisan view:cache --no-ansi -q
-    chown -R webuser:webgroup /var/www/html
-
-    ## Run user scripts, if they exist
-    for f in /var/www/html/.fly/scripts/*.sh; do
-        # Bail out if any script exits with non-zero status code
-        bash "$f" || break
-    done
-
-
     exec /init
 fi
