f5a4583c98c0cc639aa67f9ec84cfc628356ca3b
Thomas H. Ptacek <thomas@sockpuppet.org>
2022-10-04 21:34:46 -0500

    download directory trees

--- internal/command/ssh/sftp.go
+++ internal/command/ssh/sftp.go
@@ -1,6 +1,7 @@
 package ssh
 
 import (
+	"archive/zip"
 	"context"
 	"fmt"
 	"io"
@@ -136,6 +137,205 @@ var completer = readline.NewPrefixCompleter(
 	readline.PcItem("get"),
 )
 
+type sftpContext struct {
+	ftp *sftp.Client
+	wd  string
+	out func(string, ...interface{})
+}
+
+func (sc *sftpContext) cd(args ...string) error {
+	if len(args) < 2 {
+		sc.wd = "/"
+		return nil
+	}
+
+	dir := args[1]
+	if dir[0] != '/' {
+		dir = sc.wd + dir
+	}
+
+	if !strings.HasSuffix(dir, "/") {
+		dir = dir + "/"
+	}
+
+	inf, err := sc.ftp.Stat(dir)
+	if err != nil {
+		sc.out("cd %s: %s", dir, err)
+		return nil
+	}
+
+	if !inf.IsDir() {
+		sc.out("cd %s: not a directory", dir)
+		return nil
+	}
+
+	dir = path.Clean(dir) + "/"
+
+	sc.out("[%s]", dir)
+	sc.wd = dir
+
+	return nil
+}
+
+func (sc *sftpContext) ls(args ...string) error {
+	rpath := sc.wd
+
+	if len(args) > 2 {
+		if args[1][0] == '/' {
+			rpath = args[1]
+		} else {
+			rpath = sc.wd + rpath
+		}
+	}
+
+	files, err := sc.ftp.ReadDir(rpath)
+	if err != nil {
+		sc.out("ls: %s", err)
+		return nil
+	}
+
+	for _, f := range files {
+		tl := ""
+		if f.IsDir() {
+			tl = "/"
+		}
+
+		sc.out("%s%s\n", f.Name(), tl)
+	}
+
+	return nil
+}
+
+func (sc *sftpContext) getDir(rpath string, args []string) {
+	lpath := path.Base(rpath) + ".zip"
+
+	if len(args) > 2 {
+		lpath = args[2]
+
+		if !strings.HasSuffix(lpath, ".zip") {
+			lpath += ".zip"
+		}
+	}
+
+	if _, err := os.Stat(lpath); err == nil {
+		sc.out("get %s -> %s: file exists", rpath, lpath)
+		return
+	}
+
+	f, err := os.OpenFile(lpath, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		sc.out("get %s -> %s: %s", rpath, lpath, err)
+		return
+	}
+	defer f.Close()
+	z := zip.NewWriter(f)
+
+	defer z.Close()
+
+	walker := sc.ftp.Walk(rpath)
+
+	for walker.Step() {
+		if err = walker.Err(); err != nil {
+			sc.out("get %s -> %s: walk: %s", rpath, lpath, err)
+			break
+		}
+
+		rfpath := walker.Path()
+
+		inf, err := sc.ftp.Stat(rfpath)
+		if err != nil {
+			sc.out("get %s -> %s: stat %s: %s", rpath, lpath, rfpath, err)
+			continue
+		}
+
+		if inf.IsDir() {
+			continue
+		}
+
+		rf, err := sc.ftp.Open(rfpath)
+		if err != nil {
+			sc.out("get %s -> %s: open %s: %s", rpath, lpath, rfpath, err)
+			continue
+		}
+
+		zf, err := z.Create(rfpath)
+		if err != nil {
+			rf.Close()
+			sc.out("get %s -> %s: write %s: %s", rpath, lpath, rfpath, err)
+			continue
+		}
+
+		bytes, err := rf.WriteTo(zf)
+		if err != nil {
+			sc.out("get %s -> %s: write %s: %s (wrote %d bytes)", rpath, lpath, rfpath, err, bytes)
+		} else {
+			sc.out("%s (%d bytes)", rfpath, bytes)
+		}
+
+		rf.Close()
+	}
+
+	z.Close()
+}
+
+func (sc *sftpContext) get(args ...string) error {
+	if len(args) < 2 {
+		sc.out("get <filename>")
+		return nil
+	}
+
+	rpath := sc.wd + args[1]
+
+	inf, err := sc.ftp.Stat(rpath)
+	if err != nil {
+		sc.out("get %s: %s", rpath, err)
+		return nil
+	}
+
+	if inf.IsDir() {
+		sc.getDir(rpath, args)
+		return nil
+	}
+
+	localFile := path.Base(rpath)
+	if len(args) > 2 {
+		localFile = args[2]
+	}
+
+	_, err = os.Stat(localFile)
+	if err == nil {
+		sc.out("get %s -> %s: file exists", rpath, localFile)
+		return nil
+	}
+
+	func() {
+		rf, err := sc.ftp.Open(rpath)
+		if err != nil {
+			sc.out("get %s -> %s: %s", err)
+			return
+		}
+		defer rf.Close()
+
+		f, err := os.OpenFile(localFile, os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			sc.out("get %s -> %s: %s", rpath, localFile, err)
+			return
+		}
+		defer f.Close()
+
+		sc.out("get %s -> %s", rpath, localFile)
+
+		bytes, err := rf.WriteTo(f)
+		if err != nil {
+			sc.out("get %s -> %s: %s (wrote %d bytes)", rpath, localFile, err, bytes)
+		} else {
+			sc.out("wrote %d bytes", bytes)
+		}
+	}()
+
+	return nil
+}
+
 func runShell(ctx context.Context) error {
 	ftp, err := newSFTP(ctx)
 	if err != nil {
@@ -158,12 +358,14 @@ func runShell(ctx context.Context) error {
 	defer l.Close()
 	l.CaptureExitSignal()
 
-	_ = ftp
-
-	wd := "/"
-
 	out := func(format string, args ...interface{}) {
 		fmt.Printf(format+"\n", args...)
+	}
+
+	sc := &sftpContext{
+		wd:  "/",
+		out: out,
+		ftp: ftp,
 	}
 
 	for {
@@ -178,112 +380,31 @@ func runShell(ctx context.Context) error {
 			break
 		}
 
-		line = strings.TrimSpace(line)
+		args, err := shlex.Split(strings.TrimSpace(line))
+		if err != nil {
+			out("read command: %s", err)
+			continue
+		}
 
-		switch {
-		case strings.HasPrefix(line, "cd"):
-			args, _ := shlex.Split(line)
-			if len(args) < 2 {
-				wd = "/"
-				continue
+		if len(args) == 0 {
+			continue
+		}
+
+		switch args[0] {
+		case "cd":
+			if err = sc.cd(args...); err != nil {
+				return err
 			}
 
-			dir := args[1]
-			if dir[0] != '/' {
-				dir = wd + dir
+		case "ls":
+			if err = sc.ls(args...); err != nil {
+				return err
 			}
 
-			if !strings.HasSuffix(dir, "/") {
-				dir = dir + "/"
+		case "get":
+			if err = sc.get(args...); err != nil {
+				return err
 			}
-
-			inf, err := ftp.Stat(dir)
-			if err != nil {
-				out("cd %s: %s", dir, err)
-				continue
-			}
-
-			if !inf.IsDir() {
-				out("cd %s: not a directory", dir)
-				continue
-			}
-
-			dir = path.Clean(dir) + "/"
-
-			out("[%s]", dir)
-			wd = dir
-
-		case strings.HasPrefix(line, "ls"):
-			files, err := ftp.ReadDir(wd)
-			if err != nil {
-				fmt.Printf("ls: %s", err)
-				continue
-			}
-
-			for _, f := range files {
-				tl := ""
-				if f.IsDir() {
-					tl = "/"
-				}
-
-				fmt.Printf("%s%s\n", f.Name(), tl)
-			}
-
-		case strings.HasPrefix(line, "get"):
-			args, _ := shlex.Split(line)
-			if len(args) < 2 {
-				out("get <file>")
-				continue
-			}
-
-			rpath := wd + args[1]
-
-			inf, err := ftp.Stat(rpath)
-			if err != nil {
-				out("get %s: %s", rpath, err)
-				continue
-			}
-
-			if inf.IsDir() {
-				out("get %s: is a directory", rpath)
-				continue
-			}
-
-			localFile := path.Base(rpath)
-			if len(args) > 2 {
-				localFile = args[2]
-			}
-
-			_, err = os.Stat(localFile)
-			if err == nil {
-				out("get %s -> %s: file exists", rpath, localFile)
-				continue
-			}
-
-			func() {
-				rf, err := ftp.Open(rpath)
-				if err != nil {
-					out("get %s -> %s: %s", err)
-					return
-				}
-				defer rf.Close()
-
-				f, err := os.OpenFile(localFile, os.O_WRONLY|os.O_CREATE, 0644)
-				if err != nil {
-					out("get %s -> %s: %s", rpath, localFile, err)
-					return
-				}
-				defer f.Close()
-
-				out("get %s -> %s", rpath, localFile)
-
-				bytes, err := rf.WriteTo(f)
-				if err != nil {
-					out("get %s -> %s: %s (wrote %d bytes)", rpath, localFile, err, bytes)
-				} else {
-					out("wrote %d bytes", bytes)
-				}
-			}()
 		}
 	}
 
