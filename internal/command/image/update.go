f0b258bdcb01a28cf227bd4b447b6731303cb42a
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2022-10-12 19:57:16 +0200

    func machineRole breaks out of loop when we find the role health check

--- internal/command/image/update.go
+++ internal/command/image/update.go
@@ -419,6 +419,7 @@ func machineRole(machine *api.Machine) (role string) {
 			} else {
 				role = "error"
 			}
+			break
 		}
 	}
 	return role
--- internal/command/postgres/postgres.go
+++ internal/command/postgres/postgres.go
@@ -172,6 +172,7 @@ func machineRole(machine *api.Machine) (role string) {
 			} else {
 				role = "error"
 			}
+			break
 		}
 	}
 	return role
