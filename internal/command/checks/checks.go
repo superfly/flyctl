7e71e75920b9d9afee0eb7b321aa83bd2616f930
divya <hello@shortdiv.com>
2022-11-03 14:28:37 +0800

    fix: Remove checks handler

--- internal/command/checks/checks.go
+++ internal/command/checks/checks.go
@@ -20,30 +20,30 @@ func New() *cobra.Command {
 	cmd.AddCommand(listCmd)
 
 	// fly checks handlers
-	handlersCmd := command.New("handlers", "Manage health check handlers", "", nil, command.RequireSession, command.RequireAppName)
-	cmd.AddCommand(handlersCmd)
+	// handlersCmd := command.New("handlers", "Manage health check handlers", "", nil, command.RequireSession, command.RequireAppName)
+	// cmd.AddCommand(handlersCmd)
 
 	// fly checks handlers list
-	hListCmd := command.New("list <organization>", "List health check handlers", "", runListChecksHandlers)
-	hListCmd.Args = cobra.ExactArgs(1)
-	handlersCmd.AddCommand(hListCmd)
+	// hListCmd := command.New("list <organization>", "List health check handlers", "", runListChecksHandlers)
+	// hListCmd.Args = cobra.ExactArgs(1)
+	// handlersCmd.AddCommand(hListCmd)
 
 	// fly checks handlers create
-	hCreateCmd := command.New("create", "Create a health check handler", "", runCreateChecksHandler)
-	flag.Add(hCreateCmd,
-		flag.String{Name: "type", Description: "The type of handler to create, can be slack or pagerduty"},
-		flag.String{Name: "organization", Description: "The organization to add the handler to"},
-		flag.String{Name: "name", Description: "The name of the handler"},
-		flag.String{Name: "webhook-url", Description: "The Slack webhook url"},
-		flag.String{Name: "slack-channel", Description: "The Slack channel"},
-		flag.String{Name: "pagerduty-token", Description: "The PagerDuty token"},
-	)
-	handlersCmd.AddCommand(hCreateCmd)
+	// hCreateCmd := command.New("create", "Create a health check handler", "", runCreateChecksHandler)
+	// flag.Add(hCreateCmd,
+	// 	flag.String{Name: "type", Description: "The type of handler to create, can be slack or pagerduty"},
+	// 	flag.String{Name: "organization", Description: "The organization to add the handler to"},
+	// 	flag.String{Name: "name", Description: "The name of the handler"},
+	// 	flag.String{Name: "webhook-url", Description: "The Slack webhook url"},
+	// 	flag.String{Name: "slack-channel", Description: "The Slack channel"},
+	// 	flag.String{Name: "pagerduty-token", Description: "The PagerDuty token"},
+	// )
+	// handlersCmd.AddCommand(hCreateCmd)
 
 	// fly checks handlers delete
-	hDeleteCmd := command.New("delete <organization> <handler-name>", "Delete a health check handler", "", runDeleteChecksHandler)
-	hDeleteCmd.Args = cobra.ExactArgs(2)
-	handlersCmd.AddCommand(hDeleteCmd)
+	// hDeleteCmd := command.New("delete <organization> <handler-name>", "Delete a health check handler", "", runDeleteChecksHandler)
+	// hDeleteCmd.Args = cobra.ExactArgs(2)
+	// handlersCmd.AddCommand(hDeleteCmd)
 
 	return cmd
 }
