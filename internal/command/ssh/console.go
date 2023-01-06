d4843533a0817a531ec563c8e0f889526622147d
T Van Doren <108194999+tvdfly@users.noreply.github.com>
2023-01-06 00:46:46 -0800

    fly deploy: concurrently acquire/release leases, update flaps.Wait() to better handle timeouts

--- internal/command/ssh/console.go
+++ internal/command/ssh/console.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/docker/docker/pkg/ioutils"
@@ -303,7 +304,7 @@ func addrForMachines(ctx context.Context, app *api.AppCompact, console bool) (ad
 				return "", err
 			}
 
-			err = flapsClient.Wait(ctx, selectedMachine, "started")
+			err = flapsClient.Wait(ctx, selectedMachine, "started", 60*time.Second)
 
 			if err != nil {
 				return "", err
