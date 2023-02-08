1286c43c46d57a5230fe9a4948ca876b004ede9a
William Batista <bootlegbilly@protonmail.ch>
2023-02-08 08:45:56 -0500

    Changed the services command to `services list`
    This helps the command to be similar looking to the others, while also allowing more subcommands in the future

--- internal/command/services/services.go
+++ internal/command/services/services.go
@@ -1,14 +1,9 @@
 package services
 
 import (
-	"context"
-
-	"github.com/superfly/flyctl/client"
-	"github.com/superfly/flyctl/internal/app"
-	"github.com/superfly/flyctl/internal/command"
-	"github.com/superfly/flyctl/internal/flag"
-
 	"github.com/spf13/cobra"
+
+	"github.com/superfly/flyctl/internal/command"
 )
 
 func New() *cobra.Command {
@@ -17,35 +12,11 @@ func New() *cobra.Command {
 		short = `Show the application's services`
 	)
 
-	cmd := command.New("services", short, long, runServiceInfo,
-		command.RequireSession,
-		command.RequireAppName,
+	services := command.New("services", short, long, nil)
+
+	services.AddCommand(
+		newList(),
 	)
 
-	cmd.Args = cobra.NoArgs
-
-	flag.Add(cmd,
-		flag.App(),
-		flag.AppConfig(),
-	)
-
-	return cmd
-}
-
-func runServiceInfo(ctx context.Context) error {
-	var (
-		client  = client.FromContext(ctx).API()
-		appName = app.NameFromContext(ctx)
-	)
-
-	appInfo, err := client.GetAppInfo(ctx, appName)
-	if err != nil {
-		return err
-	}
-
-	if appInfo.PlatformVersion == "machines" {
-		return showMachineServiceInfo(ctx, appInfo)
-	} else {
-		return showNomadServiceInfo(ctx, appInfo)
-	}
+	return services
 }
