acedfab7ce74aa25679064a0d5e115035de9faca
William Batista <bootlegbilly@protonmail.ch>
2023-02-09 11:30:23 -0500

    Added info regarding platform to `fly machines list`

--- internal/command/machine/list.go
+++ internal/command/machine/list.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/superfly/flyctl/api"
 	"github.com/superfly/flyctl/client"
 	"github.com/superfly/flyctl/flaps"
 	"github.com/superfly/flyctl/internal/app"
@@ -102,6 +103,16 @@ func runMachineList(ctx context.Context) (err error) {
 				volName = machine.Config.Mounts[0].Volume
 			}
 
+			appPlatform := ""
+
+			if machine.Config != nil {
+				if platformVersion, ok := machine.Config.Metadata[api.MachineConfigMetadataKeyFlyPlatformVersion]; ok {
+					appPlatform = platformVersion
+
+				}
+
+			}
+
 			rows = append(rows, []string{
 				machine.ID,
 				machine.Name,
@@ -112,10 +123,12 @@ func runMachineList(ctx context.Context) (err error) {
 				volName,
 				machine.CreatedAt,
 				machine.UpdatedAt,
+				appPlatform,
 			})
+
 		}
 
-		_ = render.Table(io.Out, appName, rows, "ID", "Name", "State", "Region", "Image", "IP Address", "Volume", "Created", "Last Updated")
+		_ = render.Table(io.Out, appName, rows, "ID", "Name", "State", "Region", "Image", "IP Address", "Volume", "Created", "Last Updated", "App Platform")
 	}
 	return nil
 }
