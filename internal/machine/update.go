3b5a4e8a1b81e1d1e578392cf65894b50dc10676
William Batista <bootlegbilly@protonmail.ch>
2023-02-10 16:02:15 -0500

    Added some validation logic to machines
    Before being merged, this needs to be discussed more:
    https://flyio.discourse.team/t/applying-limits-to-virtual-machines-on-the-cli/1843

--- internal/machine/update.go
+++ internal/machine/update.go
@@ -18,6 +18,72 @@ func Update(ctx context.Context, m *api.Machine, input *api.LaunchMachineInput) 
 		colorize    = io.ColorScheme()
 	)
 
+	if input != nil && input.Config != nil && input.Config.Guest != nil {
+		// Check that there's a valid number of CPUs
+		var validNumCpus []int
+
+		if input.Config.Guest.CPUKind == "shared" {
+			validNumCpus = append(validNumCpus, 1, 2, 4, 8)
+
+		} else if input.Config.Guest.CPUKind == "performance" {
+			validNumCpus = append(validNumCpus, 1, 2, 4, 8, 16, 64)
+
+		}
+
+		validCpuNum := false
+
+		for _, num := range validNumCpus {
+			if num == input.Config.Guest.CPUs {
+				validCpuNum = true
+				break
+
+			}
+		}
+
+		if !validCpuNum {
+			return fmt.Errorf("invalid config: invalid number of CPUs for %s guest. Valid numbers are %v", input.Config.Guest.CPUKind, validNumCpus)
+
+		}
+
+		// Check that the amount of memory is evenly divisible by 256 MiB
+		if input.Config.Guest.MemoryMB%256 != 0 {
+			return fmt.Errorf("invalid config: invalid memory size; must be in 256 MiB increment")
+
+		}
+
+		// Check that the amount of memory is valid
+		var presetName string
+
+		if input.Config.Guest.CPUKind == "shared" {
+			presetName = fmt.Sprintf("shared-cpu-%dx", input.Config.Guest.CPUs)
+		} else if input.Config.Guest.CPUKind == "performance" {
+			presetName = fmt.Sprintf("performance-%dx", input.Config.Guest.CPUs)
+		}
+
+		// Check memory sizes
+		if machinePreset, ok := api.MachinePresets[presetName]; ok {
+			if machinePreset.MemoryMB > input.Config.Guest.MemoryMB {
+				return fmt.Errorf("invalid config: for machines with %d CPUs, the minimum amount of memory is %d MiB", machinePreset.CPUs, machinePreset.MemoryMB)
+
+			}
+
+			var maxMemory int
+
+			if input.Config.Guest.CPUKind == "shared" {
+				maxMemory = input.Config.Guest.CPUs * api.MIN_MEMORY_MB_PER_SHARED_CPU
+			} else if input.Config.Guest.CPUKind == "performance" {
+				maxMemory = input.Config.Guest.CPUs * api.MIN_MEMORY_MB_PER_CPU
+			}
+
+			if input.Config.Guest.MemoryMB > maxMemory {
+				return fmt.Errorf("invalid config: for machines with %d CPUs, the maximum amount of memory is %d MiB", machinePreset.CPUs, maxMemory)
+
+			}
+
+		}
+
+	}
+
 	fmt.Fprintf(io.Out, "Updating machine %s\n", colorize.Bold(m.ID))
 
 	input.ID = m.ID
