037fc47a47a4eedf110f1aa918ff61e838da109a
T Van Doren <108194999+tvdfly@users.noreply.github.com>
2023-02-14 02:51:28 -0800

    more progress on appsv2 integration test; now calls cli.Run() library directly instead of using a binary

--- test/preflight/apps_v2_integration_test.go
+++ test/preflight/apps_v2_integration_test.go
@@ -3,125 +3,56 @@
 package test
 
 import (
+	"bytes"
 	"context"
 	"crypto/rand"
 	"encoding/base32"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/google/shlex"
 	"github.com/stretchr/testify/assert"
+	"github.com/superfly/flyctl/internal/cli"
+	"github.com/superfly/flyctl/iostreams"
 )
-
-func randomAppName(t *testing.T) string {
-	t.Helper()
-	b := make([]byte, 4)
-	_, err := rand.Read(b)
-	if err != nil {
-		t.Fatalf("failed to read from random: %v", err)
-	}
-	randStr := base32.StdEncoding.EncodeToString(b)
-	randStr = strings.Replace(randStr, "=", "z", -1)
-	dateStr := time.Now().Format("2006-01")
-	return fmt.Sprintf("preflight-%s-%s", dateStr, strings.ToLower(randStr))
-}
 
 func TestAppsV2Example(t *testing.T) {
 	var (
-		err     error
-		argsStr string
-		args    []string
-		cmdStr  string
-		cmd     *exec.Cmd
-		output  []byte
+		err    error
+		result *flyCmdTest
 
-		ctx            = context.TODO()
-		tempDir        = t.TempDir()
-		flyctlBin      = "/Users/tvd/src/superfly/flyctl/bin/flyctl"  // FIXME: flag
-		flyOrg         = "tvd-preflight"                              // FIXME: flag
-		flyAccessToken = os.Getenv("FLY_PREFLIGHT_TEST_ACCESS_TOKEN") // FIXME: flag
-		primaryRegion  = "sea"                                        // FIXME: flag
-		otherRegions   = []string{"ams", "syd"}                       // FIXME: flag
+		orgSlug       = "tvd-preflight"        // FIXME: flag / env
+		primaryRegion = "sea"                  // FIXME: flag / env
+		otherRegions  = []string{"ams", "syd"} // FIXME: flag / env
 		// allRegions     = append([]string{primaryRegion}, otherRegions...)
 		appName = randomAppName(t)
 	)
 
-	argsStr = "orgs list --json"
-	args, err = shlex.Split(argsStr)
-	if err != nil {
-		t.Fatalf("failed to parse argStr: %s error: %v", argsStr, err)
-	}
-	cmdStr = fmt.Sprintf("%s %s", flyctlBin, argsStr)
-	cmd = exec.CommandContext(ctx, flyctlBin, args...)
-	cmd.Dir = tempDir
-	cmd.Env = append(cmd.Env,
-		fmt.Sprintf("FLY_ACCESS_TOKEN=%s", flyAccessToken),
-		fmt.Sprintf("HOME=%s", tempDir),
-		// "LOG_LEVEL=debug",
-	)
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("error [command]: %s [error]: %v [output]: %s", cmdStr, err, string(output))
-	}
+	result = fly(t, "orgs list --json").Run(t).AssertSuccessfulExit(t)
 	var orgMap map[string]string
-	err = json.Unmarshal(output, &orgMap)
+	err = json.Unmarshal(result.stdOut.Bytes(), &orgMap)
 	if err != nil {
-		t.Fatalf("failed to parse json: %v [output]: %s\n", err, string(output))
+		t.Fatalf("failed to parse json: %v [output]: %s\n", err, result.stdOut.String())
 	}
-	if _, present := orgMap[flyOrg]; !present {
-		t.Fatalf("could not find org with name '%s' in `%s` output: %s", flyOrg, cmdStr, string(output))
+	if _, present := orgMap[orgSlug]; !present {
+		t.Fatalf("could not find org with name '%s' in `%s` output: %s", orgSlug, result.cmdStr, result.stdOut.String())
 	}
-
-	// argsStr = fmt.Sprintf("apps create --org %s %s", flyOrg, appName)
-	// args, err = shlex.Split(argsStr)
-	// if err != nil {
-	// 	t.Fatalf("failed to parse argStr: %s error: %v", argsStr, err)
-	// }
-	// cmdStr = fmt.Sprintf("%s %s", flyctlBin, argsStr)
-	// cmd = exec.CommandContext(ctx, flyctlBin, args...)
-	// cmd.Dir = tempDir
-	// cmd.Env = append(cmd.Env, fmt.Sprintf("FLY_ACCESS_TOKEN=%s", flyAccessToken), fmt.Sprintf("HOME=%s", tempDir))
-	// output, err = cmd.CombinedOutput()
-	// if err != nil {
-	// 	t.Fatalf("error [command]: %s [error]: %v [output]: %s", cmdStr, err, string(output))
-	// }
 
 	t.Cleanup(func() {
-		// FIXME: consolidate run command and arg splitting stuff
-		argsStr := fmt.Sprintf("apps destroy --yes %s", appName)
-		args, err := shlex.Split(argsStr)
-		if err != nil {
-			t.Fatalf("failed to parse argStr: %s error: %v", argsStr, err)
-		}
-		cmdStr := fmt.Sprintf("%s %s", flyctlBin, argsStr)
-		cmd := exec.CommandContext(ctx, flyctlBin, args...)
-		cmd.Dir = tempDir
-		cmd.Env = append(cmd.Env, fmt.Sprintf("FLY_ACCESS_TOKEN=%s", flyAccessToken), fmt.Sprintf("HOME=%s", tempDir))
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			t.Fatalf("error [command]: %s [error]: %v [output]: %s", cmdStr, err, string(output))
-		}
+		fly(t, "apps destroy --yes %s", appName).Run(t).AssertSuccessfulExit(t)
 	})
 
-	argsStr = fmt.Sprintf("launch --org %s --name %s --region %s --image nginx --force-machines --internal-port 80 --now --auto-confirm", flyOrg, appName, primaryRegion)
-	args, err = shlex.Split(argsStr)
-	if err != nil {
-		t.Fatalf("failed to parse argStr: %s error: %v", argsStr, err)
-	}
-	cmdStr = fmt.Sprintf("%s %s", flyctlBin, argsStr)
-	cmd = exec.CommandContext(ctx, flyctlBin, args...)
-	cmd.Dir = tempDir
-	cmd.Env = append(cmd.Env, fmt.Sprintf("FLY_ACCESS_TOKEN=%s", flyAccessToken), fmt.Sprintf("HOME=%s", tempDir))
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("error [command]: %s [error]: %v [output]: %s", cmdStr, err, string(output))
-	}
-	assert.Equal(t, "FIXME: expected", string(output), "output does not match expected")
+	result = fly(t, "launch --org %s --name %s --region %s --image nginx --force-machines --internal-port 80 --now --auto-confirm", orgSlug, appName, primaryRegion).Run(t)
+	result.AssertSuccessfulExit(t)
+	assert.Contains(t, result.stdOut.String(), "Using image nginx")
+	assert.Contains(t, result.stdOut.String(), fmt.Sprintf("Created app %s in organization %s", appName, orgSlug))
+	assert.Contains(t, result.stdOut.String(), "Wrote config file fly.toml")
 	// FIXME: set other regions
 	for _, r := range otherRegions {
 		// FIXME: set other regions...
@@ -179,4 +110,93 @@ func TestAppsV2Example(t *testing.T) {
 	// migrate existing app with machines
 
 	// statics
+}
+
+func randomAppName(t *testing.T) string {
+	t.Helper()
+	b := make([]byte, 4)
+	_, err := rand.Read(b)
+	if err != nil {
+		t.Fatalf("failed to read from random: %v", err)
+	}
+	randStr := base32.StdEncoding.EncodeToString(b)
+	randStr = strings.Replace(randStr, "=", "z", -1)
+	dateStr := time.Now().Format("2006-01")
+	return fmt.Sprintf("preflight-%s-%s", dateStr, strings.ToLower(randStr))
+}
+
+type flyCmdTest struct {
+	hasRun                bool
+	argsStr               string
+	args                  []string
+	cmdStr                string
+	tempDir               string
+	testIoStreams         *iostreams.IOStreams
+	stdIn, stdOut, stdErr *bytes.Buffer
+	exitCode              int
+}
+
+func fly(t *testing.T, flyctlCmd string, vals ...interface{}) *flyCmdTest {
+	t.Helper()
+	argsStr := fmt.Sprintf(flyctlCmd, vals...)
+	args, err := shlex.Split(argsStr)
+	if err != nil {
+		t.Fatalf("failed to parse argStr: %s error: %v", argsStr, err)
+	}
+	tempDir := t.TempDir()
+	t.Setenv("FLY_ACCESS_TOKEN", os.Getenv("FLY_PREFLIGHT_TEST_ACCESS_TOKEN"))
+	// t.Setenv("LOG_LEVEL", "debug")
+	t.Setenv("HOME", tempDir)
+	assert.Nil(t, os.Chdir(tempDir))
+	testIoStreams, stdIn, stdOut, stdErr := iostreams.Test()
+	return &flyCmdTest{
+		argsStr:       argsStr,
+		args:          args,
+		cmdStr:        fmt.Sprintf("fly %s", argsStr),
+		tempDir:       tempDir,
+		testIoStreams: testIoStreams,
+		stdIn:         stdIn,
+		stdOut:        stdOut,
+		stdErr:        stdErr,
+	}
+}
+
+func (f *flyCmdTest) AgentLogs(t *testing.T) string {
+	t.Helper()
+	logDir := filepath.Join(f.tempDir, ".fly/agent-logs")
+	agentLogFiles, err := ioutil.ReadDir(logDir)
+	if err != nil {
+		t.Fatalf("failed to find agent log files in %s: %v", logDir, err)
+	}
+	for _, logFile := range agentLogFiles {
+		if !logFile.IsDir() {
+			filePath := filepath.Join(logDir, logFile.Name())
+			if logFile.Size() > 0 {
+				content, err := ioutil.ReadFile(filePath)
+				if err != nil {
+					t.Fatalf("failed to read agent log file at %s: %v", filePath, err)
+				}
+				return string(content)
+			}
+		}
+	}
+	return ""
+}
+
+func (f *flyCmdTest) Run(t *testing.T) *flyCmdTest {
+	t.Helper()
+	f.exitCode = cli.Run(context.TODO(), f.testIoStreams, f.args...)
+	f.hasRun = true
+	return f
+}
+
+func (f *flyCmdTest) AssertSuccessfulExit(t *testing.T) *flyCmdTest {
+	// t.Helper()
+	if !f.hasRun {
+		t.Fatal("cannot call AssertSuccessfulExit() before calling Run() :-(")
+	}
+	if f.exitCode != 0 {
+		t.Fatalf("expected successful zero exit code, got %d, for command: %s [stdout]: %s [strderr]: %s", f.exitCode, f.cmdStr, f.stdOut.String(), f.stdErr.String())
+	}
+	return f
 }
