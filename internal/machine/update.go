2734a6254548c0deddb5414813002ac1b2a38beb
Allison Pierson <allison@fly.io>
2023-02-22 12:37:12 -0600

    `fly machine update`: fix hanging bug
    two bugs combined to make something pretty nasty
    
    `internal/machine/update.go` created a synthetic machine object
    (`&api.Machine{ID: input.ID}`) which didn't have an `instanceId`
    
    This usually wasn't an issue, but when `WaitForStartOrStop` was waiting for
    `stop` (this is an edge case), the API call expected to have an instance ID,
    and so it returned an error.
    
    Just so happens that our error handling in `internal/machine/wait.go` didn't
    recognize this specific error code, and just assumed we needed to wait longer.
    
    oops!

--- internal/machine/update.go
+++ internal/machine/update.go
@@ -13,15 +13,18 @@ import (
 
 func Update(ctx context.Context, m *api.Machine, input *api.LaunchMachineInput) error {
 	var (
-		flapsClient = flaps.FromContext(ctx)
-		io          = iostreams.FromContext(ctx)
-		colorize    = io.ColorScheme()
+		flapsClient    = flaps.FromContext(ctx)
+		io             = iostreams.FromContext(ctx)
+		colorize       = io.ColorScheme()
+		updatedMachine *api.Machine
+		err            error
 	)
 
 	fmt.Fprintf(io.Out, "Updating machine %s\n", colorize.Bold(m.ID))
 
 	input.ID = m.ID
-	if _, err := flapsClient.Update(ctx, *input, m.LeaseNonce); err != nil {
+	updatedMachine, err = flapsClient.Update(ctx, *input, m.LeaseNonce)
+	if err != nil {
 		return fmt.Errorf("could not stop machine %s: %w", input.ID, err)
 	}
 
@@ -30,12 +33,12 @@ func Update(ctx context.Context, m *api.Machine, input *api.LaunchMachineInput) 
 		waitForAction = "stop"
 	}
 
-	if err := WaitForStartOrStop(ctx, &api.Machine{ID: input.ID}, waitForAction, time.Minute*5); err != nil {
+	if err := WaitForStartOrStop(ctx, updatedMachine, waitForAction, time.Minute*5); err != nil {
 		return err
 	}
 
 	if !input.SkipHealthChecks {
-		if err := watch.MachinesChecks(ctx, []*api.Machine{m}); err != nil {
+		if err := watch.MachinesChecks(ctx, []*api.Machine{updatedMachine}); err != nil {
 			return fmt.Errorf("failed to wait for health checks to pass: %w", err)
 		}
 	}
