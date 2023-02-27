8b1139af6b1021ade1da6f590f82013bc6f9d011
Anthony King <2099618+antdking@users.noreply.github.com>
2023-02-27 19:06:24 +0000

    Add explainer + pipenv support

--- scanner/python.go
+++ scanner/python.go
@@ -1,7 +1,8 @@
 package scanner
 
 func configurePython(sourceDir string, config *ScannerConfig) (*SourceInfo, error) {
-	if !checksPass(sourceDir, fileExists("requirements.txt", "environment.yml", "poetry.lock")) {
+	// using 'poetry.lock' as an indicator instead of 'pyproject.toml', as Paketo doesn't support PEP-517 implementations
+	if !checksPass(sourceDir, fileExists("requirements.txt", "environment.yml", "poetry.lock", "Pipfile")) {
 		return nil, nil
 	}
 
