fa89bc1c5cbbde31186274e32398d2434d026296
William Batista <bootlegbilly@protonmail.ch>
2023-03-08 12:42:12 -0500

    Remove all printing from config.Validate

--- internal/appconfig/validation.go
+++ internal/appconfig/validation.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"reflect"
 	"strings"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/logrusorgru/aurora"
 	"github.com/superfly/flyctl/client"
-	"github.com/superfly/flyctl/iostreams"
 )
 
-func (cfg *Config) Validate(ctx context.Context) (err error, error_info string) {
-	io := iostreams.FromContext(ctx)
+func (cfg *Config) Validate(ctx context.Context) (err error, extra_info string) {
 	appName := NameFromContext(ctx)
 	apiClient := client.FromContext(ctx).API()
 
@@ -23,40 +19,38 @@ func (cfg *Config) Validate(ctx context.Context) (err error, error_info string) 
 	}
 
 	platformVersion := NomadPlatform
+	extra_info = fmt.Sprintf("Validating %s (%s)\n", cfg.ConfigFilePath(), platformVersion)
+
 	app, err := apiClient.GetAppBasic(ctx, appName)
 	switch {
 	case err == nil:
 		platformVersion = app.PlatformVersion
 	case strings.Contains(err.Error(), "Could not find App"):
-		fmt.Fprintf(io.Out, "WARNING: Failed to fetch platform version: %s\n", err)
+		extra_info += fmt.Sprintf("WARNING: Failed to fetch platform version: %s\n", err)
 	default:
-		return err, ""
+		return err, extra_info
 	}
-
-	fmt.Fprintf(io.Out, "Validating %s (%s)\n", cfg.ConfigFilePath(), platformVersion)
 
 	switch platformVersion {
 	case MachinesPlatform:
 		err := cfg.EnsureV2Config()
-		var extra_info string
 		if err == nil {
-			extra_info = fmt.Sprintf("%s Configuration is valid\n", aurora.Green("✓"))
+			extra_info += fmt.Sprintf("%s Configuration is valid\n", aurora.Green("✓"))
 			return nil, extra_info
 		} else {
-			extra_info = fmt.Sprintf("\n   %s%s\n", aurora.Red("✘"), err)
+			extra_info += fmt.Sprintf("\n   %s%s\n", aurora.Red("✘"), err)
 			return errors.New("App configuration is not valid"), extra_info
 		}
 	case NomadPlatform:
 		serverCfg, err := apiClient.ValidateConfig(ctx, appName, cfg.SanitizedDefinition())
 		if err != nil {
-			return err, ""
+			return err, extra_info
 		}
 
 		if serverCfg.Valid {
-			extra_info := fmt.Sprintf("%s Configuration is valid\n", aurora.Green("✓"))
+			extra_info += fmt.Sprintf("%s Configuration is valid\n", aurora.Green("✓"))
 			return nil, extra_info
 		} else {
-			extra_info := "\n"
 			for _, errStr := range serverCfg.Errors {
 				extra_info += fmt.Sprintf("   %s%s\n", aurora.Red("✘"), errStr)
 			}
@@ -64,32 +58,7 @@ func (cfg *Config) Validate(ctx context.Context) (err error, error_info string) 
 			return errors.New("App configuration is not valid"), extra_info
 		}
 	default:
-		return fmt.Errorf("Unknown platform version '%s' for app '%s'", platformVersion, appName), ""
+		return fmt.Errorf("Unknown platform version '%s' for app '%s'", platformVersion, appName), extra_info
 	}
 
-}
-
-func (c *Config) validateLocally() (err error) {
-	Validator := validator.New()
-	Validator.RegisterTagNameFunc(func(fld reflect.StructField) string {
-		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
-		// skip if tag key says it should be ignored
-		if name == "-" {
-			return ""
-		}
-		return name
-	})
-
-	err = Validator.Struct(c)
-
-	if err != nil {
-		for _, err := range err.(validator.ValidationErrors) {
-			if err.Tag() == "required" {
-				fmt.Printf("%s is required\n", err.Field())
-			} else {
-				fmt.Printf("Validation error on %s: %s\n", err.Field(), err.Tag())
-			}
-		}
-	}
-	return
 }
