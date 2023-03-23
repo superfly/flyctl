d0d62cb4c745b886cd8773c0f1e3eb7afd4ee5c4
gwuah <griffithawuah15@gmail.com>
2023-03-23 16:52:57 +0100

    patch docker scanner

--- scanner/dockerfile.go
+++ scanner/dockerfile.go
@@ -7,34 +7,45 @@ import (
 	"strconv"
 )
 
-func configureDockerfile(sourceDir string, config *ScannerConfig) (*SourceInfo, error) {
+const defaultPort = 8080
+
+func configureDockerfile (sourceDir string, config *ScannerConfig) (*SourceInfo, error) {
 	if !checksPass(sourceDir, fileExists("Dockerfile")) {
 		return nil, nil
 	}
 
+	var portFromDockerfile int
+
 	s := &SourceInfo{
 		DockerfilePath: filepath.Join(sourceDir, "Dockerfile"),
 		Family:         "Dockerfile",
+		Port : config.ExistingPort,
 	}
 
-	// extract port number from EXPOSE statement
-	dockerfile, err := os.ReadFile("Dockerfile")
-	if err == nil {
-		port := 8080
-		re := regexp.MustCompile(`(?m)^EXPOSE\s+(?P<port>\d+)`)
-		m := re.FindStringSubmatch(string(dockerfile))
+	dockerfile, err := os.ReadFile(s.DockerfilePath)
+	if err != nil {
+		// just maintaining existing behaviour from old code.
+		return s, nil
+	}
 
-		for i, name := range re.SubexpNames() {
-			if len(m) > 0 && name == "port" {
-				port, err = strconv.Atoi(m[i])
-				if err != nil {
-					panic(err)
-				} else if port == 8080 {
-					break
-				}
+	re := regexp.MustCompile(`(?m)^EXPOSE\s+(?P<port>\d+)`)
+	m := re.FindStringSubmatch(string(dockerfile))
+
+	for i, name := range re.SubexpNames() {
+		if len(m) > 0 && name == "port" {
+			portFromDockerfile, err = strconv.Atoi(m[i])
+			if err != nil {
+				continue
 			}
 		}
-		s.Port = port
+	}
+
+	if portFromDockerfile != 0 {
+		s.Port = portFromDockerfile
+	}
+
+	if s.Port == 0 {
+		s.Port = defaultPort
 	}
 
 	return s, nil
