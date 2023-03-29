0db7a8b5a43a503fc6480d1ea0312af9dc37e66c
Daniel Graña <dangra@gmail.com>
2023-03-29 20:08:16 +0000

    Show region counts

--- internal/command/scale/show_machines.go
+++ internal/command/scale/show_machines.go
@@ -3,6 +3,7 @@ package scale
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/samber/lo"
 	"github.com/superfly/flyctl/api"
@@ -11,6 +12,7 @@ import (
 	mach "github.com/superfly/flyctl/internal/machine"
 	"github.com/superfly/flyctl/internal/render"
 	"github.com/superfly/flyctl/iostreams"
+	"golang.org/x/exp/slices"
 )
 
 func runMachinesScaleShow(ctx context.Context) error {
@@ -40,17 +42,36 @@ func runMachinesScaleShow(ctx context.Context) error {
 	rows := make([][]string, 0, len(machineGroups))
 	for groupName, machines := range machineGroups {
 		guest := machines[0].Config.Guest
+		regions := formatRegions(machines)
+
 		rows = append(rows, []string{
 			groupName,
 			fmt.Sprintf("%d", len(machines)),
 			guest.CPUKind,
 			fmt.Sprintf("%d", guest.CPUs),
 			fmt.Sprintf("%d MB", guest.MemoryMB),
+			strings.Join(regions, ","),
 		})
 	}
 
 	fmt.Fprintf(io.Out, "VM Resources for app: %s\n\n", appName)
-	render.Table(io.Out, "Groups", rows, "Name", "Count", "Kind", "CPUs", "Memory")
+	render.Table(io.Out, "Groups", rows, "Name", "Count", "Kind", "CPUs", "Memory", "Regions")
 
 	return nil
+}
+
+func formatRegions(machines []*api.Machine) []string {
+	regions := lo.Map(
+		lo.Entries(lo.CountValues(lo.Map(machines, func(m *api.Machine, _ int) string {
+			return m.Region
+		}))),
+		func(e lo.Entry[string, int], _ int) string {
+			if e.Value > 1 {
+				return fmt.Sprintf("%s(%d)", e.Key, e.Value)
+			}
+			return e.Key
+		},
+	)
+	slices.Sort(regions)
+	return regions
 }
