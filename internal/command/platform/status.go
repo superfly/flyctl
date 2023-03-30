74dcfc4a3b0b6d3964a8e93441f7742e5a5d9c3e
divya <divya@fly.io>
2023-03-30 17:10:11 -0500

    fix/enable-status-json

--- internal/command/platform/status.go
+++ internal/command/platform/status.go
@@ -2,12 +2,21 @@ package platform
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/skratchdot/open-golang/open"
 	"github.com/spf13/cobra"
 
+	"github.com/superfly/flyctl/api"
 	"github.com/superfly/flyctl/internal/command"
+	"github.com/superfly/flyctl/internal/config"
+	"github.com/superfly/flyctl/internal/logger"
+	"github.com/superfly/flyctl/internal/render"
 	"github.com/superfly/flyctl/iostreams"
 )
 
@@ -25,8 +34,75 @@ func newStatus() (cmd *cobra.Command) {
 	return
 }
 
+type Page struct {
+	ID        string    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Url       string    `json:"url,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
+
+type Status struct {
+	Description string `json:"description,omitempty"`
+	Indicator   string `json:"indicator,omitempty"`
+}
+
+type StatusPage struct {
+	Status Status
+	Page   Page
+}
+type Client struct {
+	baseUrl    *url.URL
+	authToken  string
+	httpClient *http.Client
+}
+
+func createClient(rawUrl string, ctx context.Context) (*Client, error) {
+	cleanedURL, _ := url.Parse(rawUrl)
+	logger := logger.MaybeFromContext(ctx)
+	httpClient, err := api.NewHTTPClient(logger, http.DefaultTransport)
+	if err != nil {
+		return nil, fmt.Errorf("can't setup HTTP client to %s: %w", rawUrl, err)
+	}
+
+	return &Client{
+		baseUrl:    cleanedURL,
+		authToken:  "",
+		httpClient: httpClient,
+	}, nil
+}
+
 func runStatus(ctx context.Context) error {
-	const url = "https://status.fly.io/"
+	const url = "https://status.fly.io"
+	var cfg = config.FromContext(ctx)
+
+	// check if json flag
+	if cfg.JSONOutput {
+		client, _ := createClient(url, ctx)
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/%s", client.baseUrl, "api/v2/status.json"), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var res *http.Response
+
+		if res, err = client.httpClient.Do(req); err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != 200 {
+			err = api.ErrorFromResp(res)
+			return fmt.Errorf("failed to retrieve status: %w", err)
+		}
+
+		var result = StatusPage{}
+
+		if err = json.NewDecoder(res.Body).Decode(&result); err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		out := iostreams.FromContext(ctx).Out
+		return render.JSON(out, result)
+	}
 
 	w := iostreams.FromContext(ctx).ErrOut
 	fmt.Fprintf(w, "opening %s ...\n", url)
