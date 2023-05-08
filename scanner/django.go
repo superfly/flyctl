8807daff2239684dbc64b31aa3bbcf889d02ffb2
Kátia Nakamura <katia@fly.io>
2023-05-08 20:32:35 +0200

    Fix formatting with gofmt

--- scanner/django.go
+++ scanner/django.go
@@ -39,7 +39,7 @@ func configureDjango(sourceDir string, config *ScannerConfig) (*SourceInfo, erro
 				UrlPrefix: "/static/",
 			},
 		},
-		SkipDeploy: true,
+		SkipDeploy:     true,
 		ConsoleCommand: "/code/manage.py shell",
 	}
 
