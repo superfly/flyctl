48bdf7858e377bc7d9221c01f740c826c3ed7c2a
William Batista <bootlegbilly@protonmail.ch>
2023-05-10 12:04:28 -0400

    Better checks for if failover will run successfully

--- internal/command/postgres/failover.go
+++ internal/command/postgres/failover.go
@@ -3,7 +3,6 @@ package postgres
 import (
 	"context"
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
@@ -98,10 +97,12 @@ func runFailover(ctx context.Context) (err error) {
 
 	if IsFlex(leader) {
 		if failover_err := flexFailover(ctx, machines, app); failover_err != nil {
-			if err := handleFlexFailoverFail(ctx, leader); err != nil {
+			if err := handleFlexFailoverFail(ctx, machines); err != nil {
 				fmt.Fprintf(io.ErrOut, "Failed to handle failover failure, please manually configure PG cluster primary")
 			}
-			return fmt.Errorf("Failed to run migration command: %s", failover_err)
+			return fmt.Errorf("Failed to run failover: %s", failover_err)
+		} else {
+			return nil
 		}
 	}
 
@@ -148,7 +149,6 @@ func flexFailover(ctx context.Context, machines []*api.Machine, app *api.AppComp
 	if err != nil {
 		return err
 	}
-	flapsClient := flaps.FromContext(ctx)
 
 	fmt.Fprintf(io.Out, "Performing a failover\n")
 
@@ -164,7 +164,7 @@ func flexFailover(ctx context.Context, machines []*api.Machine, app *api.AppComp
 		return fmt.Errorf("Could not find primary region for app")
 	}
 
-	newLeader, err := pickNewLeader(ctx, machines, primary_region)
+	newLeader, err := pickNewLeader(ctx, app, machines, primary_region)
 	if err != nil {
 		return err
 	}
@@ -175,6 +175,8 @@ func flexFailover(ctx context.Context, machines []*api.Machine, app *api.AppComp
 		ID:     leader.ID,
 		Signal: "SIGINT",
 	}
+
+	flapsClient := flaps.FromContext(ctx)
 	err = flapsClient.Stop(ctx, machineStopInput, leader.LeaseNonce)
 	if err != nil {
 		return fmt.Errorf("could not stop pg leader %s: %w", leader.ID, err)
@@ -196,7 +198,7 @@ func flexFailover(ctx context.Context, machines []*api.Machine, app *api.AppComp
 		Cmd:      "repmgr standby promote --siblings-follow -f /data/repmgr.conf",
 		Stdout:   ioutils.NewWriteCloserWrapper(colorable.NewColorableStdout(), func() error { return nil }),
 		Stderr:   ioutils.NewWriteCloserWrapper(colorable.NewColorableStderr(), func() error { return nil }),
-		Stdin:    os.Stdin,
+		Stdin:    nil,
 	}, newLeader.PrivateIP)
 	if err != nil {
 		return err
@@ -242,9 +244,14 @@ func flexFailover(ctx context.Context, machines []*api.Machine, app *api.AppComp
 	return nil
 }
 
-func handleFlexFailoverFail(ctx context.Context, leader *api.Machine) (err error) {
+func handleFlexFailoverFail(ctx context.Context, machines []*api.Machine) (err error) {
 	io := iostreams.FromContext(ctx)
 	flapsClient := flaps.FromContext(ctx)
+
+	leader, err := pickLeader(ctx, machines)
+	if err != nil {
+		return err
+	}
 
 	fmt.Fprintln(io.ErrOut, "Error promoting new leader, restarting existing leader")
 	fmt.Println("Waiting for old leader to finish stopping")
@@ -255,7 +262,7 @@ func handleFlexFailoverFail(ctx context.Context, leader *api.Machine) (err error
 				return err
 			}
 
-			if leader.State == "stopped" {
+			if leader.State == "stopped" || leader.State == "started" {
 				return nil
 			} else if leader.State == "stopping" {
 				return fmt.Errorf("Old leader hasn't finished stopping")
@@ -296,6 +303,8 @@ func handleFlexFailoverFail(ctx context.Context, leader *api.Machine) (err error
 	if mach.Status == "error" {
 		return fmt.Errorf("old leader %s could not be started: %s", leader.ID, mach.Message)
 	}
+
+	fmt.Println("Old leader started succesfully")
 
 	return nil
 }
