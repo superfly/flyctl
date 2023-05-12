782a888b86a6f1a2b1a194ccacd64a672ecf0ebd
Pavel Borzenkov <pavel@fly.io>
2023-05-12 09:55:07 +0200

    Change incorrect address warning text a bit

--- internal/command/deploy/machines_deploymachinesapp.go
+++ internal/command/deploy/machines_deploymachinesapp.go
@@ -467,7 +467,7 @@ func (md *machineDeployment) warnAboutIncorrectListenAddress(ctx context.Context
 	}
 
 	fmt.Fprintf(md.io.ErrOut, "\n%s The app is listening on the incorrect address and will not be reachable by fly-proxy.\n", md.colorize.Yellow("WARNING"))
-	fmt.Fprintf(md.io.ErrOut, "Make sure your app is listening on the following addresses:\n")
+	fmt.Fprintf(md.io.ErrOut, "You can fix this by configuring your app to listen on the following addresses:\n")
 	for port := range tcpServices {
 		fmt.Fprintf(md.io.ErrOut, "  - %s\n", md.colorize.Green("0.0.0.0:"+strconv.Itoa(port)))
 	}
