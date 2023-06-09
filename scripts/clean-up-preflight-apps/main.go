11770251384eceb2f7d52c0c3a50d15f2b04ac86
T Van Doren <108194999+tvdfly@users.noreply.github.com>
2023-06-09 16:37:17 -0700

    preflight clean up script

---
+++ scripts/clean-up-preflight-apps/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/google/shlex"
+	"github.com/superfly/flyctl/api"
+	"github.com/superfly/flyctl/gql"
+	"github.com/superfly/flyctl/internal/buildinfo"
+	"github.com/superfly/flyctl/iostreams"
+	"github.com/superfly/flyctl/logger"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var (
+		ctx       = context.TODO()
+		apiClient = api.NewClientFromOptions(api.ClientOptions{
+			AccessToken: os.Getenv("FLY_PREFLIGHT_TEST_ACCESS_TOKEN"),
+			Name:        buildinfo.Name(),
+			Version:     buildinfo.Version().String(),
+			BaseURL:     "https://api.fly.io",
+			Logger:      logger.FromEnv(iostreams.System().ErrOut),
+		})
+	)
+	if apiClient == nil {
+		return fmt.Errorf("failed to init api client :-(")
+	}
+	_ = `# @genqlient
+	query AllApps($orgSlug:String!) {
+		organization(slug:$orgSlug) {
+			apps {
+				nodes {
+					id
+					createdAt
+				}
+			}
+		}
+	}`
+	resp, err := gql.AllApps(ctx, apiClient.GenqClient, os.Getenv("FLY_PREFLIGHT_TEST_FLY_ORG"))
+	if err != nil {
+		return err
+	}
+	for _, app := range resp.Organization.Apps.Nodes {
+		if time.Since(app.CreatedAt) > 30*time.Minute {
+			flyctlBin := "flyctl"
+			cmdStr := fmt.Sprintf("%s apps destroy --yes %s", flyctlBin, app.Id)
+			cmdParts, err := shlex.Split(cmdStr)
+			if err != nil {
+				return err
+			}
+			cmd := exec.CommandContext(ctx, flyctlBin, cmdParts[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			fmt.Fprintln(os.Stderr, cmdStr)
+			err = cmd.Start()
+			if err != nil {
+				return err
+			}
+			err = cmd.Wait()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
