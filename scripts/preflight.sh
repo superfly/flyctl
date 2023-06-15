082e6a08720a509144ab84ed0c0f344b6e7a0618
Kazuyoshi Kato <kaz@fly.io>
2023-06-15 11:49:46 -0700

    Mention individual tests in Slack
    Our preflight tests are currently not stable. To avoid the broken window
    situation, this commit changes the tests' Slack message to mention
    individual failing tests.

--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 ref=
 total=
 index=
+out=
 
-while getopts r:t:i: name
+while getopts r:t:i:o: name
 do
     case "$name" in
         r)
@@ -17,8 +18,11 @@ do
         i)
 	    index="$OPTARG"
 	    ;;
+        o)
+	    out="$OPTARG"
+	    ;;
         ?)
-	    printf "Usage: %s: [-r REF] [-t TOTAL] [-i INDEX]\n" $0
+	    printf "Usage: %s: [-r REF] [-t TOTAL] [-i INDEX] [-o FILE]\n" $0
             exit 2
 	    ;;
     esac
@@ -31,8 +35,26 @@ if [[ "$ref" != "refs/heads/master" ]]; then
     test_opts=-short
 fi
 
+test_log="$(mktemp)"
+function finish {
+  rm "$test_log"
+}
+trap finish EXIT
+
+set +e
+
 gotesplit \
     -total "$total" \
     -index "$index" \
     github.com/superfly/flyctl/test/preflight/... \
-    -- --tags=integration -v -timeout=60m $test_opts
+    -- --tags=integration -v -timeout=60m $test_opts | tee "$test_log"
+test_status=$?
+
+set -e
+
+if [[ -n "$out" ]]; then
+    awk '/^--- FAIL:/{ printf("%s ", $3) }' "$test_log" >> "$out"
+    echo >> "$out"
+fi
+
+exit $test_status
