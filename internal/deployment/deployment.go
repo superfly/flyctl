4c9417a75f515196000cfb66e83f98093f8231bc
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2023-06-30 17:02:16 +0200

    Use crypto/rand

--- internal/deployment/deployment.go
+++ internal/deployment/deployment.go
@@ -1,19 +1,25 @@
 package deployment
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 // ID generates a unique ID for every flyctl deploy call
 func ID() string {
 	return "deploy-" + randString(8)
 }
 
-// randString generates a random string of length n
 func randString(n int) string {
 	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 	b := make([]rune, n)
 	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))] //nolint:gosec
+		val, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterRunes))))
+		if err != nil {
+			panic(err) // handle error appropriately in your real code
+		}
+		b[i] = letterRunes[val.Int64()]
 	}
 	return string(b)
 }
