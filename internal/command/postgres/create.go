f6a17e11e286ea6a90659923cfaf64aaf21d1f78
Shaun Davis <davissp14@gmail.com>
2023-07-28 10:42:49 -0500

    Default to remote forks for machines

--- internal/command/postgres/create.go
+++ internal/command/postgres/create.go
@@ -67,12 +67,6 @@ func newCreate() *cobra.Command {
 			Name:        "fork-from",
 			Description: "Specify a source Postgres application to fork from. Format: <app-name> or <app-name>:<volume-id>",
 		},
-		flag.Bool{
-			Name:        "remote-fork",
-			Description: "Enables experimental cross-host volume forking",
-			Hidden:      true,
-			Default:     false,
-		},
 		flag.String{
 			Name:        "image-ref",
 			Description: "Specify a non-default base image for the Postgres app",
