053da969d18ceeaf4257cb2d7d623f0090bfbb79
Nathan Willson <willson.b.nathan@gmail.com>
2023-08-21 11:04:19 +0900

    'fly secrets deploy' command for deploying staged secrets
    - only works with machine apps
    - requires that at least one deploy has been run prior

---
+++ internal/command/secrets/deploy.go
@@ -0,0 +1,59 @@
+package secrets
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/superfly/flyctl/client"
+	"github.com/superfly/flyctl/flaps"
+	"github.com/superfly/flyctl/internal/appconfig"
+	"github.com/superfly/flyctl/internal/command"
+	"github.com/superfly/flyctl/internal/flag"
+)
+
+func newDeploy() (cmd *cobra.Command) {
+	const (
+		long  = `Deploy staged secrets for an application`
+		short = long
+		usage = "deploy [flags]"
+	)
+
+	cmd = command.New(usage, short, long, runDeploy, command.RequireSession, command.RequireAppName)
+
+	flag.Add(cmd,
+		sharedFlags,
+	)
+
+	return cmd
+}
+
+func runDeploy(ctx context.Context) (err error) {
+	client := client.FromContext(ctx).API()
+	appName := appconfig.NameFromContext(ctx)
+	app, err := client.GetAppCompact(ctx, appName)
+	if err != nil {
+		return err
+	}
+
+	flapsClient, err := flaps.New(ctx, app)
+	if err != nil {
+		return fmt.Errorf("could not create flaps client: %w", err)
+	}
+
+	machines, _, err := flapsClient.ListFlyAppsMachines(ctx)
+	if err != nil {
+		return err
+	}
+
+	if !app.Deployed && len(machines) == 0 {
+		return errors.New("before using 'fly secrets deploy', you must first deploy your app at least once using 'fly deploy'")
+	}
+
+	if app.PlatformVersion != appconfig.MachinesPlatform {
+		return errors.New("secrets deploy is only supported for machine apps")
+	}
+
+	return DeploySecrets(ctx, app, flag.GetBool(ctx, "stage"), flag.GetBool(ctx, "detach"))
+}
