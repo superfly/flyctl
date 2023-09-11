c2d3e9349d767847978fe5851b79f4f1730763c8
Daniel Graña <dangra@gmail.com>
2023-09-11 11:08:53 -0300

    Switch GPU selection to model instead of counts (#2807)
    * Switch GPU selection to model instead of counts
    
    * update gpu model names
    
    * rename to --vm-gpu-kind

--- internal/flag/machines.go
+++ internal/flag/machines.go
@@ -3,15 +3,24 @@ package flag
 import (
 	"context"
 	"fmt"
+	"slices"
+	"strings"
 
 	"github.com/superfly/flyctl/api"
 )
 
+var validGPUKinds = []string{"a100-pcie-40gb", "a100-sxm4-80gb"}
+
 // Returns a MachineGuest based on the flags provided overwriting a default VM
 func GetMachineGuest(ctx context.Context, guest *api.MachineGuest) (*api.MachineGuest, error) {
+	defaultVMSize := api.DefaultVMSize
+	if IsSpecified(ctx, "vm-gpu-kind") {
+		defaultVMSize = api.DefaultGPUVMSize
+	}
+
 	if guest == nil {
 		guest = &api.MachineGuest{}
-		guest.SetSize(api.DefaultVMSize)
+		guest.SetSize(defaultVMSize)
 	}
 
 	if IsSpecified(ctx, "vm-size") {
@@ -19,29 +28,36 @@ func GetMachineGuest(ctx context.Context, guest *api.MachineGuest) (*api.Machine
 			return nil, err
 		}
 	}
+
 	if IsSpecified(ctx, "vm-cpus") {
 		guest.CPUs = GetInt(ctx, "vm-cpus")
 		if guest.CPUs == 0 {
-			return nil, fmt.Errorf("cannot have zero cpus")
+			return nil, fmt.Errorf("--vm-cpus cannot be zero")
 		}
 	}
 
 	if IsSpecified(ctx, "vm-memory") {
 		guest.MemoryMB = GetInt(ctx, "vm-memory")
 		if guest.MemoryMB == 0 {
-			return nil, fmt.Errorf("memory cannot be zero")
+			return nil, fmt.Errorf("--vm-memory cannot be zero")
 		}
 	}
 
-	if IsSpecified(ctx, "vm-cpukind") {
-		guest.CPUKind = GetString(ctx, "vm-cpukind")
+	if IsSpecified(ctx, "vm-cpu-kind") {
+		guest.CPUKind = GetString(ctx, "vm-cpu-kind")
 		if k := guest.CPUKind; k != "shared" && k != "performance" {
-			return nil, fmt.Errorf("cpukind must be set to 'shared' or 'performance'")
+			return nil, fmt.Errorf("--vm-cpu-kind must be set to 'shared' or 'performance'")
 		}
 	}
-	if IsSpecified(ctx, "vm-gpus") {
-		guest.GPUs = GetInt(ctx, "vm-gpus")
+
+	if IsSpecified(ctx, "vm-gpu-kind") {
+		m := GetString(ctx, "vm-gpu-kind")
+		if !slices.Contains(validGPUKinds, m) {
+			return nil, fmt.Errorf("--vm-gpu-kind must be set to one of: %v", strings.Join(validGPUKinds, ", "))
+		}
+		guest.GPUKind = m
 	}
+
 	return guest, nil
 }
 
@@ -57,16 +73,18 @@ var VMSizeFlags = Set{
 		Aliases:     []string{"cpus"},
 	},
 	String{
-		Name:        "vm-cpukind",
+		Name:        "vm-cpu-kind",
 		Description: "The kind of CPU to use ('shared' or 'performance')",
+		Aliases:     []string{"vm-cpukind"},
 	},
 	Int{
 		Name:        "vm-memory",
 		Description: "Memory (in megabytes) to attribute to the VM",
 		Aliases:     []string{"memory"},
 	},
-	Int{
-		Name:        "vm-gpus",
-		Description: "Number of GPUs",
+	String{
+		Name:        "vm-gpu-kind",
+		Description: fmt.Sprintf("If set, the GPU model to attach (%v)", strings.Join(validGPUKinds, ", ")),
+		Aliases:     []string{"vm-gpukind"},
 	},
 }
