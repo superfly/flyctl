ddfa53b88ff70c1701f63eb2554f341a115448d9
gwuah <griffithawuah15@gmail.com>
2023-09-12 02:21:54 +0200

    validate org limits before performing bluegreen deployment

--- api/resource_deploy.go
+++ api/resource_deploy.go
@@ -121,3 +121,22 @@ func (c *Client) GetReleaseCommand(ctx context.Context, id string) (*ReleaseComm
 
 	return data.ReleaseCommandNode, nil
 }
+
+func (c *Client) CanPerformBluegreenDeployment(ctx context.Context, appName string) (bool, error) {
+	query := `
+		query ($appName: String!) {
+			canPerformBluegreenDeployment(name: $appName)
+		}
+	`
+
+	req := c.NewRequest(query)
+
+	req.Var("appName", appName)
+
+	data, err := c.RunWithContext(ctx, req)
+	if err != nil {
+		return false, err
+	}
+
+	return data.CanPerformBluegreenDeployment, nil
+}
