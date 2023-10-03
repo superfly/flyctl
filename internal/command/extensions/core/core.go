a583f1b6df8c8b65f8a23234de3c91a3d6609525
Nolan Darilek <nolan@fly.io>
2023-10-03 11:05:23 -0500

    Fix compilation errors and move extension-provisioning code from master.

--- internal/command/extensions/core/core.go
+++ internal/command/extensions/core/core.go
@@ -49,7 +49,7 @@ func ProvisionExtension(ctx context.Context, params ExtensionParams) (extension 
 	resp, err := gql.GetAddOnProvider(ctx, client, params.Provider)
 
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	provider := resp.AddOnProvider.ExtensionProviderData
@@ -99,20 +99,11 @@ func ProvisionExtension(ctx context.Context, params ExtensionParams) (extension 
 			err = prompt.String(ctx, &name, "Choose a name, use the default, or leave blank to generate one:", name, false)
 
 			if err != nil {
-				return nil, err
+				return
 			}
 		}
-	} else if targetApp != nil {
+	} else {
 		name = targetApp.Name
-	} else {
-		name = providerName
-	}
-
-	var appId string
-	if targetApp != nil {
-		appId = targetApp.Id
-	} else {
-		appId = ""
 	}
 
 	input := gql.CreateAddOnInput{
@@ -130,7 +121,7 @@ func ProvisionExtension(ctx context.Context, params ExtensionParams) (extension 
 		excludedRegions, err := GetExcludedRegions(ctx, provider)
 
 		if err != nil {
-			return nil, err
+			return extension, err
 		}
 
 		cfg := appconfig.ConfigFromContext(ctx)
@@ -151,7 +142,7 @@ func ProvisionExtension(ctx context.Context, params ExtensionParams) (extension 
 			})
 
 			if err != nil {
-				return nil, err
+				return extension, err
 			}
 
 			primaryRegion = region.Code
@@ -167,13 +158,13 @@ func ProvisionExtension(ctx context.Context, params ExtensionParams) (extension 
 		absDir, err := filepath.Abs(".")
 
 		if err != nil {
-			return nil, err
+			return extension, err
 		}
 
 		detectedPlatform, err = scanner.Scan(absDir, &scanner.ScannerConfig{})
 
 		if err != nil {
-			return nil, err
+			return extension, err
 		}
 
 		if detectedPlatform != nil && PlatformMap[detectedPlatform.Family] != "" {
@@ -188,18 +179,16 @@ func ProvisionExtension(ctx context.Context, params ExtensionParams) (extension 
 	createResp, err := gql.CreateExtension(ctx, client, input)
 
 	if err != nil {
-		return nil, err
+		return
 	}
 
-	extension := &Extension{
-		Data: createResp.CreateAddOn.AddOn.ExtensionData,
-		App:  targetApp,
-	}
+	extension.Data = createResp.CreateAddOn.AddOn.ExtensionData
+	extension.App = &targetApp
 
 	if provider.AsyncProvisioning {
 		err = WaitForProvision(ctx, extension.Data.Name)
 		if err != nil {
-			return nil, err
+			return
 		}
 	}
 
