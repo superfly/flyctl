094fed733858a6fb3f75bfca18b4b4780f70b229
Joshua Sierles <joshua@fly.io>
2023-10-09 23:02:42 +0200

    Fixes for Supabase commands (#2892)

---
+++ internal/command/extensions/supabase/supabase.go
@@ -0,0 +1,19 @@
+package supabase
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/superfly/flyctl/internal/command"
+)
+
+func New() (cmd *cobra.Command) {
+
+	const (
+		short = "Provision and manage Supabase Postgresql databases"
+		long  = short + "\n"
+	)
+
+	cmd = command.New("supabase", short, long, nil)
+	cmd.AddCommand(create(), destroy(), dashboard(), list(), status())
+
+	return cmd
+}
