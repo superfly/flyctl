00b67e8e1989a96c1336a88c6a1f5007b1c04374
Allison Pierson <allison@fly.io>
2023-10-13 15:23:16 -0500

    `deploy`: add `rolling_one` to `--strategy` description

--- internal/flag/flag.go
+++ internal/flag/flag.go
@@ -516,7 +516,7 @@ func Nixpacks() Bool {
 func Strategy() String {
 	return String{
 		Name:        "strategy",
-		Description: "The strategy for replacing running instances. Options are canary, rolling, bluegreen, or immediate. Default is canary, or rolling when max-per-region is set.",
+		Description: "The strategy for replacing running instances. Options are canary, rolling, rolling_one, bluegreen, or immediate. Default is canary, or rolling when max-per-region is set.",
 	}
 }
 
