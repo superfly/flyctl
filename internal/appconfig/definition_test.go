97a193c01dd14413a292eaa06d881857d3d17f01
Chris Nicoll <c@fly.io>
2023-10-17 18:18:52 -0400

    add expected values to tests; test http_service too

--- internal/appconfig/definition_test.go
+++ internal/appconfig/definition_test.go
@@ -190,6 +190,9 @@ func TestToDefinition(t *testing.T) {
 		"http_service": map[string]any{
 			"internal_port": int64(8080),
 			"force_https":   true,
+			"auto_start_machines": false,
+			"auto_stop_machines": false,
+			"min_machines_running": int64(0),
 			"concurrency": map[string]any{
 				"type":       "donuts",
 				"hard_limit": int64(10),
@@ -313,6 +316,9 @@ func TestToDefinition(t *testing.T) {
 				"internal_port": int64(8081),
 				"protocol":      "tcp",
 				"processes":     []any{"app"},
+				"auto_start_machines": false,
+				"auto_stop_machines": false,
+				"min_machines_running": int64(1),
 				"concurrency": map[string]any{
 					"type":       "requests",
 					"hard_limit": int64(22),
