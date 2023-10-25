9208056e000969dd500d61051d9caee98dc57185
Allison Pierson <allison@fly.io>
2023-10-25 15:32:55 -0500

    `launch` v2: make launch v2 the default 🎉

--- internal/command/launch/cmd.go
+++ internal/command/launch/cmd.go
@@ -70,8 +70,8 @@ func New() (cmd *cobra.Command) {
 		},
 		// Launch V2
 		flag.Bool{
-			Name:        "ui",
-			Description: "Use the Launch V2 interface",
+			Name:        "no-ui",
+			Description: "Use the legacy CLI interface (deprecated)",
 			Hidden:      true,
 		},
 		flag.Bool{
@@ -119,7 +119,7 @@ func getManifestArgument(ctx context.Context) (*LaunchManifest, error) {
 
 func run(ctx context.Context) (err error) {
 
-	if !flag.GetBool(ctx, "ui") {
+	if flag.GetBool(ctx, "no-ui") {
 		return legacy.Run(ctx)
 	}
 
