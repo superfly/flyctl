117c4abce31a9f3ab9f07e54187d4e4b88232d89
William Batista <bootlegbilly@protonmail.ch>
2023-11-02 12:49:44 -0400

    Debug shit

--- internal/command/metrics/metrics.go
+++ internal/command/metrics/metrics.go
@@ -1,18 +1,11 @@
 package metrics
 
 import (
-	"bytes"
 	"context"
-	"fmt"
 	"io"
-	"net/http"
-	"time"
 
-	"github.com/PuerkitoBio/rehttp"
 	"github.com/spf13/cobra"
-	"github.com/superfly/flyctl/internal/buildinfo"
 	"github.com/superfly/flyctl/internal/command"
-	"github.com/superfly/flyctl/internal/config"
 	"github.com/superfly/flyctl/internal/metrics"
 	"github.com/superfly/flyctl/iostreams"
 )
@@ -60,31 +53,5 @@ func run(ctx context.Context) error {
 
 	stdin_value := string(stdin_bytes)
 
-	authToken, err := metrics.GetMetricsToken(ctx)
-	if err != nil {
-		return err
-	}
-
-	cfg := config.FromContext(ctx)
-	request, err := http.NewRequest("POST", cfg.MetricsBaseURL+"/metrics_post", bytes.NewBuffer([]byte(stdin_value)))
-	if err != nil {
-		return err
-	}
-
-	request.Header.Set("Authorization", authToken)
-	request.Header.Set("User-Agent", fmt.Sprintf("flyctl/%s", buildinfo.Info().Version))
-
-	retryTransport := rehttp.NewTransport(http.DefaultTransport, rehttp.RetryAll(rehttp.RetryMaxRetries(3), rehttp.RetryTimeoutErr()), rehttp.ConstDelay(0))
-
-	client := http.Client{
-		Transport: retryTransport,
-		Timeout:   time.Second * 5,
-	}
-
-	resp, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-
-	return resp.Body.Close()
+	return metrics.SendMetrics(ctx, stdin_value)
 }
--- internal/metrics/db.go
+++ internal/metrics/db.go
@@ -4,10 +4,17 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
+	"time"
 
+	"github.com/PuerkitoBio/rehttp"
 	"github.com/superfly/flyctl/agent"
+	"github.com/superfly/flyctl/internal/buildinfo"
+	"github.com/superfly/flyctl/internal/config"
 	"github.com/superfly/flyctl/iostreams"
 )
 
@@ -25,37 +32,72 @@ func FlushMetrics(ctx context.Context) error {
 
 	}
 
-	flyctl, err := os.Executable()
-	if err != nil {
-		return err
-	}
-
-	cmd := exec.Command(flyctl, "metrics", "send")
-
-	buffer := bytes.Buffer{}
-	buffer.Write(json)
-
-	cmd.Stdin = &buffer
-	cmd.Env = os.Environ()
-
-	agent.SetSysProcAttributes(cmd)
-
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-
-	io := iostreams.FromContext(ctx)
+	iostream := iostreams.FromContext(ctx)
 
 	// On CI, always block on metrics send. This sucks, but the alternative is not getting metrics from CI at all. There are timeouts in place to prevent this from taking more than 15 seconds
-	if io.IsInteractive() {
+
+	if iostream.IsInteractive() {
+		flyctl, err := os.Executable()
+		if err != nil {
+			return err
+		}
+
+		cmd := exec.Command(flyctl, "metrics", "send")
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			return err
+		}
+
+		go func() {
+			defer stdin.Close()
+			io.WriteString(stdin, string(json))
+		}()
+
+		cmd.Env = os.Environ()
+		cmd.Env = append(cmd.Env, "FLY_NO_UPDATE_CHECK=1")
+
+		agent.SetSysProcAttributes(cmd)
+
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+
 		if err := cmd.Process.Release(); err != nil {
 			return err
 		}
 	} else {
-		if err := cmd.Wait(); err != nil {
-			return err
-		}
+		SendMetrics(ctx, string(json))
 	}
 
 	return nil
+}
+
+func SendMetrics(ctx context.Context, json string) error {
+	authToken, err := GetMetricsToken(ctx)
+	if err != nil {
+		return err
+	}
+
+	cfg := config.FromContext(ctx)
+	request, err := http.NewRequest("POST", cfg.MetricsBaseURL+"/metrics_post", bytes.NewBuffer([]byte(json)))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Authorization", authToken)
+	request.Header.Set("User-Agent", fmt.Sprintf("flyctl/%s", buildinfo.Info().Version))
+
+	retryTransport := rehttp.NewTransport(http.DefaultTransport, rehttp.RetryAll(rehttp.RetryMaxRetries(3), rehttp.RetryTimeoutErr()), rehttp.ConstDelay(0))
+
+	client := http.Client{
+		Transport: retryTransport,
+		Timeout:   time.Second * 5,
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return resp.Body.Close()
 }
