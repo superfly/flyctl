6b1ede6de8d0786ea1552f296efe7e17337645ba
Matthew Ingwersen <matthew@fly.io>
2023-11-09 11:41:46 -0500

    Switch `fly volumes` commands to `GetAppBasic`
    They're currently using `GetApp`, which includes a lot of information,
    including a list of Machines. For apps with a lot of Machines (including
    destroyed ones), this can take a few seconds for the API to prepare.
    
    These commands don't need this information, so use `GetAppBasic`
    instead. This speeds them up, and it may reduce a bit of load on the API
    too!

--- internal/command/volumes/extend.go
+++ internal/command/volumes/extend.go
@@ -67,7 +67,7 @@ func runExtend(ctx context.Context) error {
 	}
 	ctx = flaps.NewContext(ctx, flapsClient)
 
-	app, err := client.GetApp(ctx, appName)
+	app, err := client.GetAppBasic(ctx, appName)
 	if err != nil {
 		return err
 	}
