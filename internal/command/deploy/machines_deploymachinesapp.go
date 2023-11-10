superfly/flyctl

commit e2d15d62a963c6a86daeefcfb435788575ba98f1

fly deploy: skip launch for stopped autoscaled machines (#2992)

internal/command/deploy/machines_deploymachinesapp.go
@@ -314,8 +314,8 @@ func suggestChangeWaitTimeout(err error, flagName string) error {
 
 func (md *machineDeployment) waitForMachine(ctx context.Context, e *machineUpdateEntry) error {
 	lm := e.leasableMachine
-	// Don't wait for Standby machines, they are updated but not started
-	if len(lm.Machine().Config.Standbys) > 0 {
+	// Don't wait for SkipLaunch machines, they are updated but not started
+	if e.launchInput.SkipLaunch {
 		return nil
 	}
 

internal/command/deploy/machines_launchinput.go
@@ -16,9 +16,10 @@ func (md *machineDeployment) launchInputForRestart(origMachineRaw *api.Machine) 
 	md.setMachineReleaseData(Config)
 
 	return &api.LaunchMachineInput{
-		ID:     origMachineRaw.ID,
-		Config: Config,
-		Region: origMachineRaw.Region,
+		ID:         origMachineRaw.ID,
+		Config:     Config,
+		Region:     origMachineRaw.Region,
+		SkipLaunch: skipStopped(origMachineRaw, Config),
 	}
 }
 
@@ -141,7 +142,7 @@ func (md *machineDeployment) launchInputForUpdate(origMachineRaw *api.Machine) (
 		ID:                  mID,
 		Region:              origMachineRaw.Region,
 		Config:              mConfig,
-		SkipLaunch:          len(mConfig.Standbys) > 0,
+		SkipLaunch:          len(mConfig.Standbys) > 0 || skipStopped(origMachineRaw, mConfig),
 		RequiresReplacement: machineShouldBeReplaced,
 	}, nil
 }
@@ -170,4 +171,13 @@ func (md *machineDeployment) setMachineReleaseData(mConfig *api.MachineConfig) {
 	} else {
 		delete(mConfig.Metadata, api.MachineConfigMetadataKeyFlyManagedPostgres)
 	}
+}
+
+// Skip launching currently-stopped machines if any services
+// use autoscaling (autostop or autostart).
+func skipStopped(origMachineRaw *api.Machine, mConfig *api.MachineConfig) bool {
+	return origMachineRaw.State == api.MachineStateStopped &&
+		lo.SomeBy(mConfig.Services, func(s api.MachineService) bool {
+			return (s.Autostop != nil && *s.Autostop) || (s.Autostart != nil && *s.Autostart)
+		})
 }

commit 589f8473c9d65e1b2ed178133234352a03ec6466

Fix: stopped autoscaled machines in bluegreen deployments Don't wait for green machines configured with `SkipLaunch` to start, since they will remain stopped after being created. Fixes a bug introduced in #2992.

internal/command/deploy/machines_deploymachinesapp.go
@@ -281,6 +281,12 @@ type machineUpdateEntry struct {
 	launchInput     *api.LaunchMachineInput
 }
 
+type machineUpdateEntries []*machineUpdateEntry
+
+func (m machineUpdateEntries) machines() []machine.LeasableMachine {
+	return lo.Map(m, func(i *machineUpdateEntry, _ int) machine.LeasableMachine { return i.leasableMachine })
+}
+
 func errorIsTimeout(err error) bool {
 	// Trying to match the errors in a typed way is incredibly difficult and makes this function massive.
 	if strings.Contains(err.Error(), "net/http: request canceled") {
@@ -693,8 +699,8 @@ func (md *machineDeployment) spawnMachineInGroup(ctx context.Context, groupName 
 	statuslogger.Logf(ctx, "Machine %s was created", md.colorize.Bold(lm.FormattedMachineId()))
 	defer lm.ReleaseLease(ctx)
 
-	// Don't wait for Standby machines, they are created but not started
-	if len(launchInput.Config.Standbys) > 0 {
+	// Don't wait for SkipLaunch machines, they are created but not started
+	if launchInput.SkipLaunch {
 		return lm, nil
 	}
 

internal/command/deploy/strategy_bluegreen.go
@@ -34,8 +34,8 @@ var (
 )
 
 type blueGreen struct {
-	greenMachines       []machine.LeasableMachine
-	blueMachines        []*machineUpdateEntry
+	greenMachines       machineUpdateEntries
+	blueMachines        machineUpdateEntries
 	flaps               *flaps.Client
 	apiClient           *api.Client
 	io                  *iostreams.IOStreams
@@ -52,7 +52,7 @@ type blueGreen struct {
 
 func BlueGreenStrategy(md *machineDeployment, blueMachines []*machineUpdateEntry) *blueGreen {
 	bg := &blueGreen{
-		greenMachines:       []machine.LeasableMachine{},
+		greenMachines:       machineUpdateEntries{},
 		blueMachines:        blueMachines,
 		flaps:               md.flapsClient,
 		apiClient:           md.apiClient,
@@ -77,7 +77,7 @@ func BlueGreenStrategy(md *machineDeployment, blueMachines []*machineUpdateEntry
 }
 
 func (bg *blueGreen) CreateGreenMachines(ctx context.Context) error {
-	var greenMachines []machine.LeasableMachine
+	var greenMachines machineUpdateEntries
 
 	for _, mach := range bg.blueMachines {
 		launchInput := mach.launchInput
@@ -91,7 +91,7 @@ func (bg *blueGreen) CreateGreenMachines(ctx context.Context) error {
 		greenMachine := machine.NewLeasableMachine(bg.flaps, bg.io, newMachineRaw)
 		defer greenMachine.ReleaseLease(ctx)
 
-		greenMachines = append(greenMachines, greenMachine)
+		greenMachines = append(greenMachines, &machineUpdateEntry{greenMachine, launchInput})
 
 		fmt.Fprintf(bg.io.ErrOut, "  Created machine %s\n", bg.colorize.Bold(greenMachine.FormattedMachineId()))
 	}
@@ -152,14 +152,14 @@ func (bg *blueGreen) WaitForGreenMachinesToBeStarted(ctx context.Context) error 
 	render := bg.renderMachineStates(machineIDToState)
 	errChan := make(chan error)
 
-	for _, gm := range bg.greenMachines {
+	for _, gm := range bg.greenMachines.machines() {
 		machineIDToState[gm.FormattedMachineId()] = 0
 	}
 
 	for _, gm := range bg.greenMachines {
-		id := gm.FormattedMachineId()
+		id := gm.leasableMachine.FormattedMachineId()
 
-		if len(gm.Machine().Config.Standbys) > 0 {
+		if gm.launchInput.SkipLaunch {
 			machineIDToState[id] = 1
 			continue
 		}
@@ -174,7 +174,7 @@ func (bg *blueGreen) WaitForGreenMachinesToBeStarted(ctx context.Context) error 
 			bg.stateLock.Lock()
 			machineIDToState[id] = 1
 			bg.stateLock.Unlock()
-		}(gm)
+		}(gm.leasableMachine)
 	}
 
 	for {
@@ -268,7 +268,7 @@ func (bg *blueGreen) WaitForGreenMachinesToBeHealthy(ctx context.Context) error 
 	errChan := make(chan error)
 	render := bg.renderMachineHealthchecks(machineIDToHealthStatus)
 
-	for _, gm := range bg.greenMachines {
+	for _, gm := range bg.greenMachines.machines() {
 		// in some cases, not all processes have healthchecks setup
 		// eg. processes that run background workers, etc.
 		// there's no point checking for health, a started state is enough
@@ -279,7 +279,7 @@ func (bg *blueGreen) WaitForGreenMachinesToBeHealthy(ctx context.Context) error 
 		machineIDToHealthStatus[gm.FormattedMachineId()] = &api.HealthCheckStatus{}
 	}
 
-	for _, gm := range bg.greenMachines {
+	for _, gm := range bg.greenMachines.machines() {
 
 		// in some cases, not all processes have healthchecks setup
 		// eg. processes that run background workers, etc.
@@ -347,7 +347,7 @@ func (bg *blueGreen) WaitForGreenMachinesToBeHealthy(ctx context.Context) error 
 }
 
 func (bg *blueGreen) MarkGreenMachinesAsReadyForTraffic(ctx context.Context) error {
-	for _, gm := range bg.greenMachines {
+	for _, gm := range bg.greenMachines.machines() {
 		if bg.aborted.Load() {
 			return ErrAborted
 		}
@@ -503,7 +503,7 @@ func (bg *blueGreen) Rollback(ctx context.Context, err error) error {
 		return nil
 	}
 
-	for _, mach := range bg.greenMachines {
+	for _, mach := range bg.greenMachines.machines() {
 		err := mach.Destroy(ctx, true)
 		if err != nil {
 			return err