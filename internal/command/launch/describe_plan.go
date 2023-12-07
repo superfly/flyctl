16cfd567a9118569dd57d998e3de7ac96c10359b
Kathryn Anne Tan <ktan.software.developer@gmail.com>
2023-12-07 15:41:50 +0800

    Properly assign type in switch statement to follow linting rules

--- internal/command/launch/describe_plan.go
+++ internal/command/launch/describe_plan.go
@@ -17,10 +17,10 @@ import (
 const descriptionNone = "<none>"
 
 func describePostgresPlan(ctx context.Context, p plan.PostgresPlan, org *api.Organization) (string, error) {
-	provider := p.Provider()
-	switch provider.(type) {
+	
+	switch provider := p.Provider().(type) {
 	case *plan.FlyPostgresPlan:
-		return describeFlyPostgresPlan(ctx, provider.(*plan.FlyPostgresPlan), org)
+	  return describeFlyPostgresPlan(ctx, provider, org)
 	}
 	return descriptionNone, nil
 }
@@ -49,10 +49,10 @@ func describeFlyPostgresPlan(ctx context.Context, p *plan.FlyPostgresPlan, org *
 }
 
 func describeRedisPlan(ctx context.Context, p plan.RedisPlan, org *api.Organization) (string, error) {
-	provider := p.Provider()
-	switch provider.(type) {
+	
+	switch provider := p.Provider().(type) {
 	case *plan.UpstashRedisPlan:
-		return describeUpstashRedisPlan(ctx, provider.(*plan.UpstashRedisPlan), org)
+		return describeUpstashRedisPlan(ctx, provider, org)
 	}
 	return descriptionNone, nil
 }
