54354ead4b5b0bee6efe4c1cbd6794367516586c
Allison Pierson <allison@fly.io>
2023-12-11 14:15:35 -0600

    `doctor`: WG HTTP: use raw network connection for HTTP
    The default go HTTP client didn't play nice with the agent-provided sockets for some reason. this works just as well, for this specific use-case.

--- internal/command/doctor/wireguard.go
+++ internal/command/doctor/wireguard.go
@@ -1,6 +1,7 @@
 package doctor
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"net"
@@ -9,8 +10,10 @@ import (
 
 	"github.com/superfly/flyctl/agent"
 	"github.com/superfly/flyctl/client"
+	"github.com/superfly/flyctl/helpers"
 	"github.com/superfly/flyctl/internal/command/dig"
 	"github.com/superfly/flyctl/internal/command/ping"
+	"github.com/superfly/flyctl/internal/flag"
 )
 
 // TODO: These probably shouldn't be hardcoded to use the "personal" org,
@@ -90,12 +93,14 @@ func runPersonalOrgCheckFlaps(ctx context.Context) error {
 
 	apiClient := client.FromContext(ctx).API()
 
+	// Set up the agent connection
 	ac, err := agent.DefaultClient(ctx)
 	if err != nil {
 		// shouldn't happen, already tested agent
 		return fmt.Errorf("wireguard dialer: weird error: %w", err)
 	}
 
+	// Connect to the personal org via WireGuard
 	org, err := apiClient.GetOrganizationBySlug(ctx, "personal")
 	if err != nil {
 		// shouldn't happen, already verified auth token
@@ -107,22 +112,45 @@ func runPersonalOrgCheckFlaps(ctx context.Context) error {
 		return fmt.Errorf("wireguard dialer: %w", err)
 	}
 
-	dialer := &net.Dialer{
-		Timeout: 10 * time.Second,
-		Resolver: &net.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-				return wgDialer.DialContext(ctx, network, address)
-			},
-		},
+	// HACK: Set the quiet flag to true so that the agent doesn't print out progress indicators
+	quietCtx := flag.NewContext(ctx, helpers.Clone(flag.FromContext(ctx)))
+	_ = flag.FromContext(quietCtx).BoolP("quiet", "q", true, "suppress output")
+
+	// Wait for the connection to be established
+	err = ac.WaitForDNS(quietCtx, wgDialer, org.Slug, "_api.internal")
+	if err != nil {
+		return fmt.Errorf("wireguard dialer: failed to wait for DNS to resolve: %w", err)
 	}
 
-	httpClient := http.Client{Transport: &http.Transport{DialContext: dialer.DialContext}}
+	// Resolve the IP address of _api.internal
+	ip, err := ac.Resolve(ctx, org.Slug, "_api.internal:4280")
+	if err != nil {
+		return fmt.Errorf("wireguard dialer: failed to resolve _api.internal: %w", err)
+	}
 
-	// Make an HTTP request to _api.internal:4280.
-	// It should 404. This is a success.
+	// Dial the IP address of _api.internal
+	conn, err := wgDialer.DialContext(ctx, "tcp", ip)
+	if err != nil {
+		return fmt.Errorf("wireguard dialer: failed to dial _api.internal: %w", err)
+	}
 
-	resp, err := httpClient.Get("http://_api.internal:4280")
+	// Make an HTTP request to _api.internal:4280. This should return a 404.
+	req, err := http.NewRequest("GET", "http://_api.internal:4280", nil)
+	if err != nil {
+		return fmt.Errorf("wireguard dialer: failed to create HTTP request: %w", err)
+	}
+
+	// Send the HTTP request
+	if err = req.Write(conn); err != nil {
+		return fmt.Errorf("wireguard dialer: failed to write HTTP request: %w", err)
+	}
+
+	// Read the HTTP response
+	resp, err := http.ReadResponse(bufio.NewReader(conn), req)
+	if err != nil {
+		return fmt.Errorf("wireguard dialer: failed to read HTTP response: %w", err)
+	}
+
 	if err != nil {
 		return fmt.Errorf("wireguard dialer: failed to make HTTP request: %w", err)
 	}
