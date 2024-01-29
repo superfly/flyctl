b99f2103564105eff5c1989662518d9616faf89e
Kazuyoshi Kato <kaz@fly.io>
2024-01-29 13:20:37 -0800

    Use stringer instead of maintaining the switch statement (#3200)
    * Use stringer instead of maintaining the switch statement
    
    golang.org/x/tools/cmd/stringer is Go team's official tool to
    implement String() for enum-like types.
    
    * Keep flapsAction snake_case
    
    * Don't compile the regexp multiple times

--- flaps/actions.go
+++ flaps/actions.go
@@ -1,5 +1,8 @@
 package flaps
 
+//go:generate go run golang.org/x/tools/cmd/stringer -type=flapsAction
+
+// flapsAction is used to record actions in traces' attributes.
 type flapsAction int
 
 const (
@@ -31,64 +34,3 @@ const (
 	volumeExtend
 	volumeDelete
 )
-
-func (a *flapsAction) String() string {
-	switch *a {
-	case appCreate:
-		return "app_create"
-	case machineLaunch:
-		return "machine_launch"
-	case machineUpdate:
-		return "machine_update"
-	case machineStart:
-		return "machine_start"
-	case machineWait:
-		return "machine_wait"
-	case machineStop:
-		return "machine_stop"
-	case machineRestart:
-		return "machine_restart"
-	case machineGet:
-		return "machine_get"
-	case machineList:
-		return "machine_list"
-	case machineDestroy:
-		return "machine_destroy"
-	case machineKill:
-		return "machine_kill"
-	case machineFindLease:
-		return "machine_find_lease"
-	case machineAcquireLease:
-		return "machine_acquire_lease"
-	case machineRefreshLease:
-		return "machine_refresh_lease"
-	case machineReleaseLease:
-		return "machine_release_lease"
-	case machineExec:
-		return "machine_exec"
-	case machinePs:
-		return "machine_ps"
-	case machineCordon:
-		return "machine_cordon"
-	case machineUncordon:
-		return "machine_cordon"
-	case volumeCreate:
-		return "volume_create"
-	case volumeGet:
-		return "volume_get"
-	case volumeList:
-		return "volume_list"
-	case volumetUpdate:
-		return "volume_update"
-	case volumeSnapshotCreate:
-		return "volume_snapshot_create"
-	case volumeSnapshotList:
-		return "volume_snapshot_list"
-	case volumeExtend:
-		return "volume_extend"
-	case volumeDelete:
-		return "volume_delete"
-	default:
-		return "machine_unknown_action"
-	}
-}
