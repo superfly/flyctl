42c7ebcf1d87c90af0c4a650beae2f97f80736bc
Joshua Sierles <joshua@fly.io>
2024-01-30 20:46:17 +0100

    Add new Tigris bucket options

--- internal/command/extensions/tigris/create.go
+++ internal/command/extensions/tigris/create.go
@@ -2,6 +2,7 @@ package tigris
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/superfly/flyctl/gql"
@@ -37,6 +38,41 @@ func create() (cmd *cobra.Command) {
 			Shorthand:   "p",
 			Description: "Objects in the bucket should be publicly accessible",
 		},
+		flag.String{
+			Name:        "shadow-access-key",
+			Description: "Shadow bucket access key",
+		},
+
+		flag.String{
+			Name:        "shadow-secret-key",
+			Description: "Shadow bucket secret key",
+		},
+		flag.String{
+			Name:        "shadow-region",
+			Description: "Shadow bucket region",
+		},
+		flag.String{
+			Name:        "shadow-endpoint",
+			Description: "Shadow bucket endpoint",
+		},
+		flag.String{
+			Name:        "shadow-name",
+			Description: "Shadow bucket name",
+		},
+		flag.Bool{
+			Name:        "shadow-write-through",
+			Description: "Write objects through to the shadow bucket",
+		},
+		flag.Bool{
+			Name:        "accelerate",
+			Hidden:      true,
+			Description: "Cache objects on write in all regions",
+		},
+		flag.String{
+			Name:        "website-domain-name",
+			Description: "Domain name for website",
+			Hidden:      true,
+		},
 	)
 	return cmd
 }
@@ -57,11 +93,57 @@ func runCreate(ctx context.Context) (err error) {
 		params.Organization = org
 	}
 
-	if flag.GetBool(ctx, "public") {
-		params.Options = gql.AddOnOptions{
-			"public": true,
+	options := gql.AddOnOptions{}
+
+	options["public"] = flag.GetBool(ctx, "public")
+	options["accelerate"] = flag.GetBool(ctx, "accelerate")
+
+	accessKey := flag.GetString(ctx, "shadow-access-key")
+	secretKey := flag.GetString(ctx, "shadow-secret-key")
+	region := flag.GetString(ctx, "shadow-region")
+	name := flag.GetString(ctx, "shadow-name")
+
+	endpoint := flag.GetString(ctx, "shadow-endpoint")
+	writeThrough := flag.GetBool(ctx, "shadow-write-through")
+
+	// Check for shadow bucket values
+	shadowBucketValues := []string{accessKey, secretKey, region, name, endpoint}
+	allProvided := true
+	noneProvided := true
+
+	for _, value := range shadowBucketValues {
+		if value == "" {
+			allProvided = false
+		} else {
+			noneProvided = false
 		}
 	}
+
+	// Issue error if not all values are provided together
+	if !allProvided && !noneProvided {
+		return fmt.Errorf("You must set all required shadow bucket values: shadow-access-key, shadow-secret-key, shadow-region, shadow-name, shadow-endpoint")
+	}
+
+	// Include 'shadow_bucket' if all values are provided
+	if allProvided {
+		options["shadow_bucket"] = map[string]interface{}{
+			"access_key":    accessKey,
+			"secret_key":    secretKey,
+			"region":        region,
+			"name":          name,
+			"endpoint":      endpoint,
+			"write_through": writeThrough,
+		}
+	}
+
+	// Always include 'website'
+	options["website"] = map[string]interface{}{
+		"domain_name": flag.GetString(ctx, "website-domain-name"),
+	}
+
+	// Assign the options to params
+	params.Options = options
+
 	params.Provider = "tigris"
 	extension, err := extensions_core.ProvisionExtension(ctx, params)
 
