2250b9f9056a0fff2df6176303a192aedded732c
Daniel Graña <dangra@gmail.com>
2024-02-08 22:50:23 -0300

    Remove Nomad and Detached from config validation

--- internal/appconfig/platformversion.go
+++ internal/appconfig/platformversion.go
@@ -1,9 +1,5 @@
 package appconfig
 
-func (c *Config) EnsureV2Config() error {
-	return c.v2UnmarshalError
-}
-
 // SetMachinesPlatform informs the TOML marshaller that this config is for the machines platform
 func (c *Config) SetMachinesPlatform() error {
 	if c.v2UnmarshalError != nil {
