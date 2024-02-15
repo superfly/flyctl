0a58888e000cf30b21fcb36ce04c9fdec35c7b7c
Joshua Sierles <joshua@fly.io>
2024-02-15 13:42:40 +0100

    sort keys for every potential display of keys

--- internal/command/extensions/core/core.go
+++ internal/command/extensions/core/core.go
@@ -455,15 +455,18 @@ func setSecretsFromExtension(ctx context.Context, app *gql.AppData, extension *E
 		setSecrets = false
 	}
 
+	keys := lo.Keys(secrets)
+	sort.Strings(keys)
+
 	if setSecrets {
 		extension.SetsSecrets = true
 		fmt.Fprintf(io.Out, "Setting the following secrets on %s:\n", app.Name)
 		input := gql.SetSecretsInput{
 			AppId: app.Id,
 		}
-		for key, value := range secrets {
-			input.Secrets = append(input.Secrets, gql.SecretInput{Key: key, Value: value.(string)})
-			fmt.Println(key)
+		for _, key := range keys {
+			input.Secrets = append(input.Secrets, gql.SecretInput{Key: key, Value: secrets[key].(string)})
+			fmt.Fprintln(io.Out, key)
 		}
 
 		fmt.Fprintln(io.Out)
@@ -475,9 +478,6 @@ func setSecretsFromExtension(ctx context.Context, app *gql.AppData, extension *E
 		}
 	} else {
 		fmt.Fprintf(io.Out, "Set the following secrets on your target app.\n")
-
-		keys := lo.Keys(secrets)
-		sort.Strings(keys)
 
 		for _, key := range keys {
 			fmt.Fprintf(io.Out, "%s: %s\n", key, secrets[key].(string))
