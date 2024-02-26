3b2c1c94351d1f93270c3d68e18a92166b11ea68
Senyo Simpson <senyo@fly.io>
2024-02-26 12:41:17 +0200

    persist tokens to file

--- internal/command/extensions/kubernetes/auth.go
+++ internal/command/extensions/kubernetes/auth.go
@@ -7,11 +7,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	fly "github.com/superfly/fly-go"
 	"github.com/superfly/flyctl/gql"
+	"github.com/superfly/flyctl/helpers"
 	"github.com/superfly/flyctl/internal/command"
 	"github.com/superfly/flyctl/internal/command/orgs"
 )
@@ -32,6 +36,11 @@ type status struct {
 	Expiration string `json:"expirationTimestamp,omitempty"`
 }
 
+type auth struct {
+	Token      string `json:"token,omitempty"`
+	Expiration string `json:"expirationTimestamp,omitempty"`
+}
+
 type PartialExecCredential struct {
 	Spec struct {
 		Cluster struct {
@@ -48,7 +57,7 @@ func kubectlToken() (cmd *cobra.Command) {
 	)
 
 	cmd = command.New(usage, short, long, runAuth, command.RequireSession)
-	cmd.Hidden = false
+	cmd.Hidden = true
 
 	return cmd
 }
@@ -79,14 +88,66 @@ func runAuth(ctx context.Context) error {
 		return fmt.Errorf("could not find org id for org %s", orgSlug)
 	}
 
-	tokenResp, err := makeOrgToken(ctx, client, org.ID)
+	configDir, err := helpers.GetConfigDirectory()
 	if err != nil {
+		fmt.Println("Error accessing home directory", err)
 		return err
 	}
 
-	token := tokenResp.CreateLimitedAccessToken.LimitedAccessToken.TokenHeader
-	token = strings.TrimPrefix(token, tokenPrefix)
+	fksConfigDir := filepath.Join(configDir, "fks", orgSlug)
+	configPath := filepath.Join(fksConfigDir, "config.yml")
+
+	v := viper.New()
+	v.SetConfigFile(configPath)
+	v.SetConfigType("yaml")
+
+	var token string
+	var expiry int64
+	now := time.Now().UTC()
+
+	switch err := v.ReadInConfig(); err {
+	case nil:
+		fmt.Fprintf(os.Stderr, "Using existing token")
+		token = v.GetString("auth.token")
+		expiry = int64(v.GetInt("auth.expiration"))
+		if time.Now().Unix() > expiry {
+			fmt.Fprintf(os.Stderr, "Token expired, generating new token")
+			tokenResp, err := makeOrgToken(ctx, client, org.ID, (time.Hour).String())
+			if err != nil {
+				return err
+			}
+
+			token = tokenResp.CreateLimitedAccessToken.LimitedAccessToken.TokenHeader
+			token = strings.TrimPrefix(token, tokenPrefix)
+			expiry = now.Add(time.Hour).Unix()
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "No existing token, generating new one for the first time")
+		// path doesn't exist, create the path
+		if !helpers.DirectoryExists(fksConfigDir) {
+			if err := os.MkdirAll(fksConfigDir, 0o700); err != nil {
+				return err
+			}
+		}
+		tokenResp, err := makeOrgToken(ctx, client, org.ID, (time.Hour).String())
+		if err != nil {
+			return err
+		}
+
+		token = tokenResp.CreateLimitedAccessToken.LimitedAccessToken.TokenHeader
+		token = strings.TrimPrefix(token, tokenPrefix)
+		expiry = now.Add(time.Hour).Unix()
+	}
+
+	v.Set("auth.token", token)
+	v.Set("auth.expiration", expiry)
+	if err := v.WriteConfig(); err != nil {
+		return fmt.Errorf("could not write fks config file (error: %s)", err)
+	}
+
 	resp.Status.Token = token
+	resp.Status.Expiration = time.Unix(expiry, 0).Format(time.RFC3339Nano)
+
 	var buffer bytes.Buffer
 	if err := json.NewEncoder(&buffer).Encode(resp); err != nil {
 		return err
@@ -97,7 +158,7 @@ func runAuth(ctx context.Context) error {
 	return nil
 }
 
-func makeOrgToken(ctx context.Context, apiClient *fly.Client, orgID string) (*gql.CreateLimitedAccessTokenResponse, error) {
+func makeOrgToken(ctx context.Context, apiClient *fly.Client, orgID string, expiration string) (*gql.CreateLimitedAccessTokenResponse, error) {
 	options := gql.LimitedAccessTokenOptions{}
 	resp, err := gql.CreateLimitedAccessToken(
 		ctx,
@@ -106,7 +167,7 @@ func makeOrgToken(ctx context.Context, apiClient *fly.Client, orgID string) (*gq
 		orgID,
 		"deploy_organization",
 		&options,
-		"",
+		expiration,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("failed creating deploy token: %w", err)
