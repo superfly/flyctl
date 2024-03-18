b6be65a92cac080283e6d31c2558e0a8f82de560
Ben Johnson <benbjohnson@yahoo.com>
2024-03-18 15:41:19 -0600

    Deduplicate env warning

--- internal/command/deploy/deploy.go
+++ internal/command/deploy/deploy.go
@@ -242,12 +242,10 @@ func DeployWithConfig(ctx context.Context, appConfig *appconfig.Config, forceYes
 		return err
 	}
 
-	for _, potentialSecretSubstr := range commonSecretSubstrings {
-		for env := range appConfig.Env {
-			if strings.Contains(env, potentialSecretSubstr) && env != "AWS_ACCESS_KEY_ID" {
-				warning := fmt.Sprintf("%s %s may be a potentially sensitive environment variable. Consider setting it as a secret, and removing it from the [env] section: https://fly.io/docs/reference/secrets/\n", aurora.Yellow("WARN"), env)
-				fmt.Fprintln(io.ErrOut, warning)
-			}
+	for env := range appConfig.Env {
+		if containsCommonSecretSubstring(env) {
+			warning := fmt.Sprintf("%s %s may be a potentially sensitive environment variable. Consider setting it as a secret, and removing it from the [env] section: https://fly.io/docs/reference/secrets/\n", aurora.Yellow("WARN"), env)
+			fmt.Fprintln(io.ErrOut, warning)
 		}
 	}
 
