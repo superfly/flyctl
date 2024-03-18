e3d9dcd3b481f07c08ec511a600db40f5820f920
Ben Johnson <benbjohnson@yahoo.com>
2024-03-18 14:13:16 -0600

    Trim generated name before appending new generated name (#3380)

--- internal/haikunator/haikunator.go
+++ internal/haikunator/haikunator.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/superfly/flyctl/helpers"
+	"golang.org/x/exp/slices"
 )
 
 var adjectives = strings.Fields(`
@@ -20,6 +21,7 @@ var adjectives = strings.Fields(`
 	young holy solitary fragrant aged snowy proud floral
 	restless divine polished ancient purple lively nameless
 `)
+
 var nouns = strings.Fields(`
 	waterfall river breeze moon rain wind sea morning
 	snow lake sunset pine shadow leaf dawn glitter forest
@@ -31,59 +33,78 @@ var nouns = strings.Fields(`
 	frost voice paper frog smoke star
 `)
 
-type builder struct {
+type Builder struct {
 	tokRange  int
 	delimiter string
+
+	RandN func(max int) int
 }
 
-type Builder interface {
-	TokenRange(r int) Builder
-	Delimiter(d string) Builder
-	Build() string
-	String() string
-}
-
-func randN(max int) int {
-	ret, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
-	if err != nil {
-		// Fallback to "insecure" random
-		// it doesn't really matter, this is not security critical
-		return rand2.Intn(max) // skipcq: GSC-G404
-	}
-	return int(ret.Int64())
-}
-
-func choose(list []string) string {
-	return list[randN(len(list))]
-}
-
-func Haikunator() Builder {
-	return &builder{
+func Haikunator() *Builder {
+	return &Builder{
 		tokRange:  9999,
 		delimiter: "-",
+
+		RandN: func(max int) int {
+			ret, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+			if err != nil {
+				// Fallback to "insecure" random
+				// it doesn't really matter, this is not security critical
+				return rand2.Intn(max) // skipcq: GSC-G404
+			}
+			return int(ret.Int64())
+		},
 	}
 }
 
-func (b *builder) TokenRange(r int) Builder {
+func (b *Builder) choose(list []string) string {
+	return list[b.RandN(len(list))]
+}
+
+func (b *Builder) TokenRange(r int) *Builder {
 	newB := helpers.Clone(b)
 	newB.tokRange = r
 	return newB
 }
-func (b *builder) Delimiter(d string) Builder {
+
+func (b *Builder) Delimiter(d string) *Builder {
 	newB := helpers.Clone(b)
 	newB.delimiter = d
 	return newB
 }
-func (b *builder) Build() string {
+
+func (b *Builder) Build() string {
 	sections := []string{
-		choose(adjectives),
-		choose(nouns),
+		b.choose(adjectives),
+		b.choose(nouns),
 	}
 	if b.tokRange > 0 {
-		sections = append(sections, strconv.Itoa(randN(b.tokRange)))
+		sections = append(sections, strconv.Itoa(b.RandN(b.tokRange)))
 	}
 	return strings.Join(sections, b.delimiter)
 }
-func (b *builder) String() string {
+
+func (b *Builder) String() string {
 	return b.Build()
+}
+
+// TrimSuffix removes a haiku name at the end of s, if it exists.
+// Otherwise returns the original string.
+func (b *Builder) TrimSuffix(s string) string {
+	a := strings.Split(s, b.delimiter)
+	if len(a) < 3 {
+		return s
+	}
+
+	adjective, noun, num := a[len(a)-3], a[len(a)-2], a[len(a)-1]
+	if !slices.Contains(adjectives, adjective) {
+		return s
+	}
+	if !slices.Contains(nouns, noun) {
+		return s
+	}
+	if _, err := strconv.Atoi(num); err != nil {
+		return s
+	}
+	return strings.Join(a[:len(a)-3], b.delimiter)
 }
