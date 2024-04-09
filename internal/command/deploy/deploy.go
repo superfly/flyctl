superfly/flyctl

commit c3fe6b4d523cf4d4d0b173d77bd5ca597d5b8ec8

Create machines for bluegreen deploy concurrently (#3432)
* Create machines for bluegreen deploy concurrently

Previously, we created the green (new) machines sequentially when running a bluegreen deployment. That works ok when there are a few machines, and it becomes a bit slow when there are 70 or 100 or 500+ machines in the app.

Update the CreateGreenMachines() func to use the pool pattern we have in a few places.

I did the same thing in the MarkGreenMachinesAsReadyForTraffic(), CordonBlueMachines(), StopBlueMachines() funcs.

* add --bluegreen-max-concurrent flag to fly deploy, and fix the loop variable reference issue now that things are concurrent

* --max-concurrent

internal/command/deploy/deploy.go
@@ -132,9 +132,15 @@ var CommonFlags = flag.Set{
 		Description: "Add custom metadata to an image via docker labels",
 	},
 	flag.Int{
+		Name:        "max-concurrent",
+		Description: "Maximum number of machines to operate on concurrently.",
+		Default:     16,
+	},
+	flag.Int{
 		Name:        "immediate-max-concurrent",
 		Description: "Maximum number of machines to update concurrently when using the immediate deployment strategy.",
 		Default:     16,
+		Hidden:      true,
 	},
 	flag.Int{
 		Name:        "volume-initial-size",
@@ -374,29 +380,29 @@ func deployToMachines(
 	}
 
 	md, err := NewMachineDeployment(ctx, MachineDeploymentArgs{
-		AppCompact:             app,
-		DeploymentImage:        img.Tag,
-		Strategy:               flag.GetString(ctx, "strategy"),
-		EnvFromFlags:           flag.GetStringArray(ctx, "env"),
-		PrimaryRegionFlag:      cfg.PrimaryRegion,
-		SkipSmokeChecks:        flag.GetDetach(ctx) || !flag.GetBool(ctx, "smoke-checks"),
-		SkipHealthChecks:       flag.GetDetach(ctx),
-		SkipDNSChecks:          flag.GetDetach(ctx) || !flag.GetBool(ctx, "dns-checks"),
-		WaitTimeout:            waitTimeout,
-		StopSignal:             flag.GetString(ctx, "signal"),
-		ReleaseCmdTimeout:      releaseCmdTimeout,
-		LeaseTimeout:           leaseTimeout,
-		MaxUnavailable:         maxUnavailable,
-		Guest:                  guest,
-		IncreasedAvailability:  flag.GetBool(ctx, "ha"),
-		AllocPublicIP:          !flag.GetBool(ctx, "no-public-ips"),
-		UpdateOnly:             flag.GetBool(ctx, "update-only"),
-		Files:                  files,
-		ExcludeRegions:         excludeRegions,
-		OnlyRegions:            onlyRegions,
-		ImmediateMaxConcurrent: flag.GetInt(ctx, "immediate-max-concurrent"),
-		VolumeInitialSize:      flag.GetInt(ctx, "volume-initial-size"),
-		ProcessGroups:          processGroups,
+		AppCompact:            app,
+		DeploymentImage:       img.Tag,
+		Strategy:              flag.GetString(ctx, "strategy"),
+		EnvFromFlags:          flag.GetStringArray(ctx, "env"),
+		PrimaryRegionFlag:     cfg.PrimaryRegion,
+		SkipSmokeChecks:       flag.GetDetach(ctx) || !flag.GetBool(ctx, "smoke-checks"),
+		SkipHealthChecks:      flag.GetDetach(ctx),
+		SkipDNSChecks:         flag.GetDetach(ctx) || !flag.GetBool(ctx, "dns-checks"),
+		WaitTimeout:           waitTimeout,
+		StopSignal:            flag.GetString(ctx, "signal"),
+		ReleaseCmdTimeout:     releaseCmdTimeout,
+		LeaseTimeout:          leaseTimeout,
+		MaxUnavailable:        maxUnavailable,
+		Guest:                 guest,
+		IncreasedAvailability: flag.GetBool(ctx, "ha"),
+		AllocPublicIP:         !flag.GetBool(ctx, "no-public-ips"),
+		UpdateOnly:            flag.GetBool(ctx, "update-only"),
+		Files:                 files,
+		ExcludeRegions:        excludeRegions,
+		OnlyRegions:           onlyRegions,
+		MaxConcurrent:         flag.GetInt(ctx, "max-concurrent"),
+		VolumeInitialSize:     flag.GetInt(ctx, "volume-initial-size"),
+		ProcessGroups:         processGroups,
 	})
 	if err != nil {
 		sentry.CaptureExceptionWithAppInfo(ctx, err, "deploy", app)

internal/command/deploy/machines.go
@@ -42,69 +42,69 @@ type MachineDeployment interface {
 }
 
 type MachineDeploymentArgs struct {
-	AppCompact             *fly.AppCompact
-	DeploymentImage        string
-	Strategy               string
-	EnvFromFlags           []string
-	PrimaryRegionFlag      string
-	SkipSmokeChecks        bool
-	SkipHealthChecks       bool
-	SkipDNSChecks          bool
-	MaxUnavailable         *float64
-	RestartOnly            bool
-	WaitTimeout            *time.Duration
-	StopSignal             string
-	LeaseTimeout           *time.Duration
-	ReleaseCmdTimeout      *time.Duration
-	Guest                  *fly.MachineGuest
-	IncreasedAvailability  bool
-	AllocPublicIP          bool
-	UpdateOnly             bool
-	Files                  []*fly.File
-	ExcludeRegions         map[string]interface{}
-	OnlyRegions            map[string]interface{}
-	ImmediateMaxConcurrent int
-	VolumeInitialSize      int
-	ProcessGroups          map[string]interface{}
-	RestartPolicy          *fly.MachineRestartPolicy
-	RestartMaxRetries      int
+	AppCompact            *fly.AppCompact
+	DeploymentImage       string
+	Strategy              string
+	EnvFromFlags          []string
+	PrimaryRegionFlag     string
+	SkipSmokeChecks       bool
+	SkipHealthChecks      bool
+	SkipDNSChecks         bool
+	MaxUnavailable        *float64
+	RestartOnly           bool
+	WaitTimeout           *time.Duration
+	StopSignal            string
+	LeaseTimeout          *time.Duration
+	ReleaseCmdTimeout     *time.Duration
+	Guest                 *fly.MachineGuest
+	IncreasedAvailability bool
+	AllocPublicIP         bool
+	UpdateOnly            bool
+	Files                 []*fly.File
+	ExcludeRegions        map[string]interface{}
+	OnlyRegions           map[string]interface{}
+	MaxConcurrent         int
+	VolumeInitialSize     int
+	ProcessGroups         map[string]interface{}
+	RestartPolicy         *fly.MachineRestartPolicy
+	RestartMaxRetries     int
 }
 
 type machineDeployment struct {
-	apiClient              *fly.Client
-	gqlClient              graphql.Client
-	flapsClient            *flaps.Client
-	io                     *iostreams.IOStreams
-	colorize               *iostreams.ColorScheme
-	app                    *fly.AppCompact
-	appConfig              *appconfig.Config
-	img                    string
-	machineSet             machine.MachineSet
-	releaseCommandMachine  machine.MachineSet
-	volumes                map[string][]fly.Volume
-	strategy               string
-	releaseId              string
-	releaseVersion         int
-	skipSmokeChecks        bool
-	skipHealthChecks       bool
-	skipDNSChecks          bool
-	maxUnavailable         float64
-	restartOnly            bool
-	waitTimeout            time.Duration
-	stopSignal             string
-	leaseTimeout           time.Duration
-	leaseDelayBetween      time.Duration
-	releaseCmdTimeout      time.Duration
-	isFirstDeploy          bool
-	machineGuest           *fly.MachineGuest
-	increasedAvailability  bool
-	listenAddressChecked   sync.Map
-	updateOnly             bool
-	excludeRegions         map[string]interface{}
-	onlyRegions            map[string]interface{}
-	immediateMaxConcurrent int
-	volumeInitialSize      int
-	processGroups          map[string]interface{}
+	apiClient             *fly.Client
+	gqlClient             graphql.Client
+	flapsClient           *flaps.Client
+	io                    *iostreams.IOStreams
+	colorize              *iostreams.ColorScheme
+	app                   *fly.AppCompact
+	appConfig             *appconfig.Config
+	img                   string
+	machineSet            machine.MachineSet
+	releaseCommandMachine machine.MachineSet
+	volumes               map[string][]fly.Volume
+	strategy              string
+	releaseId             string
+	releaseVersion        int
+	skipSmokeChecks       bool
+	skipHealthChecks      bool
+	skipDNSChecks         bool
+	maxUnavailable        float64
+	restartOnly           bool
+	waitTimeout           time.Duration
+	stopSignal            string
+	leaseTimeout          time.Duration
+	leaseDelayBetween     time.Duration
+	releaseCmdTimeout     time.Duration
+	isFirstDeploy         bool
+	machineGuest          *fly.MachineGuest
+	increasedAvailability bool
+	listenAddressChecked  sync.Map
+	updateOnly            bool
+	excludeRegions        map[string]interface{}
+	onlyRegions           map[string]interface{}
+	maxConcurrent         int
+	volumeInitialSize     int
+	processGroups         map[string]interface{}
 }
 
 func NewMachineDeployment(ctx context.Context, args MachineDeploymentArgs) (MachineDeployment, error) {
@@ -191,38 +191,38 @@ func NewMachineDeployment(ctx context.Context, args MachineDeploymentArgs) (Mach
 		maxUnavailable = *appConfig.Deploy.MaxUnavailable
 	}
 
-	immedateMaxConcurrent := args.ImmediateMaxConcurrent
-	if immedateMaxConcurrent < 1 {
-		immedateMaxConcurrent = 1
+	maxConcurrent := args.MaxConcurrent
+	if maxConcurrent < 1 {
+		maxConcurrent = 1
 	}
 
 	md := &machineDeployment{
-		apiClient:              apiClient,
-		gqlClient:              apiClient.GenqClient,
-		flapsClient:            flapsClient,
-		io:                     io,
-		colorize:               io.ColorScheme(),
-		app:                    args.AppCompact,
-		appConfig:              appConfig,
-		img:                    args.DeploymentImage,
-		skipSmokeChecks:        args.SkipSmokeChecks,
-		skipHealthChecks:       args.SkipHealthChecks,
-		skipDNSChecks:          args.SkipDNSChecks,
-		restartOnly:            args.RestartOnly,
-		maxUnavailable:         maxUnavailable,
-		waitTimeout:            waitTimeout,
-		stopSignal:             args.StopSignal,
-		leaseTimeout:           leaseTimeout,
-		leaseDelayBetween:      leaseDelayBetween,
-		releaseCmdTimeout:      releaseCmdTimeout,
-		increasedAvailability:  args.IncreasedAvailability,
-		updateOnly:             args.UpdateOnly,
-		machineGuest:           args.Guest,
-		excludeRegions:         args.ExcludeRegions,
-		onlyRegions:            args.OnlyRegions,
-		immediateMaxConcurrent: immedateMaxConcurrent,
-		volumeInitialSize:      args.VolumeInitialSize,
-		processGroups:          args.ProcessGroups,
+		apiClient:             apiClient,
+		gqlClient:             apiClient.GenqClient,
+		flapsClient:           flapsClient,
+		io:                    io,
+		colorize:              io.ColorScheme(),
+		app:                   args.AppCompact,
+		appConfig:             appConfig,
+		img:                   args.DeploymentImage,
+		skipSmokeChecks:       args.SkipSmokeChecks,
+		skipHealthChecks:      args.SkipHealthChecks,
+		skipDNSChecks:         args.SkipDNSChecks,
+		restartOnly:           args.RestartOnly,
+		maxUnavailable:        maxUnavailable,
+		waitTimeout:           waitTimeout,
+		stopSignal:            args.StopSignal,
+		leaseTimeout:          leaseTimeout,
+		leaseDelayBetween:     leaseDelayBetween,
+		releaseCmdTimeout:     releaseCmdTimeout,
+		increasedAvailability: args.IncreasedAvailability,
+		updateOnly:            args.UpdateOnly,
+		machineGuest:          args.Guest,
+		excludeRegions:        args.ExcludeRegions,
+		onlyRegions:           args.OnlyRegions,
+		maxConcurrent:         maxConcurrent,
+		volumeInitialSize:     args.VolumeInitialSize,
+		processGroups:         args.ProcessGroups,
 	}
 	if err := md.setStrategy(); err != nil {
 		tracing.RecordError(span, err, "failed to set strategy")
@@ -653,7 +653,7 @@ func (md *machineDeployment) ToSpanAttributes() []attribute.KeyValue {
 		attribute.Float64("deployment.release_cmd_timeout", md.releaseCmdTimeout.Seconds()),
 		attribute.Bool("deployment.increased_availability", md.increasedAvailability),
 		attribute.Bool("deployment.update_only", md.updateOnly),
-		attribute.Int("deployment.immediate_max_concurrency", md.immediateMaxConcurrent),
+		attribute.Int("deployment.max_concurrency", md.maxConcurrent),
 		attribute.Int("deployment.volume_initial_size", md.volumeInitialSize),
 	}
 

internal/command/deploy/machines_deploymachinesapp.go
@@ -460,8 +460,8 @@ func (md *machineDeployment) updateUsingImmediateStrategy(parentCtx context.Cont
 	defer sl.Destroy(false)
 
 	updatesPool := pool.New().WithErrors().WithContext(parentCtx)
-	if md.immediateMaxConcurrent > 0 {
-		updatesPool = updatesPool.WithMaxGoroutines(md.immediateMaxConcurrent)
+	if md.maxConcurrent > 0 {
+		updatesPool = updatesPool.WithMaxGoroutines(md.maxConcurrent)
 	}
 
 	for i, e := range updateEntries {

internal/command/deploy/strategy_bluegreen.go
@@ -59,6 +59,7 @@ type blueGreen struct {
 	appConfig           *appconfig.Config
 	hangingBlueMachines []string
 	timestamp           string
+	maxConcurrent       int
 }
 
 func BlueGreenStrategy(md *machineDeployment, blueMachines []*machineUpdateEntry) *blueGreen {
@@ -78,6 +79,7 @@ func BlueGreenStrategy(md *machineDeployment, blueMachines []*machineUpdateEntry
 		stateLock:           sync.RWMutex{},
 		hangingBlueMachines: []string{},
 		timestamp:           fmt.Sprintf("%d", time.Now().Unix()),
+		maxConcurrent:       md.maxConcurrent,
 	}
 
 	// Hook into Ctrl+C so that we can rollback the deployment when it's aborted.
@@ -112,24 +114,39 @@ func (bg *blueGreen) CreateGreenMachines(ctx context.Context) error {
 	defer span.End()
 
 	var greenMachines machineUpdateEntries
-
+	var lock sync.Mutex
+	p := pool.New().
+		WithErrors().
+		WithFirstError().
+		WithMaxGoroutines(bg.maxConcurrent)
 	for _, mach := range bg.blueMachines {
-		launchInput := mach.launchInput
-		launchInput.SkipServiceRegistration = true
-		launchInput.Config.Metadata[fly.MachineConfigMetadataKeyFlyctlBGTag] = bg.timestamp
+		mach := mach
+		p.Go(func() error {
+			launchInput := mach.launchInput
+			launchInput.SkipServiceRegistration = true
+			launchInput.Config.Metadata[fly.MachineConfigMetadataKeyFlyctlBGTag] = bg.timestamp
 
-		newMachineRaw, err := bg.flaps.Launch(ctx, *launchInput)
-		if err != nil {
-			tracing.RecordError(span, err, "failed to launch machine")
-			return err
-		}
+			newMachineRaw, err := bg.flaps.Launch(ctx, *launchInput)
+			if err != nil {
+				tracing.RecordError(span, err, "failed to launch machine")
+				return err
+			}
 
-		greenMachine := machine.NewLeasableMachine(bg.flaps, bg.io, newMachineRaw)
-		defer greenMachine.ReleaseLease(ctx)
+			greenMachine := machine.NewLeasableMachine(bg.flaps, bg.io, newMachineRaw)
+			defer greenMachine.ReleaseLease(ctx)
 
-		greenMachines = append(greenMachines, &machineUpdateEntry{greenMachine, launchInput})
+			lock.Lock()
+			defer lock.Unlock()
 
-		fmt.Fprintf(bg.io.ErrOut, "  Created machine %s\n", bg.colorize.Bold(greenMachine.FormattedMachineId()))
+			greenMachines = append(greenMachines, &machineUpdateEntry{greenMachine, launchInput})
+
+			fmt.Fprintf(bg.io.ErrOut, "  Created machine %s\n", bg.colorize.Bold(greenMachine.FormattedMachineId()))
+			return nil
+		})
+	}
+
+	if err := p.Wait(); err != nil {
+		return err
 	}
 
 	bg.greenMachines = greenMachines
@@ -396,58 +413,82 @@ func (bg *blueGreen) MarkGreenMachinesAsReadyForTraffic(ctx context.Context) err
 	ctx, span := tracing.GetTracer().Start(ctx, "mark_green_machines_for_traffic")
 	defer span.End()
 
+	p := pool.New().
+		WithErrors().
+		WithFirstError().
+		WithMaxGoroutines(bg.maxConcurrent)
 	for _, gm := range bg.greenMachines.machines() {
-		if bg.isAborted() {
-			return ErrAborted
-		}
-		err := bg.flaps.Uncordon(ctx, gm.Machine().ID, "")
-		if err != nil {
-			return err
-		}
+		gm := gm
+		p.Go(func() error {
+			if bg.isAborted() {
+				return ErrAborted
+			}
+			err := bg.flaps.Uncordon(ctx, gm.Machine().ID, "")
+			if err != nil {
+				return err
+			}
 
-		fmt.Fprintf(bg.io.ErrOut, "  Machine %s now ready\n", bg.colorize.Bold(gm.FormattedMachineId()))
+			fmt.Fprintf(bg.io.ErrOut, "  Machine %s now ready\n", bg.colorize.Bold(gm.FormattedMachineId()))
+			return nil
+		})
 	}
 
-	return nil
+	return p.Wait()
 }
 
 func (bg *blueGreen) CordonBlueMachines(ctx context.Context) error {
 	ctx, span := tracing.GetTracer().Start(ctx, "cordon_blue_machines")
 	defer span.End()
 
+	p := pool.New().
+		WithErrors().
+		WithFirstError().
+		WithMaxGoroutines(bg.maxConcurrent)
 	for _, gm := range bg.blueMachines {
-		if bg.isAborted() {
-			return ErrAborted
-		}
-		err := gm.leasableMachine.Cordon(ctx)
-		if err != nil {
-			// Just let the user know, it's not a critical error
-			fmt.Fprintf(bg.io.ErrOut, "  Failed to cordon machine %s: %v\n", bg.colorize.Bold(gm.leasableMachine.FormattedMachineId()), err)
-			continue
-		}
+		gm := gm
+		p.Go(func() error {
+			if bg.isAborted() {
+				return ErrAborted
+			}
+			err := gm.leasableMachine.Cordon(ctx)
+			if err != nil {
+				// Just let the user know, it's not a critical error
+				fmt.Fprintf(bg.io.ErrOut, "  Failed to cordon machine %s: %v\n", bg.colorize.Bold(gm.leasableMachine.FormattedMachineId()), err)
+				return nil
+			}
 
-		fmt.Fprintf(bg.io.ErrOut, "  Machine %s cordoned\n", bg.colorize.Bold(gm.leasableMachine.FormattedMachineId()))
+			fmt.Fprintf(bg.io.ErrOut, "  Machine %s cordoned\n", bg.colorize.Bold(gm.leasableMachine.FormattedMachineId()))
+			return nil
+		})
 	}
-	return nil
+	return p.Wait()
 }
 
 func (bg *blueGreen) StopBlueMachines(ctx context.Context) error {
 	ctx, span := tracing.GetTracer().Start(ctx, "stop_blue_machines")
 	defer span.End()
 
+	p := pool.New().
+		WithErrors().
+		WithFirstError().
+		WithMaxGoroutines(bg.maxConcurrent)
 	for _, gm := range bg.blueMachines {
-		if bg.isAborted() {
-			return ErrAborted
-		}
-		err := gm.leasableMachine.Stop(ctx, bg.stopSignal)
-		if err != nil {
-			// Just let the user know, it's not a critical error as we are gonna destroy the
-			// machines with force later
-			fmt.Fprintf(bg.io.ErrOut, "  Failed to stop machine %s: %v\n", bg.colorize.Bold(gm.leasableMachine.FormattedMachineId()), err)
-			continue
-		}
+		gm := gm
+		p.Go(func() error {
+			if bg.isAborted() {
+				return ErrAborted
+			}
+			err := gm.leasableMachine.Stop(ctx, bg.stopSignal)
+			if err != nil {
+				// Just let the user know, it's not a critical error as we are gonna destroy the
+				// machines with force later
+				fmt.Fprintf(bg.io.ErrOut, "  Failed to stop machine %s: %v\n", bg.colorize.Bold(gm.leasableMachine.FormattedMachineId()), err)
+				return nil
+			}
+			return nil
+		})
 	}
-	return nil
+	return p.Wait()
 }
 
 func (bg *blueGreen) WaitForBlueMachinesToBeStopped(ctx context.Context) error {
@@ -511,10 +552,11 @@ func (bg *blueGreen) DestroyBlueMachines(ctx context.Context) error {
 	p := pool.New().
 		WithErrors().
 		WithFirstError().
-		WithMaxGoroutines(16)
+		WithMaxGoroutines(bg.maxConcurrent)
 
 	var mu sync.Mutex
 	for _, gm := range bg.blueMachines {
+		gm := gm
 		p.Go(func() error {
 			if bg.isAborted() {
 				return ErrAborted

commit 695e6498f5ec8e9564bf8e9fda8787fb0bac6197

Fix --immedate-max-concurrent flag
It was accidentally broken in #3432

internal/command/deploy/deploy.go
@@ -27,6 +27,8 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 )
 
+var defaultMaxConcurrent = 16
+
 var CommonFlags = flag.Set{
 	flag.Region(),
 	flag.Image(),
@@ -134,12 +136,12 @@ var CommonFlags = flag.Set{
 	flag.Int{
 		Name:        "max-concurrent",
 		Description: "Maximum number of machines to operate on concurrently.",
-		Default:     16,
+		Default:     defaultMaxConcurrent,
 	},
 	flag.Int{
 		Name:        "immediate-max-concurrent",
 		Description: "Maximum number of machines to update concurrently when using the immediate deployment strategy.",
-		Default:     16,
+		Default:     defaultMaxConcurrent,
 		Hidden:      true,
 	},
 	flag.Int{
@@ -379,6 +381,12 @@ func deployToMachines(
 		}
 	}
 
+	maxConcurrent := flag.GetInt(ctx, "max-concurrent")
+	immediateMaxConcurrent := flag.GetInt(ctx, "immediate-max-concurrent")
+	if maxConcurrent == defaultMaxConcurrent && immediateMaxConcurrent != defaultMaxConcurrent {
+		maxConcurrent = immediateMaxConcurrent
+	}
+
 	md, err := NewMachineDeployment(ctx, MachineDeploymentArgs{
 		AppCompact:            app,
 		DeploymentImage:       img.Tag,
@@ -400,7 +408,7 @@ func deployToMachines(
 		Files:                 files,
 		ExcludeRegions:        excludeRegions,
 		OnlyRegions:           onlyRegions,
-		MaxConcurrent:         flag.GetInt(ctx, "max-concurrent"),
+		MaxConcurrent:         maxConcurrent,
 		VolumeInitialSize:     flag.GetInt(ctx, "volume-initial-size"),
 		ProcessGroups:         processGroups,
 	})