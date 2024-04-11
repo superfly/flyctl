b40f5fee5a5cee3f070efc5fade42ce24220207f
Saleem Rashid <dev@saleemrashid.com>
2024-04-11 05:00:52 +0100

    imgsrc: Cache build tokens created from user tokens

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io/fs"
 	"sync"
+	"time"
 
 	"github.com/spf13/pflag"
 
@@ -45,6 +46,11 @@ const (
 	defaultRegistryHost   = "registry.fly.io"
 	defaultMetricsBaseURL = "https://flyctl-metrics.fly.dev"
 )
+
+type CachedBuildToken struct {
+	Token      string
+	Expiration time.Time
+}
 
 // Config wraps the functionality of the configuration file.
 //
@@ -91,6 +97,10 @@ type Config struct {
 	// Tokens is the user's authentication token(s). They are used differently
 	// depending on where they need to be sent.
 	Tokens *tokens.Tokens
+
+	// CachedBuildTokens denotes the user's cached build token(s). They are created
+	// as needed. Because the cache is not persisted, it does not use the config mutex.
+	CachedBuildTokens map[uint64]CachedBuildToken
 
 	// MetricsToken denotes the user's metrics token.
 	MetricsToken string
