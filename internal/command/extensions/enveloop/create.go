ea874a85305681c4fb22945cde37094fa4317b32
Joshua Sierles <joshua@fly.io>
2024-05-03 21:36:50 +0200

    Clean up enveloop command

--- internal/command/extensions/enveloop/create.go
+++ internal/command/extensions/enveloop/create.go
@@ -2,7 +2,6 @@ package enveloop
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/superfly/flyctl/gql"
@@ -12,7 +11,6 @@ import (
 	"github.com/superfly/flyctl/internal/command/orgs"
 	"github.com/superfly/flyctl/internal/command/secrets"
 	"github.com/superfly/flyctl/internal/flag"
-	"github.com/superfly/flyctl/internal/prompt"
 )
 
 func create() (cmd *cobra.Command) {
@@ -32,44 +30,12 @@ func create() (cmd *cobra.Command) {
 		flag.String{
 			Name:        "name",
 			Shorthand:   "n",
-			Description: "The name of your cluster",
+			Description: "The name of your project",
 		},
 	)
 	return cmd
 }
 
-func selectSimilarityFunction(ctx context.Context, defaultValue string) (function *SimilarityFunction, err error) {
-	var options []string
-	for _, function := range similarityFunctions {
-		options = append(options, fmt.Sprintf("%s (%s)", function.Name, function.UseCases))
-	}
-
-	var index int
-	if err = prompt.Select(ctx, &index, "Select a similarity function:", defaultValue, options...); err == nil {
-		function = &similarityFunctions[index]
-	}
-
-	return
-}
-
-func selectEmbeddingModel(ctx context.Context, defaultValue string) (function *EmbeddingModel, err error) {
-	var options []string
-
-	options = append(options, "None - I will provide my own embeddings")
-
-	for _, model := range embeddingModels {
-		options = append(options, model.Name)
-	}
-
-	var index int
-	if err = prompt.Select(ctx, &index, "Select an embedding model:", defaultValue, options...); err == nil {
-		if index != 0 {
-			function = &embeddingModels[index]
-		}
-	}
-
-	return
-}
 func runCreate(ctx context.Context) (err error) {
 	appName := appconfig.NameFromContext(ctx)
 	params := extensions_core.ExtensionParams{}
@@ -85,34 +51,6 @@ func runCreate(ctx context.Context) (err error) {
 		params.Organization = org
 	}
 
-	function, err := selectSimilarityFunction(ctx, "")
-
-	if err != nil {
-		return err
-	}
-
-	model, err := selectEmbeddingModel(ctx, "")
-
-	if err != nil {
-		return err
-	}
-
-	var defaultDimensionCount int = 128
-
-	var options = gql.AddOnOptions{
-		"similarity_function": function.Identifier,
-		"dimension_count":     &defaultDimensionCount,
-	}
-
-	if model != nil {
-		options["embedding_model"] = model.Identifier
-		options["dimension_count"] = model.Dimensions
-	} else {
-		prompt.Int(ctx, options["dimension_count"].(*int), "How many dimensions?", defaultDimensionCount, false)
-	}
-
-	params.Options = options
-	params.PlanID = "aaV829vaM022XhQG28182aBG" // PAYG is the only plan for now
 	params.Provider = "enveloop"
 	extension, err := extensions_core.ProvisionExtension(ctx, params)
 	if err != nil {
