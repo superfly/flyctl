006f5f043d638e5448d1c5902fb58080f1529908
Allison Pierson <allison@fly.io>
2024-05-09 15:04:47 -0500

    `launch`: remove redundant app name validation from `determineAppName`

--- internal/command/launch/plan_builder.go
+++ internal/command/launch/plan_builder.go
@@ -492,17 +492,6 @@ func determineAppName(ctx context.Context, appConfig *appconfig.Config, configPa
 	if err := validateAppName(appName); err != nil {
 		return "", recoverableSpecifyInUi, recoverableInUiError{err}
 	}
-	// If the app name is already taken, try to generate a unique suffix.
-	taken, err := appNameTaken(ctx, appName)
-	if err != nil {
-		return "", "", flyerr.GenericErr{
-			Err:     "unable to determine app name availability",
-			Suggest: "Please try again in a minute",
-		}
-	}
-	if taken {
-		return appName, recoverableSpecifyInUi, recoverableInUiError{appNameTakenErr(appName)}
-	}
 	return appName, cause, nil
 }
 
