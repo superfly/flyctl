8be65da159d39e69d8b0f085bc0bb111efe695ca
Allison Pierson <allison@fly.io>
2024-06-05 17:16:36 -0500

    Revert "`machine`: `Retry` now supports timeouts"
    This reverts commit fb76c768d29fc96ffa55964ca009bf184bc54b3f.

--- internal/machine/retry.go
+++ internal/machine/retry.go
@@ -26,10 +26,7 @@ func isRetryable(err error) bool {
 
 // Retry retries a machine operation a few times before giving up
 // This is useful for operations like that can fail only to succeed on another try, like machine creation
-// timeout does not cancel operations proactively, it is only passed to the operation to allow it to cancel itself
-// this is useful for operations that have a timeout, like machine creation.
-// If an operation fails, subsequent calls to the operation will have their timeout shortened by the already elapsed time
-func Retry(timeout time.Duration, f func(timeout time.Duration) error) error {
+func Retry(f func() error) error {
 
 	var machineRetryBackoff = backoff.ExponentialBackOff{
 		InitialInterval:     500 * time.Millisecond,
@@ -40,37 +37,11 @@ func Retry(timeout time.Duration, f func(timeout time.Duration) error) error {
 		Clock:               backoff.SystemClock,
 	}
 
-	end := time.Now().Add(timeout)
-	var lastError error
-
 	return backoff.Retry(func() error {
-		var remainingTime time.Duration
-		if timeout != 0 {
-			// We have to handle all this for edge cases anyway, so we'll just handle exiting due to timeout
-			// in here *exclusively* instead of relying on machineRetryBackoff.MaxElapsedTime
-			remainingTime = time.Until(end)
-			if remainingTime <= 0 {
-				if lastError != nil {
-					// We don't have enough allotted time to run the operation again, and we *were*
-					// provided a timeout, so we should just return the last error
-					return backoff.Permanent(lastError)
-				} else {
-					// We get here if
-					//  * there IS a timeout
-					//  * there's no previous error (meaning this is iteration 1)
-					//  * the timeout is already expired
-					// This should never happen, but just in case, we'll just run this iteration with the full timeout
-					// since we *know* it's the first time around anyway
-					remainingTime = timeout
-				}
-			}
-		}
-		err := f(remainingTime)
+		err := f()
 		if err == nil {
 			return nil
 		}
-		lastError = err
-
 		if isRetryable(err) {
 			return err
 		}
@@ -80,11 +51,11 @@ func Retry(timeout time.Duration, f func(timeout time.Duration) error) error {
 
 // RetryRet retries a machine operation a few times before giving up
 // This is useful for operations like that can fail only to succeed on another try, like machine creation
-func RetryRet[T any](timeout time.Duration, f func(time.Duration) (T, error)) (T, error) {
+func RetryRet[T any](f func() (T, error)) (T, error) {
 	var res T
-	err := Retry(timeout, func(localTimeout time.Duration) error {
+	err := Retry(func() error {
 		var err error
-		res, err = f(localTimeout)
+		res, err = f()
 		return err
 	})
 	return res, err
