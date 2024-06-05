2281f085c6829d5236023d783691834016bc7204
Allison Pierson <allison@fly.io>
2024-06-05 15:10:32 -0500

    `machine`: local backoff state instead of global
    backoff modifies the Backoff state object while using it, so having one globally makes me uncomfortable

--- internal/machine/retry.go
+++ internal/machine/retry.go
@@ -6,18 +6,19 @@ import (
 	"github.com/cenkalti/backoff/v4"
 )
 
-var machineRetryBackoff = backoff.ExponentialBackOff{
-	InitialInterval:     500 * time.Millisecond,
-	RandomizationFactor: 0,
-	Multiplier:          2,
-	MaxInterval:         5 * time.Second,
-	MaxElapsedTime:      0,
-	Clock:               backoff.SystemClock,
-}
-
 // Retry retries a machine operation a few times before giving up
 // This is useful for operations like that can fail only to succeed on another try, like machine creation
 func Retry(f func() error) error {
+
+	var machineRetryBackoff = backoff.ExponentialBackOff{
+		InitialInterval:     500 * time.Millisecond,
+		RandomizationFactor: 0,
+		Multiplier:          2,
+		MaxInterval:         5 * time.Second,
+		MaxElapsedTime:      0,
+		Clock:               backoff.SystemClock,
+	}
+
 	return backoff.Retry(func() error {
 		err := f()
 		if err == nil {
