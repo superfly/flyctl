31f92bb4dd9f7f3595d401d57c7bb30ae0a25598
William Batista <bootlegbilly@protonmail.ch>
2024-06-06 16:36:48 -0400

    Added tests for CreateBuilder + bugfixes
    For whatever reason, if we pass app.Name to destroy the app when we
    encounter an issue creating the builder, we pass nil (in unit tests).
    Not sure why, but all good
    
    Secondly, we should sleep between API requests

--- internal/build/imgsrc/ensure_builder_test.go
+++ internal/build/imgsrc/ensure_builder_test.go
@@ -9,6 +9,7 @@ import (
 	fly "github.com/superfly/fly-go"
 	"github.com/superfly/fly-go/flaps"
 	"github.com/superfly/flyctl/internal/flapsutil"
+	"github.com/superfly/flyctl/internal/flyutil"
 	"github.com/superfly/flyctl/internal/mock"
 )
 
@@ -140,4 +141,112 @@ func TestValidateBuilderAPIErrors(t *testing.T) {
 	assert.True(t, errors.As(err, &flapsErr))
 	assert.Equal(t, 404, flapsErr.ResponseStatusCode)
 	assert.Equal(t, 1, machineRetries)
+}
+
+func TestCreateBuilder(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	org := &fly.Organization{
+		Slug: "bigorg",
+	}
+
+	createAppShouldFail := false
+	allocateIPAddressShouldFail := false
+	apiClient := mock.Client{
+		CreateAppFunc: func(ctx context.Context, input fly.CreateAppInput) (*fly.App, error) {
+			if createAppShouldFail {
+				return nil, errors.New("create app failed")
+			}
+			return &fly.App{
+				Name: input.Name,
+			}, nil
+		},
+		DeleteAppFunc: func(ctx context.Context, appName string) error {
+			return nil
+		},
+		AllocateIPAddressFunc: func(ctx context.Context, appName string, addrType string, region string, org *fly.Organization, network string) (*fly.IPAddress, error) {
+			if allocateIPAddressShouldFail {
+				return nil, errors.New("allocate ip address failed")
+			}
+			return &fly.IPAddress{}, nil
+		},
+	}
+
+	waitForAppShouldFail := false
+	launchShouldFail := false
+
+	createVolumeShouldFail := false
+	maxCreateVolumeAttempts := 3
+	createVolumeAttempts := 0
+
+	flapsClient := mock.FlapsClient{
+		WaitForAppFunc: func(ctx context.Context, name string) error {
+			if waitForAppShouldFail {
+				return errors.New("wait for app failed")
+			}
+			return nil
+		},
+		CreateVolumeFunc: func(ctx context.Context, req fly.CreateVolumeRequest) (*fly.Volume, error) {
+			if createVolumeShouldFail {
+				createVolumeAttempts += 1
+
+				if createVolumeAttempts < maxCreateVolumeAttempts {
+					return nil, &flaps.FlapsError{
+						ResponseStatusCode: 500,
+						ResponseBody:       []byte("internal server error"),
+					}
+				}
+			}
+			return &fly.Volume{
+				ID: "bigvolume",
+			}, nil
+		},
+		DeleteVolumeFunc: func(ctx context.Context, volumeId string) (*fly.Volume, error) {
+			return nil, nil
+		},
+		LaunchFunc: func(ctx context.Context, input fly.LaunchMachineInput) (*fly.Machine, error) {
+			if launchShouldFail {
+				return nil, errors.New("launch machine failed")
+			}
+			return &fly.Machine{
+				ID: "bigmachine",
+			}, nil
+		},
+	}
+	ctx = flyutil.NewContextWithClient(ctx, &apiClient)
+	ctx = flapsutil.NewContextWithClient(ctx, &flapsClient)
+
+	app, machine, err := createBuilder(ctx, org, "ord", "builder")
+	assert.NoError(t, err)
+	assert.Equal(t, "bigmachine", machine.ID)
+	assert.Equal(t, app.Name, "builder")
+
+	createAppShouldFail = true
+	_, _, err = createBuilder(ctx, org, "ord", "builder")
+	assert.Error(t, err)
+
+	createAppShouldFail = false
+	allocateIPAddressShouldFail = true
+	_, _, err = createBuilder(ctx, org, "ord", "builder")
+	assert.Error(t, err)
+
+	allocateIPAddressShouldFail = false
+	waitForAppShouldFail = true
+	_, _, err = createBuilder(ctx, org, "ord", "builder")
+	assert.Error(t, err)
+
+	waitForAppShouldFail = false
+	createVolumeShouldFail = true
+	_, _, err = createBuilder(ctx, org, "ord", "builder")
+	assert.NoError(t, err)
+
+	createVolumeAttempts = 0
+	maxCreateVolumeAttempts = 7
+	_, _, err = createBuilder(ctx, org, "ord", "builder")
+	assert.Error(t, err)
+
+	createVolumeShouldFail = false
+	launchShouldFail = true
+	_, _, err = createBuilder(ctx, org, "ord", "builder")
+	assert.Error(t, err)
 }
