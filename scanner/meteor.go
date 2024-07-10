0d1601013bf9306fd96653f5de94466d1407e95a
Kyle McLaren <mclaren@fly.io>
2024-07-10 14:34:26 +0200

    detect Meteor version

--- scanner/meteor.go
+++ scanner/meteor.go
@@ -1,8 +1,40 @@
 package scanner
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func configureMeteor(sourceDir string, config *ScannerConfig) (*SourceInfo, error) {
-	if !checksPass(sourceDir, fileExists(".meteor/release")) && !checksPass(sourceDir, dirContains("package.json", "\"next\"")) {
+	if !checksPass(sourceDir, dirContains("package.json", "\".meteor\"")) {
 		return nil, nil
+	}
+
+	releaseFilePath := sourceDir + "/.meteor/release"
+	releaseContent, err := os.ReadFile(releaseFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	releaseString := strings.TrimSpace(string(releaseContent))
+	parts := strings.Split(releaseString, "@")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid release format in %s", releaseFilePath)
+	}
+	meteorVersion := parts[1]
+
+	versionParts := strings.Split(meteorVersion, ".")
+	if len(versionParts) == 0 {
+		return nil, fmt.Errorf("invalid version format in %s", releaseFilePath)
+	}
+	majorVersion, err := strconv.Atoi(strings.TrimPrefix(versionParts[0], "v"))
+	if err != nil {
+		return nil, fmt.Errorf("invalid major version in %s", releaseFilePath)
+	}
+	if majorVersion < 3 {
+		return nil, fmt.Errorf("major version must be 3 or higher in %s", releaseFilePath)
 	}
 
 	env := map[string]string{
@@ -18,9 +50,7 @@ func configureMeteor(sourceDir string, config *ScannerConfig) (*SourceInfo, erro
 
 	s.Files = templates("templates/meteor")
 
-	s.BuildArgs = map[string]string{
-		"NEXT_PUBLIC_EXAMPLE": "Value goes here",
-	}
+	s.Env["meteorVersion"] = meteorVersion
 
 	return s, nil
 }
