d85dd8da3845e48f8ff13a514f26d2673576a687
Tim Newsham <tim@fly.io>
2024-07-10 12:05:57 -1000

    support vulnerability filtering

--- internal/command/scan/vulns.go
+++ internal/command/scan/vulns.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 
 	"github.com/superfly/fly-go/flaps"
@@ -17,32 +18,42 @@ import (
 	"github.com/superfly/flyctl/iostreams"
 )
 
+var allowedSeverities = []string{"LOW", "MEDIUM", "HIGH", "CRITICAL"}
+
 func newVulns() *cobra.Command {
 	const (
-		usage = "vulns"
+		usage = "vulns <vulnid> ... [flags]"
 		short = "Scan an application's image for vulnerabilities"
 		long  = "Generate a text or JSON report of vulnerabilities found in a application's image.\n" +
 			"If a machine is selected the image from that machine is scanned. Otherwise the image\n" +
-			"of the first running machine is scanned."
+			"of the first running machine is scanned. When a severity is specified, any vulnerabilities\n" +
+			"less than the severity are omitted. When vulnIds are specified, any vulnerability not\n" +
+			"in the vulnID list is omitted."
 	)
 	cmd := command.New(usage, short, long, runVulns,
 		command.RequireSession,
 		command.RequireAppName,
 	)
 
-	cmd.Args = cobra.NoArgs
+	cmd.Args = cobra.ArbitraryArgs
 	flag.Add(
 		cmd,
 		flag.App(),
 		flag.String{
 			Name:        "machine",
+			Shorthand:   "m",
 			Description: "Scan the image of the machine with the specified ID",
 		},
 		flag.Bool{
 			Name:        "select",
 			Shorthand:   "s",
-			Description: "Select which machine to scan the image of from a list.",
+			Description: "Select which machine to scan the image of from a list",
 			Default:     false,
+		},
+		flag.String{
+			Name:        "severity",
+			Shorthand:   "S",
+			Description: fmt.Sprintf("Report only issues with a specific severity %v", allowedSeverities),
 		},
 		// TODO: output file
 		// TODO: format json/text
@@ -74,11 +85,23 @@ type ScanVuln struct {
 	Severity         string
 }
 
+type VulnFilter struct {
+	SeverityLevel int
+	VulnIds       []string
+}
+
 func runVulns(ctx context.Context) error {
 	var (
 		appName   = appconfig.NameFromContext(ctx)
 		apiClient = flyutil.ClientFromContext(ctx)
 	)
+
+	vulnIds := flag.Args(ctx)
+	sev := flag.GetString(ctx, "severity")
+	if flag.IsSpecified(ctx, "severity") && !lo.Contains(allowedSeverities, sev) {
+		return fmt.Errorf("severity (%s) must be one of %v", sev, allowedSeverities)
+	}
+	filter := &VulnFilter{severityLevel(sev), vulnIds}
 
 	app, err := apiClient.GetAppCompact(ctx, appName)
 	if err != nil {
@@ -117,27 +140,39 @@ func runVulns(ctx context.Context) error {
 		return fmt.Errorf("scan result has the wrong schema")
 	}
 
-	scan = filterScan(ctx, scan)
+	scan = filterScan(scan, filter)
 	return presentScan(ctx, scan)
+}
+
+// severityLevel converts an `allowedSeverity` into an integer,
+// where larger integers are more severe. Unknown severities
+// result in `-1`.
+func severityLevel(sev string) int {
+	return lo.IndexOf(allowedSeverities, sev)
 }
 
 // filterVuln returns true if the user's command line preferences
 // indicate interested in the vulnerability.
-func filterVuln(ctx context.Context, vuln *ScanVuln) bool {
-	// TODO: placeholder
-	return vuln.VulnerabilityID == "CVE-2024-24791" ||
-		vuln.VulnerabilityID == "CVE-2023-31484" ||
-		vuln.Severity == "CRITICAL"
+func filterVuln(vuln *ScanVuln, filter *VulnFilter) bool {
+	if filter.SeverityLevel > severityLevel(vuln.Severity) {
+		return false
+	}
+	if len(filter.VulnIds) > 0 {
+		if !lo.Contains(filter.VulnIds, vuln.VulnerabilityID) {
+			return false
+		}
+	}
+	return true
 }
 
 // filterScan filters each vuln in each result based on the command
 // line preferences of the user. Any empty results are discarded.
-func filterScan(ctx context.Context, scan *Scan) *Scan {
+func filterScan(scan *Scan, filter *VulnFilter) *Scan {
 	newRes := []ScanResult{}
 	for _, res := range scan.Results {
 		newVulns := []ScanVuln{}
 		for _, vuln := range res.Vulnerabilities {
-			if filterVuln(ctx, &vuln) {
+			if filterVuln(&vuln, filter) {
 				newVulns = append(newVulns, vuln)
 			}
 		}
