0cff739b8388d8ce4f7feb12aae98bbd7a0afb07
Jeremy Smith <jeremy@jeremysmith.co>
2024-07-11 12:30:18 -0400

    Fix go formatting

--- internal/command/extensions/wafris/wafris.go
+++ internal/command/extensions/wafris/wafris.go
@@ -1,20 +1,20 @@
 package wafris
 
 import (
-  "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 
-  "github.com/superfly/flyctl/internal/command"
+	"github.com/superfly/flyctl/internal/command"
 )
 
 func New() (cmd *cobra.Command) {
 
-  const (
-    short = "Provision and manage Wafris WAFs (Web Application Firewalls)"
-    long  = short + "\n"
-  )
+	const (
+		short = "Provision and manage Wafris WAFs (Web Application Firewalls)"
+		long  = short + "\n"
+	)
 
-  cmd = command.New("wafris", short, long, nil)
-  cmd.AddCommand(create(), dashboard())
+	cmd = command.New("wafris", short, long, nil)
+	cmd.AddCommand(create(), dashboard())
 
-  return cmd
+	return cmd
 }
