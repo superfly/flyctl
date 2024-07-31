3fd9db9af4b76b573cf593174a42e89e46f18bea
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2024-07-31 17:15:25 +0200

    Print some more information

--- internal/command/deploy/deploy.go
+++ internal/command/deploy/deploy.go
@@ -284,7 +284,7 @@ func (cmd *Command) run(ctx context.Context) (err error) {
 
 	switch manifestPath {
 	case "-":
-		manifest, err := ManifestFromReader(io.In)
+		manifest, err := manifestFromReader(io.In)
 		if err != nil {
 			return err
 		}
@@ -292,7 +292,7 @@ func (cmd *Command) run(ctx context.Context) (err error) {
 	case "":
 		break
 	default:
-		manifest, err := ManifestFromFile(manifestPath)
+		manifest, err := manifestFromFile(manifestPath)
 		if err != nil {
 			return err
 		}
