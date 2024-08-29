225f8c63043be9d79f54e689610f732a97fc2cc0
Tim Newsham <tim@fly.io>
2024-08-28 14:30:24 -1000

    update ver.go to support increased range of versions now supported by kmsfs

--- internal/command/secrets/ver.go
+++ internal/command/secrets/ver.go
@@ -1,6 +1,11 @@
 package secrets
 
+// Taken from kmsfs/kms/ver.go.
+// It is not essential that this file is kept in sync with that version,
+// as long as the encoding properties dont change (mostly min/max/increment).
+
 import (
+	"encoding/binary"
 	"fmt"
 	"regexp"
 	"strconv"
@@ -10,69 +15,79 @@ var ErrVersNotFound = func(label string, ver Ver) error {
 	return fmt.Errorf("%s(%s): not found", label, ver)
 }
 
-// Ver is a key version. It is representable in a byte.
-// The value VerUnspec is treated as the lowest possible version
-// number, and encodes as byte(255).
-type Ver int
+// Ver is a key version. It is representable as binary using the binary Varint encoding.
+// The value VerUnspec is encoded as a zero byte and is treated as the lowest possible version number.
+// All other versions are offset by 1, ie. Ver(0) encodes as 1 in Varint format.
+// Ver must be between VerUnspec and VerMax, and encodes as at most four bytes.
+type Ver int32
 
-const VerUnspec Ver = -1
+const (
+	VerUnspec Ver = -1
+	VerZero   Ver = 0
+	VerMax    Ver = 0x1000_0000 - 2 // 268_435_454, 9 digits.
+)
 
-func (v Ver) asByte() uint8 {
-	if v == VerUnspec {
-		return 255
+func (v Ver) Append(buf []byte) ([]byte, error) {
+	if !(VerUnspec <= v && v <= VerMax) {
+		return nil, fmt.Errorf("ver out of range")
 	}
-	return byte(v)
+
+	val := uint64(v + 1)
+	return binary.AppendUvarint(buf, val), nil
 }
 
-func verFromByte(n byte) Ver {
-	if n == 255 {
-		return VerUnspec
+func VerFromBuf(buf []byte) (Ver, []byte, error) {
+	val, n := binary.Uvarint(buf[:4])
+	if n <= 0 {
+		return VerUnspec, nil, fmt.Errorf("error reading version: out of range value")
 	}
-	return Ver(n)
+	resid := buf[n:]
+
+	if !(uint64(VerUnspec+1) <= val && val <= uint64(VerMax+1)) {
+		return VerUnspec, nil, fmt.Errorf("error reading version: out of range value")
+	}
+
+	return Ver(val - 1), resid, nil
 }
 
 func (v Ver) String() string {
 	if v == VerUnspec {
 		return "unspec"
 	}
-	return fmt.Sprintf("%d", byte(v))
+	return fmt.Sprintf("%d", int(v))
 }
 
 func (v Ver) Incr() (Ver, error) {
-	n := int(v)
-	switch n {
-	case 254:
+	if v >= VerMax {
 		return VerUnspec, fmt.Errorf("cannot increment version beyond maximum")
-	case 255:
-		return Ver(0), nil
-	default:
-		return Ver(n + 1), nil
 	}
+	return v + 1, nil
 }
 
 func CompareVer(a, b Ver) int {
 	return int(a) - int(b)
 }
 
-var labelVersionPat = regexp.MustCompile("^(.*)v([0-9]{1,3})$")
+var labelVersionPat = regexp.MustCompile("^(.*)v([0-9]{1,9})$")
 
 // splitLabelVersion splits a label into an integer version and the remaining label.
 // It returns a version of VerUnspec if no label is present or if it would be out of range.
 // Otherwise it returns a version from 0 to 254.
 func splitLabelVersion(label string) (Ver, string) {
-	m := labelVersionPat.FindSubmatch([]byte(label))
+	m := labelVersionPat.FindStringSubmatch(label)
 	if m == nil {
 		return VerUnspec, label
 	}
 
 	l := string(m[1])
 	nstr := string(m[2])
-	n, _ := strconv.ParseInt(nstr, 10, 16)
-	if n > 254 {
+	n, _ := strconv.ParseUint(nstr, 10, 32)
+	ver := Ver(n)
+	if !(VerZero <= ver && ver <= VerMax) {
 		return VerUnspec, label
 	}
 
-	return Ver(n), l
+	return ver, l
 }
 
 func joinLabelVersion(ver Ver, prefix string) string {
