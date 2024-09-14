0f4956c0ab19d5347da0307a603a0387f4cc0ea3
Tim Newsham <tim@fly.io>
2024-09-14 09:50:28 -1000

    update outdated command description

--- internal/command/secrets/key_delete.go
+++ internal/command/secrets/key_delete.go
@@ -15,8 +15,7 @@ import (
 
 func newKeyDelete() (cmd *cobra.Command) {
 	const (
-		long = `Delete the application key secret by label. The label must be fully qualified
-with the key version.`
+		long = `Delete the application key secret by label.`
 		short = `Delete the application key secret`
 		usage = "delete [flags] label"
 	)
