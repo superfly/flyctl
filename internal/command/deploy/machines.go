0d44c9eb1d58f085dac5022f41cd4c476e6111ff
Kazuyoshi Kato <kaz@fly.io>
2024-09-16 17:25:30 -0700

    refactor: bye golang.org/x/exp
    After Go 1.23, the standard library provides what we use from
    the exp package.

--- internal/command/deploy/machines.go
+++ internal/command/deploy/machines.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"maps"
 	"slices"
 	"strings"
 	"sync"
@@ -26,7 +27,6 @@ import (
 	"github.com/superfly/flyctl/terminal"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
-	"golang.org/x/exp/maps"
 )
 
 const (
@@ -404,7 +404,7 @@ func (md *machineDeployment) setMachinesForDeployment(ctx context.Context) error
 			s = "s"
 		}
 
-		filtersAppliedStr := strings.Join(maps.Keys(filtersApplied), "/")
+		filtersAppliedStr := strings.Join(slices.Collect(maps.Keys(filtersApplied)), "/")
 
 		fmt.Fprintf(md.io.ErrOut, "%s filter%s applied, deploying to %d/%d machines\n", filtersAppliedStr, s, len(machines), nMachines)
 	}
