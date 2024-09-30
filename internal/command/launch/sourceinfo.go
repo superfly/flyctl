717395f035ee73618694316c43e37e4554d8f271
Jerome Gravel-Niquet <jeromegn@gmail.com>
2024-09-30 12:40:03 -0400

    fix color output for error message when no framework is detected

--- internal/command/launch/sourceinfo.go
+++ internal/command/launch/sourceinfo.go
@@ -77,8 +77,15 @@ func determineSourceInfo(ctx context.Context, appConfig *appconfig.Config, copyC
 	}
 
 	if srcInfo == nil {
-		fmt.Fprintln(io.Out, aurora.Green("Could not find a Dockerfile, nor detect a runtime or framework from source code. Continuing with a blank app."))
-		if flag.GetBool(ctx, "no-blank") {
+		var colorFn func(arg interface{}) aurora.Value
+		noBlank := flag.GetBool(ctx, "no-blank")
+		if noBlank {
+			colorFn = aurora.Red
+		} else {
+			colorFn = aurora.Green
+		}
+		fmt.Fprintln(io.Out, colorFn("Could not find a Dockerfile, nor detect a runtime or framework from source code. Continuing with a blank app."))
+		if noBlank {
 			entries, err := os.ReadDir("./")
 			if err == nil {
 				// TODO: probably remove this...
