9ec4879794bc97b55afa882dbf69f9dfdb5de4f8
Lubien <lubien1996@gmail.com>
2024-10-07 10:56:54 -0300

    Custom config for deployer monorepos (#3976)
    * Allow custom fly config path
    
    * Add DEPLOYER_SOURCE_CWD
    
    * Do not use --config unless available
    
    * Move config file check to git pull step
    
    * Fix fly.toml path
    
    * Test custom CWD
    
    * Fix envs
    
    * Always check DEPLOYER_SOURCE_CWD regardless of git pull
    
    * Do not dupe steps

--- test/deployer/deployer_test.go
+++ test/deployer/deployer_test.go
@@ -36,6 +36,56 @@ func TestDeployBasicNode(t *testing.T) {
 	require.Contains(t, string(body), fmt.Sprintf("Hello, World! %s", deploy.Extra["TEST_ID"].(string)))
 }
 
+func TestDeployBasicNodeWithCustomConfigPath(t *testing.T) {
+	deploy := testDeployer(t,
+		withCustomFlyTomlPath("custom-fly-config.toml"),
+		withFixtureApp("deploy-node-custom-config-path"),
+		createRandomApp,
+		withOverwrittenConfig(func(d *testlib.DeployTestRun) map[string]any {
+			return map[string]any{
+				"app":    d.Extra["appName"],
+				"region": d.PrimaryRegion(),
+				"env": map[string]string{
+					"TEST_ID": d.ID(),
+				},
+			}
+		}),
+		testlib.DeployOnly,
+		testlib.DeployNow,
+		withWorkDirAppSource,
+	)
+
+	body, err := testlib.RunHealthCheck(fmt.Sprintf("https://%s.fly.dev", deploy.Extra["appName"].(string)))
+	require.NoError(t, err)
+
+	require.Contains(t, string(body), fmt.Sprintf("Hello, World! %s", deploy.Extra["TEST_ID"].(string)))
+}
+
+func TestDeployBasicNodeMonorepo(t *testing.T) {
+	deploy := testDeployer(t,
+		withCustomCwd("inner-repo"),
+		withFixtureApp("deploy-node-monorepo"),
+		createRandomApp,
+		withOverwrittenConfig(func(d *testlib.DeployTestRun) map[string]any {
+			return map[string]any{
+				"app":    d.Extra["appName"],
+				"region": d.PrimaryRegion(),
+				"env": map[string]string{
+					"TEST_ID": d.ID(),
+				},
+			}
+		}),
+		testlib.DeployOnly,
+		testlib.DeployNow,
+		withWorkDirAppSource,
+	)
+
+	body, err := testlib.RunHealthCheck(fmt.Sprintf("https://%s.fly.dev", deploy.Extra["appName"].(string)))
+	require.NoError(t, err)
+
+	require.Contains(t, string(body), fmt.Sprintf("Hello, World! %s", deploy.Extra["TEST_ID"].(string)))
+}
+
 func TestLaunchBasicNodeWithDockerfile(t *testing.T) {
 	deploy := testDeployer(t,
 		withFixtureApp("deploy-node"),
@@ -124,9 +174,25 @@ func withFixtureApp(name string) func(*testlib.DeployTestRun) {
 	}
 }
 
+func withCustomFlyTomlPath(name string) func(*testlib.DeployTestRun) {
+	return func(d *testlib.DeployTestRun) {
+		d.FlyTomlPath = name
+	}
+}
+
+func withCustomCwd(name string) func(*testlib.DeployTestRun) {
+	return func(d *testlib.DeployTestRun) {
+		d.Cwd = name
+	}
+}
+
 func withOverwrittenConfig(raw any) func(*testlib.DeployTestRun) {
 	return func(d *testlib.DeployTestRun) {
-		flyTomlPath := fmt.Sprintf("%s/fly.toml", d.WorkDir())
+		flyTomlPath := d.WorkDir()
+		if d.Cwd != "" {
+			flyTomlPath = fmt.Sprintf("%s/%s", flyTomlPath, d.Cwd)
+		}
+		flyTomlPath = fmt.Sprintf("%s/%s", flyTomlPath, d.FlyTomlPath)
 		data := make(map[string]any)
 		switch cast := raw.(type) {
 		case map[string]any:
