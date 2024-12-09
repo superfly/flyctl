c9b180fce608ee33bfb11796a0281ba664313331
lubien <lubien1996@gmail.com>
2024-12-09 11:19:24 -0300

    this is actually useless

--- test/testlib/deployer.go
+++ test/testlib/deployer.go
@@ -205,7 +205,6 @@ func (d *DeployTestRun) Start(ctx context.Context) error {
 	env := []string{
 		fmt.Sprintf("FLY_API_TOKEN=%s", d.apiToken),
 		fmt.Sprintf("DEPLOY_ORG_SLUG=%s", d.orgSlug),
-		fmt.Sprintf("DEPLOY_TRIGGER=%s", "launch"),
 	}
 
 	if d.appName != "" {
