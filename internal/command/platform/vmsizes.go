superfly/flyctl

commit bd6d9663b489a1fc0ee259d3c6ef1caf7f4a842f

Remove nomad vm sizes (#3024)

internal/command/platform/vmsizes.go
@@ -9,9 +9,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/superfly/flyctl/api"
-	"github.com/superfly/flyctl/client"
 	"github.com/superfly/flyctl/internal/command"
-	"github.com/superfly/flyctl/internal/config"
 	"github.com/superfly/flyctl/internal/flag"
 	"github.com/superfly/flyctl/internal/render"
 	"github.com/superfly/flyctl/iostreams"
@@ -24,7 +22,7 @@ func newVMSizes() (cmd *cobra.Command) {
 		short = "List VM Sizes"
 	)
 
-	cmd = command.New("vm-sizes", short, long, runVMSizes,
+	cmd = command.New("vm-sizes", short, long, runMachineVMSizes,
 		command.RequireSession,
 	)
 
@@ -32,33 +30,6 @@ func newVMSizes() (cmd *cobra.Command) {
 
 	flag.Add(cmd, flag.JSONOutput())
 	return
-}
-
-func runVMSizes(ctx context.Context) error {
-	client := client.FromContext(ctx).API()
-
-	sizes, err := client.PlatformVMSizes(ctx)
-	if err != nil {
-		return fmt.Errorf("failed retrieving sizes: %w", err)
-	}
-
-	out := iostreams.FromContext(ctx).Out
-	if config.FromContext(ctx).JSONOutput {
-		return render.JSON(out, sizes)
-	}
-
-	var rows [][]string
-	for _, size := range sizes {
-		rows = append(rows, []string{
-			size.Name,
-			cores(int(size.CPUCores)),
-			memory(size.MemoryMB),
-		})
-	}
-
-	render.Table(out, "Nomad platform", rows, "Name", "CPU Cores", "Memory")
-
-	return runMachineVMSizes(ctx)
 }
 
 func runMachineVMSizes(ctx context.Context) error {

commit d045e9089d030559ff2518d1862a34305ab26afd

Support JSON output of machine VM sizes
- Fixes #4146

Regressed in bd6d9663b489a1fc0ee259d3c6ef1caf7f4a842f according to bisect.

Previously the JSON output format included priceMonth and priceSecond as it was a dump of [fly.VMSize](https://pkg.go.dev/github.com/superfly/fly-go#VMSize) returned from the API, but the function which returned this information has been removed in a different change. Since the non-JSON format doesn't include this information, that's probably a future enhancement to add it to both.

internal/command/platform/vmsizes.go
@@ -3,6 +3,8 @@ package platform
 import (
 	"context"
 	"fmt"
+	"maps"
+	"slices"
 	"sort"
 
 	"github.com/samber/lo"
@@ -10,6 +12,7 @@ import (
 
 	fly "github.com/superfly/fly-go"
 	"github.com/superfly/flyctl/internal/command"
+	"github.com/superfly/flyctl/internal/config"
 	"github.com/superfly/flyctl/internal/flag"
 	"github.com/superfly/flyctl/internal/render"
 	"github.com/superfly/flyctl/iostreams"
@@ -34,6 +37,10 @@ func newVMSizes() (cmd *cobra.Command) {
 
 func runMachineVMSizes(ctx context.Context) error {
 	out := iostreams.FromContext(ctx).Out
+
+	if config.FromContext(ctx).JSONOutput {
+		return render.JSON(out, slices.Collect(maps.Values(fly.MachinePresets)))
+	}
 
 	type preset struct {
 		guest   *fly.MachineGuest