7e0854fc0a4ab97448e2a29a041432ddcf9ac83f
George Garside <apps@georgegarside.com>
2025-04-08 22:53:02 +0100

    Don't pick 'first' machine if specified machine is not found
    - Fixes #3807

--- internal/command/ssh/console.go
+++ internal/command/ssh/console.go
@@ -300,6 +300,10 @@ func addrForMachines(ctx context.Context, app *fly.AppCompact, console bool) (ad
 		namesWithRegion = append(namesWithRegion, nameWithRegion)
 	}
 
+	if machineID != "" && selectedMachine == nil {
+		return "", fmt.Errorf("--machine=%q not found/started", machineID)
+	}
+
 	if flag.GetBool(ctx, "select") {
 		if flag.IsSpecified(ctx, "machine") {
 			return "", errors.New("--machine can't be used with -s/--select")
