1ce5580eda334c3a4c785c65d50a2c59fb138b0b
Will Jordan <will.jordan@gmail.com>
2025-05-12 14:56:35 -0700

    group regions by GeoRegion update description with info on capacity column. link description to mcp server

--- internal/command/platform/regions.go
+++ internal/command/platform/regions.go
@@ -1,35 +1,38 @@
 package platform
 
 import (
+	"cmp"
 	"context"
 	"fmt"
+	"slices"
 	"sort"
 
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
+	"github.com/superfly/fly-go"
 	"github.com/superfly/fly-go/flaps"
-	"github.com/superfly/flyctl/internal/flapsutil"
-	"golang.org/x/exp/slices"
-
-	"github.com/superfly/flyctl/iostreams"
-
 	"github.com/superfly/flyctl/internal/command"
 	"github.com/superfly/flyctl/internal/config"
 	"github.com/superfly/flyctl/internal/flag"
+	"github.com/superfly/flyctl/internal/flapsutil"
 	"github.com/superfly/flyctl/internal/render"
+	"github.com/superfly/flyctl/iostreams"
 )
 
 // Hardcoded list of regions with GPUs
 // TODO: fetch this list from the graphql endpoint once it is there
 var gpuRegions = []string{"iad", "sjc", "syd", "ams"}
 
+const RegionsCommandDesc = `View a list of regions where Fly has datacenters.
+'Capacity' shows how many performance-1x VMs can currently be launched in each region.
+`
+
 func newRegions() (cmd *cobra.Command) {
 	const (
-		long = `View a list of regions where Fly has edges and/or datacenters
-`
 		short = "List regions"
 	)
 
-	cmd = command.New("regions", short, long, runRegions,
+	cmd = command.New("regions", short, RegionsCommandDesc+"\n", runRegions,
 		command.RequireSession,
 	)
 
@@ -43,7 +46,7 @@ func runRegions(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	regions, err := flapsClient.GetRegions(ctx, "performance-1x")
+	regions, err := flapsClient.GetRegions(ctx, "")
 	if err != nil {
 		return fmt.Errorf("failed retrieving regions: %w", err)
 	}
@@ -58,31 +61,39 @@ func runRegions(ctx context.Context) error {
 	}
 
 	var rows [][]string
-	for _, region := range regions {
-		gateway := ""
-		if region.GatewayAvailable {
-			gateway = "✓"
-		}
-		paidPlan := ""
-		if region.RequiresPaidPlan {
-			paidPlan = "✓"
-		}
-		gpuAvailable := ""
-		if slices.Contains(gpuRegions, region.Code) {
-			gpuAvailable = "✓"
-		}
+	regionGroups := lo.GroupBy(regions, func(item fly.Region) fly.GeoRegion { return item.GeoRegion })
+	keys := lo.Keys(regionGroups)
+	slices.SortFunc(keys, func(a, b fly.GeoRegion) int { return cmp.Compare(a, b) })
+	for _, key := range keys {
+		regionGroup := regionGroups[key]
+		rows = append(rows, []string{""})
+		rows = append(rows, []string{key.String() + ":"})
+		for _, region := range regionGroup {
+			gateway := ""
+			if region.GatewayAvailable {
+				gateway = "✓"
+			}
+			paidPlan := ""
+			if region.RequiresPaidPlan {
+				paidPlan = "✓"
+			}
+			gpuAvailable := ""
+			if slices.Contains(gpuRegions, region.Code) {
+				gpuAvailable = "✓"
+			}
 
-		capacity := fmt.Sprint(region.Capacity)
-		capacity = io.ColorScheme().RedGreenGradient(capacity, float64(region.Capacity)/1000)
+			capacity := fmt.Sprint(region.Capacity)
+			capacity = io.ColorScheme().RedGreenGradient(capacity, float64(region.Capacity)/1000)
 
-		rows = append(rows, []string{
-			region.Name,
-			region.Code,
-			gateway,
-			gpuAvailable,
-			capacity,
-			paidPlan,
-		})
+			rows = append(rows, []string{
+				region.Name,
+				region.Code,
+				gateway,
+				gpuAvailable,
+				capacity,
+				paidPlan,
+			})
+		}
 	}
 
 	return render.Table(out, "", rows, "Name", "Code", "Gateway", "GPUs", "Capacity", "Launch Plan+")
