0ba3348792f6f4b1bd4e48725929f6f4822eddb3
Dmitry Ledentsov <dmlled@yahoo.com>
2025-05-22 18:40:54 +0200

    feat: happiness both on fly.io and in docker compose

--- scanner/phoenix.go
+++ scanner/phoenix.go
@@ -143,17 +143,23 @@ a Postgres database.
 func PhoenixCallback(appName string, srcInfo *SourceInfo, plan *plan.LaunchPlan, flags []string) error {
 	envEExPath := "rel/env.sh.eex"
 	envEExContents := `
-# configure node for distributed erlang with IPV6 support
-export ERL_AFLAGS="-proto_dist inet6_tcp"
-export ECTO_IPV6="true"
-export DNS_CLUSTER_QUERY="${FLY_APP_NAME}.internal"
+if [ -n "$FLY_APP_NAME" ]; then
+  export DNS_CLUSTER_QUERY="${FLY_APP_NAME}.internal"
+  export RELEASE_NODE="${FLY_APP_NAME}-${FLY_IMAGE_REF##*-}@${FLY_PRIVATE_IP}"
+  # configure node for distributed erlang with IPV6 support
+  export ERL_AFLAGS="-proto_dist inet6_tcp"
+  export ECTO_IPV6="true"
+fi
+
 export RELEASE_DISTRIBUTION="name"
-export RELEASE_NODE="${FLY_APP_NAME}-${FLY_IMAGE_REF##*-}@${FLY_PRIVATE_IP}"
 
 # Uncomment to send crash dumps to stderr
 # This can be useful for debugging, but may log sensitive information
 # export ERL_CRASH_DUMP=/dev/stderr
 # export ERL_CRASH_DUMP_BYTES=4096
+
+# when not running on fly.io, use a sensible default
+export RELEASE_NODE=${RELEASE_NODE:-<%= @release.name %>@$(hostname)}
 `
 	_, err := os.Stat(envEExPath)
 	if os.IsNotExist(err) {
