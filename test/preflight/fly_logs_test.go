7e4afd8a7289d7124d156abdfaef29164fc4b18b
Kazuyoshi Kato <kaz@fly.io>
2025-07-15 09:37:23 -0700

    test: `fly logs` should be tested
    Sadly this test is currently flaky due to our backend plumbing though.

--- test/preflight/fly_logs_test.go
+++ test/preflight/fly_logs_test.go
@@ -4,9 +4,11 @@
 package preflight
 
 import (
+	"fmt"
+	"testing"
+
 	"github.com/stretchr/testify/require"
 	"github.com/superfly/flyctl/test/preflight/testlib"
-	"testing"
 )
 
 func TestFlyLogsMachineFlagBehavior(t *testing.T) {
@@ -39,4 +41,32 @@ func TestFlyLogsMachineFlagBehavior(t *testing.T) {
 	t.Run("TestRunsWhenInstanceShorthandProvided", func(tt *testing.T) {
 		f.Fly("logs --app " + appName + " --no-tail -i " + machineId)
 	})
+}
+
+func TestFlyLogs(t *testing.T) {
+	f := testlib.NewTestEnvFromEnv(t)
+
+	err := copyFixtureIntoWorkDir(f.WorkDir(), "deploy-node")
+	require.NoError(t, err)
+
+	toml := fmt.Sprintf("%s/fly.toml", f.WorkDir())
+	app := f.CreateRandomAppName()
+
+	err = testlib.OverwriteConfig(toml, map[string]any{
+		"app":    app,
+		"region": f.PrimaryRegion(),
+		"env":    map[string]string{"TEST_ID": f.ID()},
+	})
+	require.NoError(t, err)
+
+	f.Fly("launch --ha=false --copy-config --name %s --region %s --org %s --now", app, f.PrimaryRegion(), f.OrgSlug())
+
+	body, err := testlib.RunHealthCheck(fmt.Sprintf("https://%s.fly.dev", app))
+	require.NoError(t, err)
+	require.Contains(t, string(body), fmt.Sprintf("Hello, World! %s", f.ID()))
+
+	res := f.Fly("logs --app %s --no-tail", app)
+
+	// The app logs the following message to stdout.
+	require.Contains(t, res.StdOutString(), f.ID()+" is up\n")
 }
