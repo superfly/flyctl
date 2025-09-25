e638841574da4015a632d07db46a115f3f221ddf
Adam S <adam@fly.io>
2025-09-25 15:01:57 +0200

    add more meaningful comments

--- internal/flag/validation/compression.go
+++ internal/flag/validation/compression.go
@@ -7,23 +7,24 @@ import (
 )
 
 // ValidateCompressionFlag checks if the --compression flag has a valid value.
-// Valid values are "gzip", "zstd", or unset.
+// This can be "gzip" (soon to be legacy) or "zstd" (what we'd like to be the default)
 func ValidateCompressionFlag(compression string) error {
 	if compression == "" || compression == "gzip" || compression == "zstd" {
 		return nil // Valid
 	}
 
 	return flyerr.GenericErr{
-		Err:     fmt.Sprintf("Invalid value '%s' for --compression. Valid options are 'gzip', 'zstd', or leave unset.", compression),
+		Err:     fmt.Sprintf("Invalid value '%s' for compression. Valid options are 'gzip', 'zstd', or leave unset.", compression),
 		Suggest: "Please use 'gzip', 'zstd', or omit the flag.",
 	}
 }
 
-// ValidateCompressionLevelFlag checks if the --compression-level flag has a valid value.
+// ValidateCompressionLevelFlag checks if the --compression-level flag has a value between 0 and 9.
+// This is what is currently supported by Builder (they map these to proper zstd compression levels)
 func ValidateCompressionLevelFlag(level int) error {
 	if level < 0 || level > 9 {
 		return flyerr.GenericErr{
-			Err:     fmt.Sprintf("Invalid value '%d' for --compression-level. Must be an integer between 0 and 9.", level),
+			Err:     fmt.Sprintf("Invalid value '%d' for compression level. Must be an integer between 0 and 9.", level),
 			Suggest: "Please use an integer between 0 and 9, or omit the flag.",
 		}
 	}
