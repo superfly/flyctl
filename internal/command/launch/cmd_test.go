154fb986a640d8068548341e47c5b054ca090482
Rian McGuire <rian@fly.io>
2025-10-01 14:26:56 +1000

    Configure scheduled_snapshots via fly.toml and flyctl launch (#4586)
    * Configure scheduled_snapshots via fly.toml
    
    * Configure scheduled_snapshots via flyctl launch

--- internal/command/launch/cmd_test.go
+++ internal/command/launch/cmd_test.go
@@ -2,9 +2,13 @@ package launch
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+	fly "github.com/superfly/fly-go"
+	"github.com/superfly/flyctl/internal/appconfig"
 	"github.com/superfly/flyctl/internal/flag/flagctx"
 	"github.com/superfly/flyctl/iostreams"
 )
@@ -89,7 +93,7 @@ func TestValidatePostgresFlags(t *testing.T) {
 					t.Errorf("expected error but got none")
 					return
 				}
-				if tt.errorMsg != "" && !containsString(err.Error(), tt.errorMsg) {
+				if tt.errorMsg != "" && !strings.Contains(err.Error(), tt.errorMsg) {
 					t.Errorf("expected error message to contain '%s', got '%s'", tt.errorMsg, err.Error())
 				}
 			} else {
@@ -101,12 +105,130 @@ func TestValidatePostgresFlags(t *testing.T) {
 	}
 }
 
-// Helper function to check if a string contains a substring
-func containsString(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
+func TestParseMountOptions(t *testing.T) {
+	tests := []struct {
+		name          string
+		options       string
+		expectedMount appconfig.Mount
+		expectError   bool
+		errMsg        string
+	}{
+		{
+			name:          "empty options",
+			options:       "",
+			expectedMount: appconfig.Mount{},
+		},
+		{
+			name:    "scheduled_snapshots true",
+			options: "scheduled_snapshots=true",
+			expectedMount: appconfig.Mount{
+				ScheduledSnapshots: fly.Pointer(true),
+			},
+		},
+		{
+			name:    "scheduled_snapshots false",
+			options: "scheduled_snapshots=false",
+			expectedMount: appconfig.Mount{
+				ScheduledSnapshots: fly.Pointer(false),
+			},
+		},
+		{
+			name:        "scheduled_snapshots invalid value",
+			options:     "scheduled_snapshots=invalid",
+			expectError: true,
+			errMsg:      "invalid value for scheduled_snapshots",
+		},
+		{
+			name:    "snapshot_retention",
+			options: "snapshot_retention=7",
+			expectedMount: appconfig.Mount{
+				SnapshotRetention: fly.Pointer(7),
+			},
+		},
+		{
+			name:        "snapshot_retention invalid",
+			options:     "snapshot_retention=invalid",
+			expectError: true,
+			errMsg:      "invalid value for snapshot_retention",
+		},
+		{
+			name:    "initial_size",
+			options: "initial_size=10GB",
+			expectedMount: appconfig.Mount{
+				InitialSize: "10GB",
+			},
+		},
+		{
+			name:    "auto_extend_size_threshold",
+			options: "auto_extend_size_threshold=80",
+			expectedMount: appconfig.Mount{
+				AutoExtendSizeThreshold: 80,
+			},
+		},
+		{
+			name:        "auto_extend_size_threshold invalid",
+			options:     "auto_extend_size_threshold=invalid",
+			expectError: true,
+			errMsg:      "invalid value for auto_extend_size_threshold",
+		},
+		{
+			name:    "auto_extend_size_increment",
+			options: "auto_extend_size_increment=1GB",
+			expectedMount: appconfig.Mount{
+				AutoExtendSizeIncrement: "1GB",
+			},
+		},
+		{
+			name:    "auto_extend_size_limit",
+			options: "auto_extend_size_limit=100GB",
+			expectedMount: appconfig.Mount{
+				AutoExtendSizeLimit: "100GB",
+			},
+		},
+		{
+			name:    "multiple options",
+			options: "initial_size=10GB,scheduled_snapshots=true,snapshot_retention=14",
+			expectedMount: appconfig.Mount{
+				InitialSize:        "10GB",
+				ScheduledSnapshots: fly.Pointer(true),
+				SnapshotRetention:  fly.Pointer(14),
+			},
+		},
+		{
+			name:        "unknown option",
+			options:     "unknown_option=value",
+			expectError: true,
+			errMsg:      "unknown mount option",
+		},
+		{
+			name:        "invalid format",
+			options:     "invalid_format",
+			expectError: true,
+			errMsg:      "invalid mount option",
+		},
 	}
-	return false
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mount := &appconfig.Mount{}
+			err := ParseMountOptions(mount, tt.options)
+
+			if tt.expectError {
+				if err == nil {
+					t.Errorf("expected error but got none")
+					return
+				}
+				if tt.errMsg != "" && !strings.Contains(err.Error(), tt.errMsg) {
+					t.Errorf("expected error message to contain '%s', got '%s'", tt.errMsg, err.Error())
+				}
+			} else {
+				if err != nil {
+					t.Errorf("expected no error but got: %v", err)
+					return
+				}
+
+				assert.Equal(t, tt.expectedMount, *mount)
+			}
+		})
+	}
 }
