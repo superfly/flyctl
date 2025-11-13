724d50a2b5f3a105c8fc226b81ba18fbf5f576ea
Jon Phenow <jon@jphenow.com>
2025-11-13 12:17:25 -0600

    I can't see why we need to force that nil

--- internal/command/launch/sessions.go
+++ internal/command/launch/sessions.go
@@ -288,7 +288,7 @@ func runSessionFinalize(ctx context.Context) (err error) {
 	// This should never be changed by the UI!!
 	state.Plan.ScannerFamily = oldPlan.ScannerFamily
 
-	state.updateConfig(ctx, state.Plan, nil, state.Config)
+	state.updateConfig(ctx)
 
 	manifestPath := flag.GetString(ctx, "manifest-path")
 
