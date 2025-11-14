cc431038d8081b6b6ffc5bd1ec8c3669f9d03c9d
Jon Phenow <jon@jphenow.com>
2025-11-14 13:13:29 -0600

    these appear to be gone

--- internal/command/launch/plan_commands.go
+++ internal/command/launch/plan_commands.go
@@ -15,7 +15,7 @@ import (
 func NewPlan() *cobra.Command {
 	const desc = `[experimental] Granular subcommands for creating and configuring apps`
 
-	cmd := command.New("plan", desc, desc, nil, command.RequireSession, command.RequireUiex, command.LoadAppConfigIfPresent)
+	cmd := command.New("plan", desc, desc, nil, command.RequireSession, command.LoadAppConfigIfPresent)
 	cmd.Args = cobra.NoArgs
 
 	cmd.AddCommand(newPropose())
@@ -33,7 +33,7 @@ func NewPlan() *cobra.Command {
 
 func newPropose() *cobra.Command {
 	const desc = "[experimental] propose a plan based on scanning the source code or Dockerfile"
-	cmd := command.New("propose", desc, desc, runPropose, command.LoadAppConfigIfPresent, command.RequireUiex)
+	cmd := command.New("propose", desc, desc, runPropose, command.LoadAppConfigIfPresent)
 
 	flag.Add(cmd,
 		flag.Region(),
@@ -92,7 +92,7 @@ func newPropose() *cobra.Command {
 
 func newCreate() *cobra.Command {
 	const desc = "[experimental] create application"
-	cmd := command.New("create", desc, desc, runCreate, command.RequireUiex)
+	cmd := command.New("create", desc, desc, runCreate)
 	cmd.Args = cobra.ExactArgs(1)
 
 	flag.Add(cmd,
@@ -118,7 +118,7 @@ func newCreate() *cobra.Command {
 
 func newPostgres() *cobra.Command {
 	const desc = "[experimental] create postgres database"
-	cmd := command.New("postgres", desc, desc, runPostgres, command.RequireUiex)
+	cmd := command.New("postgres", desc, desc, runPostgres)
 	cmd.Args = cobra.ExactArgs(1)
 
 	flag.Add(cmd,
@@ -137,7 +137,7 @@ func newPostgres() *cobra.Command {
 
 func newRedis() *cobra.Command {
 	const desc = "[experimental] create redis database"
-	cmd := command.New("redis", desc, desc, runRedis, command.RequireUiex)
+	cmd := command.New("redis", desc, desc, runRedis)
 	cmd.Args = cobra.ExactArgs(1)
 
 	flag.Add(cmd,
@@ -156,7 +156,7 @@ func newRedis() *cobra.Command {
 
 func newTigris() *cobra.Command {
 	const desc = "[experimental] create tigris database"
-	cmd := command.New("tigris", desc, desc, runTigris, command.RequireUiex)
+	cmd := command.New("tigris", desc, desc, runTigris)
 	cmd.Args = cobra.ExactArgs(1)
 
 	flag.Add(cmd,
@@ -175,7 +175,7 @@ func newTigris() *cobra.Command {
 
 func newGenerate() *cobra.Command {
 	const desc = "[experimental] generate Dockerfile and other configuration files based on the plan"
-	cmd := command.New("generate", desc, desc, runGenerate, command.RequireUiex)
+	cmd := command.New("generate", desc, desc, runGenerate)
 	cmd.Args = cobra.ExactArgs(1)
 
 	flag.Add(cmd,
