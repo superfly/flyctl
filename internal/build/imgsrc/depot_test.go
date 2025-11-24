35e8fde4f925f620cc96fb599b986f72f07881aa
Rian McGuire <rian@fly.io>
2025-11-24 17:21:04 +1100

    imgsrc: replace GraphQL mutations with uiex

--- internal/build/imgsrc/depot_test.go
+++ internal/build/imgsrc/depot_test.go
@@ -2,19 +2,31 @@ package imgsrc
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/superfly/fly-go"
 	"github.com/superfly/flyctl/internal/flyutil"
+	"github.com/superfly/flyctl/internal/mock"
+	"github.com/superfly/flyctl/internal/uiex"
+	"github.com/superfly/flyctl/internal/uiexutil"
 	"github.com/superfly/flyctl/iostreams"
 )
 
 func TestInitBuilder(t *testing.T) {
 	ctx := context.Background()
 	ctx = flyutil.NewContextWithClient(ctx, flyutil.NewClientFromOptions(ctx, fly.ClientOptions{BaseURL: "invalid://localhost"}))
+
+	mockUiex := &mock.UiexClient{
+		EnsureDepotBuilderFunc: func(ctx context.Context, in uiex.EnsureDepotBuilderRequest) (*uiex.EnsureDepotBuilderResponse, error) {
+			return nil, errors.New(`unsupported protocol scheme "invalid"`)
+		},
+	}
+	ctx = uiexutil.NewContextWithClient(ctx, mockUiex)
+
 	ios, _, _, _ := iostreams.Test()
-	build := newBuild("build1", false)
+	build := newBuild(1, false)
 
 	// The invocation below doesn't test things much, but it may be better than nothing.
 	_, _, err := initBuilder(ctx, build, "app1", ios, DepotBuilderScopeOrganization)
