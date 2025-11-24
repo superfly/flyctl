ae824c29e53e512a0ee5a82d327e222e1252eff2
Rian McGuire <rian@fly.io>
2025-11-24 15:30:53 +1100

    deploy: use flaps to get app IPs

--- internal/command/deploy/machines_deploymachinesapp.go
+++ internal/command/deploy/machines_deploymachinesapp.go
@@ -23,8 +23,8 @@ import (
 	"github.com/superfly/flyctl/internal/buildinfo"
 	"github.com/superfly/flyctl/internal/command/deploy/statics"
 	machcmd "github.com/superfly/flyctl/internal/command/machine"
+	"github.com/superfly/flyctl/internal/flapsutil"
 	"github.com/superfly/flyctl/internal/flyerr"
-	"github.com/superfly/flyctl/internal/flyutil"
 	"github.com/superfly/flyctl/internal/machine"
 	"github.com/superfly/flyctl/internal/statuslogger"
 	"github.com/superfly/flyctl/internal/tracing"
@@ -1350,12 +1350,13 @@ func (md *machineDeployment) checkDNS(ctx context.Context) error {
 	ctx, cancel := context.WithTimeout(ctx, time.Second*70)
 	defer cancel()
 
-	client := flyutil.ClientFromContext(ctx)
-	ipAddrs, err := client.GetIPAddresses(ctx, md.appConfig.AppName)
+	flapsClient := flapsutil.ClientFromContext(ctx)
+	ipRes, err := flapsClient.GetIPAssignments(ctx, md.appConfig.AppName)
 	if err != nil {
 		tracing.RecordError(span, err, "failed to get ip addresses")
 		return err
 	}
+	ipAddrs := ipRes.IPs
 
 	if appURL := md.appConfig.URL(); appURL != nil && len(ipAddrs) > 0 {
 		iostreams := iostreams.FromContext(ctx)
@@ -1379,9 +1380,9 @@ func (md *machineDeployment) checkDNS(ctx context.Context) error {
 
 			var numIPv4, numIPv6 int
 			for _, ipAddr := range ipAddrs {
-				if (ipAddr.Type == "v4" && ipAddr.Region == "global") || ipAddr.Type == "shared_v4" {
+				if strings.Contains(ipAddr.IP, ".") && (ipAddr.Region == "global" || ipAddr.Shared) {
 					numIPv4 += 1
-				} else if ipAddr.Type == "v6" && ipAddr.Region == "global" {
+				} else if strings.Contains(ipAddr.IP, ":") && ipAddr.Region == "global" {
 					numIPv6 += 1
 				}
 			}
